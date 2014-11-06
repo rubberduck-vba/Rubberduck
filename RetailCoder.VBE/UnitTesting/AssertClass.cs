b08b317c0efba38a4316765990622f53edab0d6e
retailcoder <retailcoder@gmail.com>
2014-11-05 19:02:35 -0500

    Addressing issue #4 with XML comments and type checks.

--- RetailCoder.VBE/UnitTesting/AssertClass.cs
+++ RetailCoder.VBE/UnitTesting/AssertClass.cs
@@ -11,6 +11,11 @@ namespace RetailCoderVBE.UnitTesting
     [ComDefaultInterface(typeof(IAssert))]
     public class AssertClass : IAssert
     {
+        /// <summary>
+        /// Verifies that the specified condition is <c>true</c>. The assertion fails if the condition is <c>false</c>.
+        /// </summary>
+        /// <param name="condition">Any Boolean value or expression.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
         public void IsTrue(bool condition, string message = null)
         {
             if (condition)
@@ -23,6 +28,11 @@ namespace RetailCoderVBE.UnitTesting
             }
         }
 
+        /// <summary>
+        /// Verifies that the specified condition is <c>false</c>. The assertion fails if the condition is <c>true</c>.
+        /// </summary>
+        /// <param name="condition">Any Boolean value or expression.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
         public void IsFalse(bool condition, string message = null)
         {
             if (!condition)
@@ -35,16 +45,29 @@ namespace RetailCoderVBE.UnitTesting
             }
         }
 
+        /// <summary>
+        /// Indicates that the assertion cannot be verified.
+        /// </summary>
+        /// <param name="message">An optional message to display.</param>
         public void Inconclusive(string message = null)
         {
             AssertHandler.OnAssertInconclusive(message);
         }
 
+        /// <summary>
+        /// Fails the assertion without checking any conditions.
+        /// </summary>
+        /// <param name="message">An optional message to display.</param>
         public void Fail(string message = null)
         {
             AssertHandler.OnAssertFailed("Fail", message);
         }
 
+        /// <summary>
+        /// Verifies that the specified object is <c>Nothing</c>. The assertion fails if it is not <c>Nothing</c>.
+        /// </summary>
+        /// <param name="value">The object to verify.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
         public void IsNothing(object value, string message = null)
         {
             if (value == null)
@@ -57,6 +80,11 @@ namespace RetailCoderVBE.UnitTesting
             }
         }
 
+        /// <summary>
+        /// Verifies that the specified object is not <c>Nothing</c>. The assertion fails if it is <c>Nothing</c>.
+        /// </summary>
+        /// <param name="value">The object to verify.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
         public void IsNotNothing(object value, string message = null)
         {
             if (value != null)
@@ -69,51 +97,93 @@ namespace RetailCoderVBE.UnitTesting
             }
         }
 
-        public void AreEqual(object value1, object value2, string message = null)
+        /// <summary>
+        /// Verifies that two specified objects are equal. The assertion fails if the objects are not equal.
+        /// </summary>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        /// <remarks>
+        /// <paramref name="expected"/> and <paramref name="actual"/> must be the same type.
+        /// </remarks>
+        public void AreEqual(object expected, object actual, string message = null)
         {
-            if (value1.Equals(value2))
+            if (expected.GetType() != actual.GetType())
+            {
+                AssertHandler.OnAssertInconclusive("[expected] and [actual] values are not the same type.");
+                return;
+            }
+
+            if (expected.Equals(actual))
             {
                 AssertHandler.OnAssertSucceeded();
             }
             else
             {
-                AssertHandler.OnAssertFailed("AreEqual", message);
+                AssertHandler.OnAssertFailed("AreEqual", string.Concat("expected: ", expected.ToString(), "; actual: ", actual.ToString(), ". ", message));
             }
         }
 
-        public void AreNotEqual(object value1, object value2, string message = null)
+        /// <summary>
+        /// Verifies that two specified objects are not equal. The assertion fails if the objects are equal.
+        /// </summary>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        /// <remarks>
+        /// <paramref name="expected"/> and <paramref name="actual"/> must be the same type.
+        /// </remarks>
+        public void AreNotEqual(object expected, object actual, string message = null)
         {
-            if (!value1.Equals(value2))
+            if (expected.GetType() != actual.GetType())
+            {
+                AssertHandler.OnAssertInconclusive("[expected] and [actual] values are not the same type.");
+                return;
+            }
+
+            if (!expected.Equals(actual))
             {
                 AssertHandler.OnAssertSucceeded();
             }
             else
             {
-                AssertHandler.OnAssertFailed("AreNotEqual", message);
+                AssertHandler.OnAssertFailed("AreNotEqual", string.Concat("expected: ", expected.ToString(), "; actual: ", actual.ToString(), ". ", message));
             }
         }
 
-        public void AreSame(object value1, object value2, string message = null)
+        /// <summary>
+        /// Verifies that two specified object variables refer to the same object. The assertion fails if they refer to different objects.
+        /// </summary>
+        /// <param name="expected">The expected reference.</param>
+        /// <param name="actual">The actual reference.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        public void AreSame(object expected, object actual, string message = null)
         {
-            if (object.ReferenceEquals(value1, value2))
+            if (object.ReferenceEquals(expected, actual))
             {
                 AssertHandler.OnAssertSucceeded();
             }
             else
             {
-                AssertHandler.OnAssertFailed("AreSame", message);
+                AssertHandler.OnAssertFailed("AreSame", string.Concat("expected: ", expected.GetHashCode(), "; actual: ", actual.GetHashCode(), ". ", message));
             }
         }
 
-        public void AreNotSame(object value1, object value2, string message = null)
+        /// <summary>
+        /// Verifies that two specified object variables refer to different objects. The assertion fails if they refer to the same object.
+        /// </summary>
+        /// <param name="expected">The expected reference.</param>
+        /// <param name="actual">The actual reference.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        public void AreNotSame(object expected, object actual, string message = null)
         {
-            if (!object.ReferenceEquals(value1, value2))
+            if (!object.ReferenceEquals(expected, actual))
             {
                 AssertHandler.OnAssertSucceeded();
             }
             else
             {
-                AssertHandler.OnAssertFailed("AreNotSame", message);
+                AssertHandler.OnAssertFailed("AreNotSame", string.Concat("expected: ", expected.GetHashCode(), "; actual: ", actual.GetHashCode(), ". ", message));
             }
         }
     }
