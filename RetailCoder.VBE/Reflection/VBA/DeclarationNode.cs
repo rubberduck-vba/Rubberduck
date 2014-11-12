b62d07842c2535c9ff9131dea9a9cecd6b0b1387
retailcoder <retailcoder@gmail.com>
2014-11-11 19:14:38 -0500

    cleanup

---
+++ RetailCoder.VBE/Reflection/VBA/DeclarationNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Rubberduck.Reflection.VBA
+{
+    internal class DeclarationNode : SyntaxTreeNode
+    {
+        public DeclarationNode(string scope, Match match, string comment)
+            : base(scope, match, comment, true)
+        {
+
+        }
+
+
+        IEnumerable<DeclarationNodeBase> _declarations;
+        IEnumerable<DeclarationNodeBase> Declarations { get { return _declarations; } }
+    }
+}
