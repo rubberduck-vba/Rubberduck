9561af52bf6629422ddc7b8e0e86512ced1b82d0
Christopher J. McClellan <ckuhn203@gmail.com>
2014-11-22 10:07:28 -0500

    added case insensitive contains string extension

---
+++ RetailCoder.VBE/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubberduck.Extensions
+{
+    [System.Runtime.InteropServices.ComVisible(false)]
+    public static class StringExtensions
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
