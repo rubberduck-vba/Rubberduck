dc11a614124f13e3538df50ae037856cfc458b18
retailcoder <retailcoder@gmail.com>
2014-11-21 21:25:02 -0500

    ForLoopSyntax

---
+++ RetailCoder.VBE/VBA/Parser/Grammar/ForLoopSyntax.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Rubberduck.VBA.Parser.Grammar
+{
+    [ComVisible(false)]
+    public class ForLoopSyntax : SyntaxBase
+    {
+        public ForLoopSyntax()
+            : base(SyntaxType.HasChildNodes)
+        {
+            
+        }
+
+        protected override bool MatchesSyntax(string instruction, out Match match)
+        {
+            match = Regex.Match(instruction, VBAGrammar.ForLoopSyntax);
+            return match.Success;
+        }
+
+        protected override SyntaxTreeNode CreateNode(Instruction instruction, string scope, Match match)
+        {
+            
+        }
+    }
+}
--- RetailCoder.VBE/VBA/Parser/IfBlockNode.cs
+++ RetailCoder.VBE/VBA/Parser/IfBlockNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Rubberduck.VBA.Parser.Grammar;
@@ -15,5 +16,18 @@ namespace Rubberduck.VBA.Parser
 
         public Expression Condition { get { return new Expression(RegexMatch.Groups["condition"].Value); } }
         public Expression Expression { get { return new Expression(RegexMatch.Groups["expression"].Value); } }
+    }
+
+    [ComVisible(false)]
+    public class ForLoopNode : CodeBlockNode
+    {
+        public ForLoopNode(Instruction instruction, string scope, Match match, IEnumerable<SyntaxTreeNode> nodes) 
+            : base(instruction, scope, match, new[] {ReservedKeywords.Next}, null, nodes)
+        {
+        }
+
+        public Expression Lower { get { return new Expression(RegexMatch.Groups["lower"].Value); } }
+        public Expression Upper { get { return new Expression(RegexMatch.Groups["upper"].Value); } }
+        public Expression Step { get { return new Expression(RegexMatch.Groups["step"].Value); } }
     }
 }
