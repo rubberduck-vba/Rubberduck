9dbaa0884c19d2d5989bb12f743c7cbaa74669aa
Christopher J. McClellan <ckuhn203@gmail.com>
2014-11-22 21:33:06 -0500

    extracted interfaces for config/todo

---
+++ RetailCoder.VBE/Config/ToDoListSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using System.Runtime.InteropServices;
+
+namespace Rubberduck.Config
+{
+    interface IToDoListSettings
+    {
+        ToDoMarker[] ToDoMarkers { get; set; }
+    }
+
+    [ComVisible(false)]
+    [XmlTypeAttribute(AnonymousType = true)]
+    public class ToDoListSettings : IToDoListSettings
+    {
+        [XmlArrayItemAttribute("ToDoMarker", IsNullable = false)]
+        public ToDoMarker[] ToDoMarkers { get; set; }
+    }
+}
