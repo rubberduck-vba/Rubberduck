f13be11510a99d23bc8c16180d7946af36b0c5d1
retailcoder <retailcoder@gmail.com>
2014-11-24 00:13:39 -0500

    added missing file

---
+++ RetailCoder.VBE/UI/ToDoItems/ToDoExplorerWindow.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Rubberduck.ToDoItems;
+
+namespace Rubberduck.UI.ToDoItems
+{
+    [ComVisible(false)]
+    public partial class ToDoExplorerWindow : UserControl, IDockableUserControl
+    {
+        private const string ClassId = "8B071EDA-2C9C-4009-9A22-A1958BF98B28";
+        string IDockableUserControl.ClassId { get { return ClassId; } }
+        string IDockableUserControl.Caption { get { return "ToDo Explorer"; } }
+
+        private readonly BindingList<ToDoItem> _todoItems;
+
+        public ToDoExplorerWindow()
+            : this(new ToDoItem[]{})
+        { }
+
+        public ToDoExplorerWindow(IEnumerable<ToDoItem> items)
+        {
+            _todoItems = new BindingList<ToDoItem>(items.ToList());
+            InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            todoItemsGridView.DataSource = _todoItems;
+            var descriptionColumn = todoItemsGridView.Columns["Description"];
+            if (descriptionColumn != null)
+            {
+                descriptionColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+
+            todoItemsGridView.CellDoubleClick += ToDoGridViewCellDoubleClicked;
+            refreshButton.Click += RefreshButtonClicked;
+        }
+
+        public event EventHandler<ToDoItemClickEventArgs> NavigateToDoItem;
+        private void ToDoGridViewCellDoubleClicked(object sender, DataGridViewCellEventArgs e)
+        {
+            var handler = NavigateToDoItem;
+            if (handler == null)
+            {
+                return;
+            }
+
+            var item = (ToDoItem)todoItemsGridView[e.ColumnIndex, e.RowIndex].OwningRow.DataBoundItem;
+            var args = new ToDoItemClickEventArgs(item);
+            handler(this, args);
+        }
+
+        public event EventHandler RefreshToDoItems;
+        private void RefreshButtonClicked(object sender, EventArgs e)
+        {
+            var handler = RefreshToDoItems;
+            if (handler == null)
+            {
+                return;
+            }
+
+            handler(this, EventArgs.Empty);
+        }
+
+        public void SetItems(IEnumerable<ToDoItem> items)
+        {
+            _todoItems.Clear();
+            foreach (var toDoItem in items)
+            {
+                _todoItems.Add(toDoItem);
+            }
+        }
+    }
+}
