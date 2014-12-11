82d2c15aaaa5b091bedbe604b1976b893e2e7ad8
Christopher J. McClellan <ckuhn203@gmail.com>
2014-12-10 23:44:54 -0500

    Addded HostAppBase; Implemented Excel

--- RetailCoder.VBE/IHostApplication.cs
+++ RetailCoder.VBE/IHostApplication.cs
@@ -9,45 +9,59 @@ namespace Rubberduck
     [ComVisible(false)]
     public interface IHostApplication
     {
+        /// <summary>   Runs VBA procedure specified by name. </summary>
+        /// <param name="target"> Method Name of the Target VBA Procedure. </param>
         void Run(string target);
+
+        /// <summary>   Timed call to Application.Run </summary>
+        /// <param name="projectName">  Name of the project containing the method to be run. </param>
+        /// <param name="moduleName">   Name of the module containing the method to be run. </param>
+        /// <param name="methodName">   Name of the method run. </param>
+        /// <returns>   Number of milliseconds it took to run the VBA procedure. </returns>
         long TimedMethodCall(string projectName, string moduleName, string methodName);
     }
 
     [ComVisible(false)]
-    public class ExcelApp : IHostApplication
+    public abstract class HostApplicationBase<TApplication> : IHostApplication
     {
-        Excel.Application _application;
-        public ExcelApp()
+        protected readonly TApplication _application;
+        protected HostApplicationBase(string applicationName)
         {
-            _application = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            _application = (TApplication)Marshal.GetActiveObject(applicationName + ".Application");
         }
 
-        ~ExcelApp()
+        ~HostApplicationBase()
         {
             Marshal.ReleaseComObject(_application);
         }
 
-        public void Run(string target)
-        {
-            _application.Run(target);
-        }
+        public abstract void Run(string target);
+        protected abstract string GenerateFullyQualifiedName(string projectName, string moduleName, string methodName);
 
-        /// <summary>   Timed call to Application.Run </summary>
-        ///
-        /// <param name="projectName">  Name of the project containing the method to be run. </param>
-        /// <param name="moduleName">   Name of the module containing the method to be run. </param>
-        /// <param name="methodName">   Name of the method run. </param>
-        ///
-        /// <returns>   Number of milliseconds it took to run the VBA procedure. </returns>
         public long TimedMethodCall(string projectName, string moduleName, string methodName)
         {
             var stopwatch = Stopwatch.StartNew();
-            //excel can have multiple projects, so it supports fully quantified names
-            _application.Run(string.Concat(projectName, ".", moduleName, ".", methodName));
+
+            Run(GenerateFullyQualifiedName(projectName, moduleName, methodName));
 
             stopwatch.Stop();
-
             return stopwatch.ElapsedMilliseconds;
+        }
+    }
+
+    [ComVisible(false)]
+    public class ExcelApp : HostApplicationBase<Excel.Application>
+    {
+        public ExcelApp() : base("Excel") { }
+
+        public override void Run(string target)
+        {
+            base._application.Run(target);
+        }
+
+        protected override string GenerateFullyQualifiedName(string projectName, string moduleName, string methodName)
+        {
+            return string.Concat(projectName, ".", moduleName, ".", methodName);
         }
     }
 
