bc9f8a2c4207e5099b0820dabf3faf9faf9d5ab0
Christopher J. McClellan <ckuhn203@gmail.com>
2014-12-18 20:06:05 -0500

    SettingsDialog successfully determines which node is clicked

--- RetailCoder.VBE/UI/Settings/SettingsDialog.cs
+++ RetailCoder.VBE/UI/Settings/SettingsDialog.cs
@@ -14,7 +14,7 @@ namespace Rubberduck.UI.Settings
     public partial class SettingsDialog : Form
     {
         private Configuration _config;
-        private ConfigurationTreeView _treeview;
+        private ConfigurationTreeViewControl _treeview;
         private Control _settingsControl;
 
         public SettingsDialog()
@@ -22,16 +22,47 @@ namespace Rubberduck.UI.Settings
             InitializeComponent();
 
             _config = ConfigurationLoader.LoadConfiguration();
-            _treeview = new ConfigurationTreeView(_config);
-            //Smell: many nested news
-            _settingsControl = new TodoListSettingsControl(new TodoSettingModel(new List<ToDoMarker>(_config.UserSettings.ToDoListSettings.ToDoMarkers)));
-            
+            _treeview = new ConfigurationTreeViewControl(_config);
+
+            var markers = new List<ToDoMarker>(_config.UserSettings.ToDoListSettings.ToDoMarkers);
+            _settingsControl = new TodoListSettingsControl(new TodoSettingModel(markers));
+           
             this.splitContainer1.Panel1.Controls.Add(_treeview);
             this.splitContainer1.Panel2.Controls.Add(_settingsControl);
 
             _treeview.Dock = DockStyle.Fill;
             _settingsControl.Dock = DockStyle.Fill;
 
+            RegisterEvents();   
         }
+
+        private void RegisterEvents()
+        {
+            _treeview.NodeSelected += _treeview_NodeSelected;
+           
+        }
+
+        private void _treeview_NodeSelected(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Rubberduck")
+            {
+                return;
+            }
+
+            if (e.Node.Text == "Todo List")
+            {
+                //todo: activate todolist control in Panel2
+                return;
+            }
+
+            if (e.Node.Text == "Code Inpsections")
+            {
+                //todo: activate inspection control in Panel2
+                return;
+            }
+            throw new NotImplementedException();
+        }
+
+
     }
 }
