359b51fe30d04a8022e7ff460d1e73f0d365a96e
Christopher J. McClellan <ckuhn203@gmail.com>
2015-01-21 22:24:29 -0500

    moved VbProjectExtensions from reflection to extension namespace

--- RetailCoder.VBE/Extensions/VbProjectExtensions.cs
+++ RetailCoder.VBE/Extensions/VbProjectExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.Vbe.Interop;
 
-namespace Rubberduck.Reflection
+namespace Rubberduck.Extensions
 {
     [ComVisible(false)]
     public static class ProjectExtensions
