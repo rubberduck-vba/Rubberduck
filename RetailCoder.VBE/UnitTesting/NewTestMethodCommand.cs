08ac9a582364c6492fe933235afe0a43324128f4
Christopher J. McClellan <ckuhn203@gmail.com>
2015-01-21 22:34:41 -0500

    moved vbcomponent extensions from reflection to extensions

--- RetailCoder.VBE/UnitTesting/NewTestMethodCommand.cs
+++ RetailCoder.VBE/UnitTesting/NewTestMethodCommand.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.Vbe.Interop;
 using Rubberduck.Reflection;
+using Rubberduck.Extensions;
 
 namespace Rubberduck.UnitTesting
 {
