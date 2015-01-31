8ac9f1d17e7f95963a9c148dc88033548d7224ee
retailcoder <retailcoder@gmail.com>
2015-01-30 22:53:21 -0500

    added VBProjectParseResult class to encapsulate IDictionary<QualifiedModuleName,IParseTree>.

---
+++ RetailCoder.VBE/VBA/Nodes/VBProjectParseResult.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Antlr4.Runtime.Tree;
+using Rubberduck.Inspections;
+
+namespace Rubberduck.VBA.Nodes
+{
+    public class VBProjectParseResult
+    {
+        private readonly IDictionary<QualifiedModuleName, IParseTree> _results;
+
+        public VBProjectParseResult()
+            : this(new Dictionary<QualifiedModuleName, IParseTree>())
+        { }
+
+        public VBProjectParseResult(IDictionary<QualifiedModuleName,IParseTree> results)
+        {
+            _results = results;
+        }
+
+        public IParseTree this[QualifiedModuleName qualifiedName]
+        {
+            get { return _results[qualifiedName]; }
+        }
+
+        public void AddParseTree(QualifiedModuleName qualifiedName, IParseTree parseTree)
+        {
+            _results.Add(qualifiedName, parseTree);
+        }
+    }
+}
