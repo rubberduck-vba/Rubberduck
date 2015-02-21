9c20bce10b4b79184e79566497a8ac41301a64c5
retailcoder <retailcoder@gmail.com>
2015-02-21 17:07:22 -0500

    removed unused "using" statements

--- RetailCoder.VBE/Inspections/VariableNotAssignedInspection.cs
+++ RetailCoder.VBE/Inspections/VariableNotAssignedInspection.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Rubberduck.VBA;
-using Rubberduck.VBA.Grammar;
 using Rubberduck.VBA.Nodes;
 
 namespace Rubberduck.Inspections
