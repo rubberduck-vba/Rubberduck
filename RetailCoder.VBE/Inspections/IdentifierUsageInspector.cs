rubberduck-vba/Rubberduck

commit 31a695c4c31c2e907f75684818aa2dce45240bf4

refactored a bit

RetailCoder.VBE/Rubberduck.csproj
@@ -129,6 +129,7 @@
     <Compile Include="VBA\Nodes\ProcedureNode.cs" />
     <Compile Include="VBA\Nodes\ParameterNode.cs" />
     <Compile Include="VBA\Nodes\TypeNode.cs" />
+    <Compile Include="VBA\ParseTreeListeners\ExposedMemberListener.cs" />
     <Compile Include="VBA\ParseTreeListeners\ProcedureNameListener.cs" />
     <Compile Include="VBA\ParseTreeListeners\ObsoleteInstructionsListener.cs" />
     <Compile Include="VBA\ParseTreeListeners\ModuleOptionsListener.cs" />
@@ -152,7 +153,7 @@
     <Compile Include="VBA\QualifiedContext.cs" />
     <Compile Include="VBA\RubberduckParser.cs" />
     <Compile Include="VBA\ParseTreeListeners\NodeBuildingListener.cs" />
-    <Compile Include="VBA\Grammar\IVBBaseListener.cs" />
+    <Compile Include="VBA\Grammar\VBListenerBase.cs" />
     <Compile Include="VBA\Grammar\IVBBaseVisitor.cs" />
     <Compile Include="VBA\Grammar\VBLexer.cs" />
     <Compile Include="VBA\Grammar\IVBListener.cs" />

RetailCoder.VBE/UI/Refactorings/ExtractMethod/ExtractMethodPresenter.cs
@@ -26,16 +26,16 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
 
         private readonly string _selectedCode;
         private readonly VBE _vbe;
-        private readonly Selection _selection;
+        private readonly QualifiedSelection _selection;
 
-        public ExtractMethodPresenter(VBE vbe, IExtractMethodDialog dialog, IParseTree parentMethod, Selection selection)
+        public ExtractMethodPresenter(VBE vbe, IExtractMethodDialog dialog, IParseTree parentMethod, QualifiedSelection selection)
         {
             _vbe = vbe;
             _selection = selection;
 
             _view = dialog;
             _parentMethodTree = parentMethod;
-            _selectedCode = vbe.ActiveCodePane.CodeModule.get_Lines(selection.StartLine, selection.LineCount);
+            _selectedCode = vbe.ActiveCodePane.CodeModule.get_Lines(selection.Selection.StartLine, selection.Selection.LineCount);
 
             _parentMethodDeclarations = ExtractMethodRefactoring.GetParentMethodDeclarations(parentMethod, selection);
 
@@ -112,8 +112,8 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
                 return;
             }
 
-            _vbe.ActiveCodePane.CodeModule.DeleteLines(_selection.StartLine, _selection.LineCount - 1);
-            _vbe.ActiveCodePane.CodeModule.ReplaceLine(_selection.StartLine, GetMethodCall());
+            _vbe.ActiveCodePane.CodeModule.DeleteLines(_selection.Selection.StartLine, _selection.Selection.LineCount - 1);
+            _vbe.ActiveCodePane.CodeModule.ReplaceLine(_selection.Selection.StartLine, GetMethodCall());
 
             _vbe.ActiveCodePane.CodeModule.AddFromString(GetExtractedMethod());
         }

RetailCoder.VBE/UI/Refactorings/ExtractMethod/ExtractMethodRefactoring.cs
@@ -11,9 +11,9 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
 {
     public class ExtractMethodRefactoring
     {
-        public static IDictionary<VBParser.AmbiguousIdentifierContext, ExtractedDeclarationUsage> GetParentMethodDeclarations(IParseTree parseTree, Selection selection)
+        public static IDictionary<VBParser.AmbiguousIdentifierContext, ExtractedDeclarationUsage> GetParentMethodDeclarations(IParseTree parseTree, QualifiedSelection selection)
         {
-            var declarations = parseTree.GetContexts<DeclarationListener, ParserRuleContext>(new DeclarationListener()).ToList();
+            var declarations = parseTree.GetContexts<DeclarationListener, ParserRuleContext>(new DeclarationListener(selection.QualifiedName)).ToList();
 
             var constants = declarations.OfType<VBParser.ConstSubStmtContext>().Select(constant => constant.AmbiguousIdentifier());
             var variables = declarations.OfType<VBParser.VariableSubStmtContext>().Select(variable => variable.AmbiguousIdentifier());
@@ -24,22 +24,22 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
                                        .ToDictionary(declaration => declaration.GetText(),
                                                      declaration => declaration);
 
-            var references = parseTree.GetContexts<VariableReferencesListener, VBParser.AmbiguousIdentifierContext>(new VariableReferencesListener())
-                                      .GroupBy(usage => new { Identifier = usage.GetText() })
+            var references = parseTree.GetContexts<VariableReferencesListener, VBParser.AmbiguousIdentifierContext>(new VariableReferencesListener(selection.QualifiedName))
+                                      .GroupBy(usage => new { Identifier = usage.Context.GetText() })
                                       .ToList();
 
-            var notUsedInSelection = references.Where(usage => usage.All(token => !selection.Contains(token.GetSelection())))
+            var notUsedInSelection = references.Where(usage => usage.All(token => !selection.Selection.Contains(token.Context.GetSelection())))
                                                .Select(usage => usage.Key).ToList();
 
-            var usedBeforeSelection = references.Where(usage => usage.Any(token => token.GetSelection().EndLine < selection.StartLine))
+            var usedBeforeSelection = references.Where(usage => usage.Any(token => token.Context.GetSelection().EndLine < selection.Selection.StartLine))
                                                     .Select(usage => usage.Key)
                                                 .Where(usage => notUsedInSelection.All(e => e.Identifier != usage.Identifier));
 
-            var usedAfterSelection = references.Where(usage => usage.Any(token => token.GetSelection().StartLine > selection.EndLine))
+            var usedAfterSelection = references.Where(usage => usage.Any(token => token.Context.GetSelection().StartLine > selection.Selection.EndLine))
                                                    .Select(usage => usage.Key)
                                                 .Where(usage => notUsedInSelection.All(e => e.Identifier != usage.Identifier));
 
-            var usedOnlyWithinSelection = references.Where(usage => usage.All(token => selection.Contains(token.GetSelection())))
+            var usedOnlyWithinSelection = references.Where(usage => usage.All(token => selection.Selection.Contains(token.Context.GetSelection())))
                                                     .Select(usage => usage.Key);
 
 

commit fdd586e79f9b723a933a8d30022863649d6a128b

fixed a bug introduced in 31a695c4 - an IEnumerable<QualifiedContext<TContext>> can't be filtered with .OfType<TContext> directly, must select the context first.

RetailCoder.VBE/Inspections/IdentifierUsageInspector.cs
@@ -44,6 +44,9 @@ namespace Rubberduck.Inspections
             }
         }
 
+        /// <summary>
+        /// Gets all global-scope fields that are not assigned in any standard or class module.
+        /// </summary>
         public IEnumerable<QualifiedContext<VBParser.AmbiguousIdentifierContext>> UnassignedGlobals()
         {
             var unassignedGlobals = _globals
@@ -61,6 +64,9 @@ namespace Rubberduck.Inspections
             }
         }
 
+        /// <summary>
+        /// Gets all module-scope fields that are not assigned.
+        /// </summary>
         public IEnumerable<QualifiedContext<VBParser.AmbiguousIdentifierContext>> UnassignedFields()
         {
             var unassignedFields = _fields
@@ -73,6 +79,9 @@ namespace Rubberduck.Inspections
             }
         }
 
+        /// <summary>
+        /// Gets all procedure-scope locals that are not assigned.
+        /// </summary>
         public IEnumerable<QualifiedContext<VBParser.AmbiguousIdentifierContext>> UnassignedLocals()
         {
             var unassignedFields = _locals
@@ -102,11 +111,6 @@ namespace Rubberduck.Inspections
                     .Where(declaration => IsGlobal(declaration.Visibility()))
                     .SelectMany(declaration => declaration.VariableListStmt().VariableSubStmt())
                     .Select(identifier => identifier.AmbiguousIdentifier().ToQualifiedContext(module.QualifiedName)));
-
-                result.AddRange(declarations.Select(declaration => declaration.Context)
-                                            .OfType<VBParser.TypeStmtContext>()
-                    .Where(declaration => IsGlobal(declaration.Visibility()))
-                    .Select(declaration => declaration.AmbiguousIdentifier().ToQualifiedContext(module.QualifiedName)));
             }
 
             return result;
@@ -159,22 +163,38 @@ namespace Rubberduck.Inspections
             {
                 var listener = new VariableAssignmentListener(module.QualifiedName);
                 result.AddRange(module.ParseTree
-                    .GetContexts<VariableAssignmentListener, VBParser.AmbiguousIdentifierContext>(listener));
+                    .GetContexts<VariableAssignmentListener, VBParser.AmbiguousIdentifierContext>(listener)
+                    .Where(identifier => !IsConstant(identifier.Context) && !IsJoinedAssignemntDeclaration(identifier.Context)));
             }
 
             return result;
         }
 
+        private static bool IsConstant(VBParser.AmbiguousIdentifierContext context)
+        {
+            return context.Parent.Parent.GetType() == typeof(VBParser.ConstSubStmtContext);
+        }
+
+        private static bool IsJoinedAssignemntDeclaration(VBParser.AmbiguousIdentifierContext context)
+        {
+            var declaration = context.Parent as VBParser.VariableSubStmtContext;
+            if (declaration == null)
+            {
+                return false;
+            }
+
+            var asTypeClause = declaration.AsTypeClause();
+            if (asTypeClause == null)
+            {
+                return false;
+            }
+
+            return asTypeClause.NEW() == null;
+        }
+
         private static bool IsGlobal(VBParser.VisibilityContext context)
         {
             return context != null && context.GetText() != Tokens.Private;
-        }
-
-        private static IEnumerable<VBParser.AmbiguousIdentifierContext> GetDeclaredIdentifiers(VBParser.VariableStmtContext context)
-        {
-            return context.VariableListStmt()
-                .VariableSubStmt()
-                .Select(variable => variable.AmbiguousIdentifier());
         }
     }
 }

RetailCoder.VBE/Inspections/VariableTypeNotDeclaredInspectionResult.cs
@@ -77,7 +77,7 @@ namespace Rubberduck.Inspections
                          + context.AmbiguousIdentifier().GetText() + ' '
                          + Tokens.As + ' ' + Tokens.Variant + ' '
                          + context.EQ().GetText() + ' '
-                         + context.valueStmt().GetText();
+                         + context.ValueStmt().GetText();
             return result;
         }
     }

RetailCoder.VBE/Rubberduck.csproj
@@ -151,6 +151,7 @@
     <Compile Include="VBA\ParseTreeListeners\ScopedDeclaration.cs" />
     <Compile Include="VBA\ParseTreeListeners\TreeViewListener.cs" />
     <Compile Include="VBA\ParseTreeListeners\VariableAssignmentListener.cs" />
+    <Compile Include="VBA\ParseTreeListeners\VariableUsageListener.cs" />
     <Compile Include="VBA\QualifiedContext.cs" />
     <Compile Include="VBA\RubberduckParser.cs" />
     <Compile Include="VBA\ParseTreeListeners\NodeBuildingListener.cs" />

RetailCoder.VBE/UI/Refactorings/ExtractMethod/ExtractMethodPresenter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -191,7 +190,7 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
 
             var localConsts = _locals.Select(e => e.Parent)
                 .OfType<VBParser.ConstSubStmtContext>()
-                .Select(e => "    " + Tokens.Const + ' ' + e.AmbiguousIdentifier().GetText() + ' ' + e.AsTypeClause().GetText() + " = " + e.valueStmt().GetText());
+                .Select(e => "    " + Tokens.Const + ' ' + e.AmbiguousIdentifier().GetText() + ' ' + e.AsTypeClause().GetText() + " = " + e.ValueStmt().GetText());
             var localVariables = _locals.Select(e => e.Parent)
                 .OfType<VBParser.VariableSubStmtContext>()
                 .Where(e => _view.Parameters.All(param => param.Name != e.AmbiguousIdentifier().GetText()))

RetailCoder.VBE/UI/Refactorings/ExtractMethod/ExtractMethodRefactoring.cs
@@ -15,9 +15,9 @@ namespace Rubberduck.UI.Refactorings.ExtractMethod
         {
             var declarations = parseTree.GetContexts<DeclarationListener, ParserRuleContext>(new DeclarationListener(selection.QualifiedName)).ToList();
 
-            var constants = declarations.OfType<VBParser.ConstSubStmtContext>().Select(constant => constant.AmbiguousIdentifier());
-            var variables = declarations.OfType<VBParser.VariableSubStmtContext>().Select(variable => variable.AmbiguousIdentifier());
-            var arguments = declarations.OfType<VBParser.ArgContext>().Select(arg => arg.AmbiguousIdentifier());
+            var constants = declarations.Select(d => d.Context).OfType<VBParser.ConstSubStmtContext>().Select(constant => constant.AmbiguousIdentifier());
+            var variables = declarations.Select(d => d.Context).OfType<VBParser.VariableSubStmtContext>().Select(variable => variable.AmbiguousIdentifier());
+            var arguments = declarations.Select(d => d.Context).OfType<VBParser.ArgContext>().Select(arg => arg.AmbiguousIdentifier());
 
             var identifiers = constants.Union(variables)
                                        .Union(arguments)

RetailCoder.VBE/VBA/Grammar/VBParser.cs
@@ -2577,7 +2577,7 @@ public partial class VBParser : Parser {
 			return GetRuleContext<AsTypeClauseContext>(0);
 		}
 		public IReadOnlyList<ITerminalNode> WS() { return GetTokens(VBParser.WS); }
-		public ValueStmtContext valueStmt() {
+		public ValueStmtContext ValueStmt() {
 			return GetRuleContext<ValueStmtContext>(0);
 		}
 		public AmbiguousIdentifierContext AmbiguousIdentifier() {

RetailCoder.VBE/VBA/ParseTreeListeners/NodeBuildingListener.cs
@@ -2,7 +2,152 @@
 using Rubberduck.VBA.Grammar;
 using Rubberduck.VBA.Nodes;
 
-namespace Rubberduck.VBA
+namespace Rubberduck.VBA.ParseTreeListeners
 {
+    public class NodeBuildingListener : VBListenerBase
+    {
+        private readonly string _project;
+        private readonly string _module;
+        private readonly IList<Node> _members = new List<Node>();
 
+        private string _currentScope;
+        private Node _currentNode;
+
+        public NodeBuildingListener(string project, string module)
+        {
+            _project = project;
+            _module = module;
+            _currentScope = project + "." + module;
+        }
+
+        public Node Root
+        {
+            get { return new ModuleNode(null, _project, _module, _members); }
+        }
+
+        private void AddCurrentMember()
+        {
+            _members.Add(_currentNode);
+            _currentNode = null;
+        }
+
+        public override void EnterSubStmt(VBParser.SubStmtContext context)
+        {
+            _currentNode = CreateProcedureNode(context);
+        }
+
+        public override void EnterFunctionStmt(VBParser.FunctionStmtContext context)
+        {
+            _currentNode = CreateProcedureNode(context);
+        }
+
+        public override void EnterPropertyGetStmt(VBParser.PropertyGetStmtContext context)
+        {
+            _currentNode = CreateProcedureNode(context);
+        }
+
+        public override void EnterPropertyLetStmt(VBParser.PropertyLetStmtContext context)
+        {
+            _currentNode = CreateProcedureNode(context);
+        }
+
+        public override void EnterPropertySetStmt(VBParser.PropertySetStmtContext context)
+        {
+            _currentNode = CreateProcedureNode(context);
+        }
+
+        private ProcedureNode CreateProcedureNode(dynamic context)
+        {
+            var procedureName = context.ambiguousIdentifier().GetText();
+            var node = new ProcedureNode(context, _currentScope, procedureName);
+
+            var args = context.argList().arg() as IReadOnlyList<VBParser.ArgContext>;
+            if (args != null)
+            {
+                foreach (var arg in args)
+                {
+                    node.AddChild(new ParameterNode(arg, _currentScope));
+                }
+            }
+
+            _currentScope = _project + "." + _module + "." + node.Name;
+            return node;
+        }
+
+        public override void ExitOptionExplicitStmt(VBParser.OptionExplicitStmtContext context)
+        {
+            _members.Add(new OptionNode(context, _currentScope));
+        }
+
+        public override void ExitOptionBaseStmt(VBParser.OptionBaseStmtContext context)
+        {
+            _members.Add(new OptionNode(context, _currentScope));
+        }
+
+        public override void ExitOptionCompareStmt(VBParser.OptionCompareStmtContext context)
+        {
+            _members.Add(new OptionNode(context, _currentScope));
+        }
+
+        public override void ExitEnumerationStmt(VBParser.EnumerationStmtContext context)
+        {
+            _members.Add(new EnumNode(context, _currentScope));
+        }
+
+        public override void ExitSubStmt(VBParser.SubStmtContext context)
+        {
+            AddCurrentMember();
+        }
+
+        public override void ExitFunctionStmt(VBParser.FunctionStmtContext context)
+        {
+            AddCurrentMember();
+        }
+
+        public override void ExitPropertyGetStmt(VBParser.PropertyGetStmtContext context)
+        {
+            AddCurrentMember();
+        }
+
+        public override void ExitPropertyLetStmt(VBParser.PropertyLetStmtContext context)
+        {
+            AddCurrentMember();
+        }
+
+        public override void ExitPropertySetStmt(VBParser.PropertySetStmtContext context)
+        {
+            AddCurrentMember();
+        }
+
+        public override void ExitVariableStmt(VBParser.VariableStmtContext context)
+        {
+            var node = new VariableDeclarationNode(context, _currentScope);
+            if (_currentNode == null)
+            {
+                _members.Add(node);
+            }
+            else
+            {
+                _currentNode.AddChild(node);
+            }
+        }
+
+        public override void ExitConstStmt(VBParser.ConstStmtContext context)
+        {
+            var node = new ConstDeclarationNode(context, _currentScope);
+            if (_currentNode == null)
+            {
+                _members.Add(node);
+            }
+            else
+            {
+                _currentNode.AddChild(node);
+            }
+        }
+
+        public override void ExitTypeStmt(VBParser.TypeStmtContext context)
+        {
+            _members.Add(new TypeNode(context, _currentScope));
+        }
+    }
 }

RetailCoder.VBE/VBA/ParseTreeListeners/VariableAssignmentListener.cs
@@ -1,158 +1,8 @@
-using System.Collections.Generic;
-using System.Linq;
 using Rubberduck.Inspections;
 using Rubberduck.VBA.Grammar;
-using Rubberduck.VBA.Nodes;
 
 namespace Rubberduck.VBA.ParseTreeListeners
 {
-    public class NodeBuildingListener : VBListenerBase
-    {
-        private readonly string _project;
-        private readonly string _module;
-        private readonly IList<Node> _members = new List<Node>();
-
-        private string _currentScope;
-        private Node _currentNode;
-
-        public NodeBuildingListener(string project, string module)
-        {
-            _project = project;
-            _module = module;
-            _currentScope = project + "." + module;
-        }
-
-        public Node Root
-        {
-            get { return new ModuleNode(null, _project, _module, _members); }
-        }
-
-        private void AddCurrentMember()
-        {
-            _members.Add(_currentNode);
-            _currentNode = null;
-        }
-
-        public override void EnterSubStmt(VBParser.SubStmtContext context)
-        {
-            _currentNode = CreateProcedureNode(context);
-        }
-
-        public override void EnterFunctionStmt(VBParser.FunctionStmtContext context)
-        {
-            _currentNode = CreateProcedureNode(context);
-        }
-
-        public override void EnterPropertyGetStmt(VBParser.PropertyGetStmtContext context)
-        {
-            _currentNode = CreateProcedureNode(context);
-        }
-
-        public override void EnterPropertyLetStmt(VBParser.PropertyLetStmtContext context)
-        {
-            _currentNode = CreateProcedureNode(context);
-        }
-
-        public override void EnterPropertySetStmt(VBParser.PropertySetStmtContext context)
-        {
-            _currentNode = CreateProcedureNode(context);
-        }
-
-        private ProcedureNode CreateProcedureNode(dynamic context)
-        {
-            var procedureName = context.ambiguousIdentifier().GetText();
-            var node = new ProcedureNode(context, _currentScope, procedureName);
-
-            var args = context.argList().arg() as IReadOnlyList<VBParser.ArgContext>;
-            if (args != null)
-            {
-                foreach (var arg in args)
-                {
-                    node.AddChild(new ParameterNode(arg, _currentScope));
-                }
-            }
-
-            _currentScope = _project + "." + _module + "." + node.Name;
-            return node;
-        }
-
-        public override void ExitOptionExplicitStmt(VBParser.OptionExplicitStmtContext context)
-        {
-            _members.Add(new OptionNode(context, _currentScope));
-        }
-
-        public override void ExitOptionBaseStmt(VBParser.OptionBaseStmtContext context)
-        {
-            _members.Add(new OptionNode(context, _currentScope));
-        }
-
-        public override void ExitOptionCompareStmt(VBParser.OptionCompareStmtContext context)
-        {
-            _members.Add(new OptionNode(context, _currentScope));
-        }
-
-        public override void ExitEnumerationStmt(VBParser.EnumerationStmtContext context)
-        {
-            _members.Add(new EnumNode(context, _currentScope));
-        }
-
-        public override void ExitSubStmt(VBParser.SubStmtContext context)
-        {
-            AddCurrentMember();
-        }
-
-        public override void ExitFunctionStmt(VBParser.FunctionStmtContext context)
-        {
-            AddCurrentMember();
-        }
-
-        public override void ExitPropertyGetStmt(VBParser.PropertyGetStmtContext context)
-        {
-            AddCurrentMember();
-        }
-
-        public override void ExitPropertyLetStmt(VBParser.PropertyLetStmtContext context)
-        {
-            AddCurrentMember();
-        }
-
-        public override void ExitPropertySetStmt(VBParser.PropertySetStmtContext context)
-        {
-            AddCurrentMember();
-        }
-
-        public override void ExitVariableStmt(VBParser.VariableStmtContext context)
-        {
-            var node = new VariableDeclarationNode(context, _currentScope);
-            if (_currentNode == null)
-            {
-                _members.Add(node);
-            }
-            else
-            {
-                _currentNode.AddChild(node);
-            }
-        }
-
-        public override void ExitConstStmt(VBParser.ConstStmtContext context)
-        {
-            var node = new ConstDeclarationNode(context, _currentScope);
-            if (_currentNode == null)
-            {
-                _members.Add(node);
-            }
-            else
-            {
-                _currentNode.AddChild(node);
-            }
-        }
-
-        public override void ExitTypeStmt(VBParser.TypeStmtContext context)
-        {
-            _members.Add(new TypeNode(context, _currentScope));
-        }
-    }
-    
     public class VariableAssignmentListener : VariableUsageListener
     {
         public override void EnterVariableCallStmt(VBParser.VariableCallStmtContext context)
@@ -166,36 +16,6 @@ namespace Rubberduck.VBA.ParseTreeListeners
         public VariableAssignmentListener(QualifiedModuleName qualifiedName) 
             : base(qualifiedName)
         {
-        }
-    }
-
-    public class VariableUsageListener : VBListenerBase, IExtensionListener<VBParser.AmbiguousIdentifierContext>
-    {
-        private readonly IList<QualifiedContext<VBParser.AmbiguousIdentifierContext>> _members = 
-            new List<QualifiedContext<VBParser.AmbiguousIdentifierContext>>();
-
-        private readonly QualifiedModuleName _qualifiedName;
-
-        public VariableUsageListener(QualifiedModuleName qualifiedName)
-        {
-            _qualifiedName = qualifiedName;
-        }
-
-        public IEnumerable<QualifiedContext<VBParser.AmbiguousIdentifierContext>> Members { get { return _members; } }
-
-        public override void EnterForNextStmt(VBParser.ForNextStmtContext context)
-        {
-            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.AmbiguousIdentifier().First()));
-        }
-
-        public override void EnterVariableCallStmt(VBParser.VariableCallStmtContext context)
-        {
-            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.AmbiguousIdentifier()));
-        }
-
-        public override void EnterWithStmt(VBParser.WithStmtContext context)
-        {
-            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.ImplicitCallStmt_InStmt().ICS_S_VariableCall().VariableCallStmt().AmbiguousIdentifier()));
         }
     }
 }

RetailCoder.VBE/VBA/ParseTreeListeners/VariableUsageListener.cs
@@ -1,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Rubberduck.Inspections;
+using Rubberduck.VBA.Grammar;
+
+namespace Rubberduck.VBA.ParseTreeListeners
+{
+    public class VariableUsageListener : VBListenerBase, IExtensionListener<VBParser.AmbiguousIdentifierContext>
+    {
+        private readonly IList<QualifiedContext<VBParser.AmbiguousIdentifierContext>> _members = 
+            new List<QualifiedContext<VBParser.AmbiguousIdentifierContext>>();
+
+        private readonly QualifiedModuleName _qualifiedName;
+
+        public VariableUsageListener(QualifiedModuleName qualifiedName)
+        {
+            _qualifiedName = qualifiedName;
+        }
+
+        public IEnumerable<QualifiedContext<VBParser.AmbiguousIdentifierContext>> Members { get { return _members; } }
+
+        public override void EnterForNextStmt(VBParser.ForNextStmtContext context)
+        {
+            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.AmbiguousIdentifier().First()));
+        }
+
+        public override void EnterVariableCallStmt(VBParser.VariableCallStmtContext context)
+        {
+            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.AmbiguousIdentifier()));
+        }
+
+        public override void EnterWithStmt(VBParser.WithStmtContext context)
+        {
+            _members.Add(new QualifiedContext<VBParser.AmbiguousIdentifierContext>(_qualifiedName, context.ImplicitCallStmt_InStmt().ICS_S_VariableCall().VariableCallStmt().AmbiguousIdentifier()));
+        }
+    }
+}