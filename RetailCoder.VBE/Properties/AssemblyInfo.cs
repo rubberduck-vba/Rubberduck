162bd498f82e0fa8eb03212b6d6ce8abf1a4d79a
Christopher J. McClellan <ckuhn203@gmail.com>
2015-02-25 12:05:35 -0500

    Corrected Assembly Description

--- RetailCoder.VBE/Properties/AssemblyInfo.cs
+++ RetailCoder.VBE/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rubberduck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rubberduck VBA Editor Add-In")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rubberduck")]
