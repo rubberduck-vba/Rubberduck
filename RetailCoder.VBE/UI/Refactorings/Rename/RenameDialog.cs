9496eeb8f516b52b07ce110420062e52806a1f30
retailcoder <retailcoder@gmail.com>
2015-03-24 22:16:50 -0400

    added RenameDialog

---
+++ RetailCoder.VBE/UI/Refactorings/Rename/RenameDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rubberduck.UI.Refactorings.Rename
+{
+    public partial class RenameDialog : Form
+    {
+        public RenameDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
