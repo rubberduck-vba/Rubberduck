dc7f2ea63f01dc5a61e74488dc5fb3f89c8c2f83
Hosch250 <abrahamhorsch@gmail.com>
2015-04-19 16:22:28 -0500

    Update AboutWindow.cs

--- RetailCoder.VBE/UI/AboutWindow.cs
+++ RetailCoder.VBE/UI/AboutWindow.cs
@@ -1,58 +1,75 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Rubberduck.UI
 {
-    public partial class AboutWindow : Form
+    [ComVisible(true)]
+    [Guid(ClassId)]
+    [ProgId(ProgId)]
+    public partial class _AboutWindow : Form
     {
-        public AboutWindow()
+        public const string ClassId = "939CC8BB-A8CA-3BE6-89A3-5450949A6A43";
+        public const string ProgId = "Rubberduck.UI._AboutWindow";
+
+        private static readonly IDictionary<string, string> Links =
+            new Dictionary<string, string>
+            {
+                {"Home","http://www.rubberduck-vba.com"},
+                {"GitHub", "http://www.github.com/retailcoder/rubberduck"},
+                {"Twitter","http://www.twitter.com/rubberduckvba"},
+                {"Facebook", "http://www.facebook.com/rubberduckvba"},
+                {"Google+", "http://plus.google.com/116859653258584466987"}
+            };
+
+        public _AboutWindow()
         {
             InitializeComponent();
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var assembly = Assembly.GetExecutingAssembly();
             var name = assembly.GetName();
 
-            titleLabel.Text = name.Name;
-            versionLabel.Text = name.Version.ToString();
-
-            repositoryLinkLabel.LinkClicked += repositoryLinkLabel_LinkClicked;
-            retailcoderLinkLabel.LinkClicked += retailcoderLinkLabel_LinkClicked;
-            ckuhn203LinkLabel.LinkClicked += ckuhn203LinkLabel_LinkClicked;
-
-            codereviewLinkLabel.LinkClicked += codereviewLinkLabel_LinkClicked;
-            mztoolsLinkLabel.LinkClicked += mztoolsLinkLabel_LinkClicked;
-
+            AppVersionLabel.Text = string.Format("Build {0} ({1})", name.Version, name.ProcessorArchitecture);
             CloseButton.Click += CloseButton_Click;
+
+            OfficialWebsiteLinkLabel.LinkClicked += OfficialWebsiteLinkLabel_LinkClicked;
+            GitHubIcon.Click += GitHubIcon_Click;
+            TwitterIcon.Click += TwitterIcon_Click;
+            FacebookIcon.Click += FacebookIcon_Click;
+            GooglePlusIcon.Click += GooglePlusIcon_Click;
         }
 
-        void CloseButton_Click(object sender, EventArgs e)
+        private void GooglePlusIcon_Click(object sender, EventArgs e)
+        {
+            VisitLink(Links["Google+"]);
+        }
+
+        private void FacebookIcon_Click(object sender, EventArgs e)
+        {
+            VisitLink(Links["Facebook"]);
+        }
+
+        private void TwitterIcon_Click(object sender, EventArgs e)
+        {
+            VisitLink(Links["Twitter"]);
+        }
+
+        private void GitHubIcon_Click(object sender, EventArgs e)
+        {
+            VisitLink(Links["GitHub"]);
+        }
+
+        private void OfficialWebsiteLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            VisitLink(Links["Home"]);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
         {
             Close();
-        }
-
-        void mztoolsLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitLink("http://www.mztools.com/articles/2006/mz2006007.aspx");
-        }
-
-        void codereviewLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitLink("http://www.codereview.stackexchange.com");
-        }
-
-        void ckuhn203LinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitLink("https://github.com/ckuhn203");
-        }
-
-        void retailcoderLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitLink("https://github.com/retailcoder");
-        }
-
-        void repositoryLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            VisitLink("https://github.com/retailcoder/Rubberduck");
         }
 
         private void VisitLink(string url)
