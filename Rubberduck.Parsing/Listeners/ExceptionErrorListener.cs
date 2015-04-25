0377f7c82a9daa48a7c8dd3c89412df296c3cf69
retailcoder <retailcoder@gmail.com>
2015-04-25 14:06:28 -0400

    Added ExceptionErrorListener class

---
+++ Rubberduck.Parsing/Listeners/ExceptionErrorListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace Rubberduck.Parsing.Listeners
+{
+    public class ExceptionErrorListener : BaseErrorListener
+    {
+        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            var message = string.Format("A RecognitionException was thrown in the lexer. line {0}, position {1}. Message: {2}", line, charPositionInLine, msg);
+            throw new ArgumentException(message, e);
+        }
+    }
+}
