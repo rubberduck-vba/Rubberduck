1a525e3483211a864e2be98103b1f2604939e7ed
Christopher J. McClellan <ckuhn203@gmail.com>
2015-05-24 08:04:46 -0400

    Added ISettingsView

--- RetailCoder.VBE/UI/SourceControl/ISettingsView.cs
+++ RetailCoder.VBE/UI/SourceControl/ISettingsView.cs
@@ -1,6 +1,17 @@
-﻿namespace Rubberduck.UI.SourceControl
+﻿using System;
+
+namespace Rubberduck.UI.SourceControl
 {
     public interface ISettingsView
     {
+        string UserName { get; set; }
+        string EmailAddress { get; set; }
+        string DefaultRepositoryLocation { get; set; }
+
+        event EventHandler<EventArgs> SelectDefaultRepositoryLocation; 
+        event EventHandler<EventArgs> Save;
+        event EventHandler<EventArgs> Cancel; 
+        event EventHandler<EventArgs> EditIgnoreFile;
+        event EventHandler<EventArgs> EditAttributesFile;
     }
 }
