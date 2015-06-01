4d1a0a334c57b056153f3a09aa7014e0e900ce63
hosch250 <hosch250@live.com>
2015-06-01 16:44:21 -0500

    Load config on change

--- RetailCoder.VBE/App.cs
+++ RetailCoder.VBE/App.cs
@@ -20,26 +20,40 @@ namespace Rubberduck
         private readonly IList<IInspection> _inspections;
         private readonly Inspector _inspector;
         private readonly ParserErrorsPresenter _parserErrorsPresenter;
+        private readonly IGeneralConfigService _configService = new ConfigurationLoader();
+
+        private Configuration _config;
 
         public App(VBE vbe, AddIn addIn)
         {
-            IGeneralConfigService configService = new ConfigurationLoader();
-            _inspections = configService.GetImplementedCodeInspections();
+            _inspections = _configService.GetImplementedCodeInspections();
 
-            var config = configService.LoadConfiguration();
-            RubberduckUI.Culture = CultureInfo.GetCultureInfo(config.UserSettings.LanguageSetting.Code);
+            LoadConfig();
 
-            EnableCodeInspections(config);
             IRubberduckParser parser = new RubberduckParser();
             _parserErrorsPresenter = new ParserErrorsPresenter(vbe, addIn);
             parser.ParseStarted += _parser_ParseStarted;
             parser.ParserError += _parser_ParserError;
+            _configService.SettingsChanged += _configService_SettingsChanged;
 
             var editor = new ActiveCodePaneEditor(vbe);
 
             _inspector = new Inspector(parser, _inspections);
-            _menu = new RubberduckMenu(vbe, addIn, configService, parser, editor, _inspector);
+            _menu = new RubberduckMenu(vbe, addIn, _configService, parser, editor, _inspector);
             _codeInspectionsToolbar = new CodeInspectionsToolbar(vbe, _inspector);
+        }
+
+        private void _configService_SettingsChanged(object sender, EventArgs e)
+        {
+            LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
+            _config = _configService.LoadConfiguration();
+            RubberduckUI.Culture = CultureInfo.GetCultureInfo(_config.UserSettings.LanguageSetting.Code);
+
+            EnableCodeInspections(_config);
         }
 
         private void _parser_ParseStarted(object sender, ParseStartedEventArgs e)
