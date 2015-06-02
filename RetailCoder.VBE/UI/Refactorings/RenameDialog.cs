3dc8cf09e3cbdd97477df8a8845d15fb17016e08
hosch250 <hosch250@live.com>
2015-06-02 11:45:15 -0500

    Partial fix for Rename refactoring

--- RetailCoder.VBE/UI/Refactorings/RenameDialog.cs
+++ RetailCoder.VBE/UI/Refactorings/RenameDialog.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
+using Rubberduck.Parsing.Grammar;
 using Rubberduck.Parsing.Symbols;
 using Rubberduck.Refactorings.Rename;
 
@@ -19,7 +21,8 @@ namespace Rubberduck.UI.Refactorings
 
         private void InitializeCaptions()
         {
-            Text = RubberduckUI.RenameDialog_Caption; OkButton.Text = RubberduckUI.OK;
+            Text = RubberduckUI.RenameDialog_Caption;
+            OkButton.Text = RubberduckUI.OK;
             CancelButton.Text = RubberduckUI.CancelButtonText;
             TitleLabel.Text = RubberduckUI.RenameDialog_TitleText;
             InstructionsLabel.Text = RubberduckUI.RenameDialog_InstructionsLabelText;
@@ -43,12 +46,14 @@ namespace Rubberduck.UI.Refactorings
         }
 
         public event EventHandler CancelButtonClicked;
+
         public void OnCancelButtonClicked()
         {
             Hide();
         }
 
         public event EventHandler OkButtonClicked;
+
         public void OnOkButtonClicked()
         {
             var handler = OkButtonClicked;
@@ -72,8 +77,10 @@ namespace Rubberduck.UI.Refactorings
                 }
 
                 NewName = _target.IdentifierName;
-                var declarationType = RubberduckUI.ResourceManager.GetString("DeclarationType_" + _target.DeclarationType);
-                InstructionsLabel.Text = string.Format(RubberduckUI.RenameDialog_InstructionsLabelText, declarationType, _target.IdentifierName);
+                var declarationType =
+                    RubberduckUI.ResourceManager.GetString("DeclarationType_" + _target.DeclarationType);
+                InstructionsLabel.Text = string.Format(RubberduckUI.RenameDialog_InstructionsLabelText, declarationType,
+                    _target.IdentifierName);
             }
         }
 
@@ -89,9 +96,13 @@ namespace Rubberduck.UI.Refactorings
 
         private void ValidateNewName()
         {
-            OkButton.Enabled = (NewName != Target.IdentifierName) 
-                && !string.IsNullOrWhiteSpace(NewName)
-                && !NewName.StartsWith("_"); // also invalid if it starts with a number...
+            Tokens.ResourceManager.IgnoreCase = true;
+
+            OkButton.Enabled = (NewName != Target.IdentifierName)
+                               && char.IsLetter(NewName.FirstOrDefault())
+                               && Tokens.ResourceManager.GetString(NewName) == null;
+
+            Tokens.ResourceManager.IgnoreCase = false;
 
             InvalidNameValidationIcon.Visible = !OkButton.Enabled;
         }
