11f168d7ecfd356e54ad02e351b701101ab73600
retailcoder <retailcoder@gmail.com>
2015-06-02 01:17:35 -0400

    localized reset button

--- RetailCoder.VBE/UI/Settings/GeneralSettingsControl.Designer.cs
+++ RetailCoder.VBE/UI/Settings/GeneralSettingsControl.Designer.cs
@@ -45,9 +45,8 @@ namespace Rubberduck.UI.Settings
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = global::Rubberduck.Properties.Resources.Rubberduck;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(533, 96);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 78);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -56,32 +55,29 @@ namespace Rubberduck.UI.Settings
             // 
             this.TitleLabel.AutoSize = true;
             this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleLabel.Location = new System.Drawing.Point(4, 100);
-            this.TitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleLabel.Location = new System.Drawing.Point(3, 81);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.TitleLabel.Size = new System.Drawing.Size(139, 22);
+            this.TitleLabel.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TitleLabel.Size = new System.Drawing.Size(118, 19);
             this.TitleLabel.TabIndex = 4;
             this.TitleLabel.Text = "General Settings";
             // 
             // InstructionsLabel
             // 
             this.InstructionsLabel.AutoSize = true;
-            this.InstructionsLabel.Location = new System.Drawing.Point(4, 123);
-            this.InstructionsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.InstructionsLabel.MaximumSize = new System.Drawing.Size(467, 0);
+            this.InstructionsLabel.Location = new System.Drawing.Point(3, 100);
+            this.InstructionsLabel.MaximumSize = new System.Drawing.Size(350, 0);
             this.InstructionsLabel.Name = "InstructionsLabel";
-            this.InstructionsLabel.Padding = new System.Windows.Forms.Padding(5);
-            this.InstructionsLabel.Size = new System.Drawing.Size(10, 27);
+            this.InstructionsLabel.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.InstructionsLabel.Size = new System.Drawing.Size(8, 21);
             this.InstructionsLabel.TabIndex = 5;
             // 
             // LanguageLabel
             // 
             this.LanguageLabel.AutoSize = true;
-            this.LanguageLabel.Location = new System.Drawing.Point(19, 198);
-            this.LanguageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LanguageLabel.Location = new System.Drawing.Point(14, 161);
             this.LanguageLabel.Name = "LanguageLabel";
-            this.LanguageLabel.Size = new System.Drawing.Size(76, 17);
+            this.LanguageLabel.Size = new System.Drawing.Size(58, 13);
             this.LanguageLabel.TabIndex = 6;
             this.LanguageLabel.Text = "Language:";
             // 
@@ -89,26 +85,26 @@ namespace Rubberduck.UI.Settings
             // 
             this.LanguageList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.LanguageList.FormattingEnabled = true;
-            this.LanguageList.Location = new System.Drawing.Point(23, 219);
-            this.LanguageList.Margin = new System.Windows.Forms.Padding(4);
+            this.LanguageList.Location = new System.Drawing.Point(17, 178);
             this.LanguageList.Name = "LanguageList";
-            this.LanguageList.Size = new System.Drawing.Size(207, 24);
+            this.LanguageList.Size = new System.Drawing.Size(156, 21);
             this.LanguageList.TabIndex = 7;
             // 
             // resetSettings
             // 
             this.resetSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.resetSettings.ForeColor = System.Drawing.Color.Black;
-            this.resetSettings.Location = new System.Drawing.Point(23, 299);
+            this.resetSettings.Location = new System.Drawing.Point(17, 243);
+            this.resetSettings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.resetSettings.Name = "resetSettings";
-            this.resetSettings.Size = new System.Drawing.Size(120, 42);
+            this.resetSettings.Size = new System.Drawing.Size(90, 34);
             this.resetSettings.TabIndex = 8;
-            this.resetSettings.Text = "Reset Settings";
+            this.resetSettings.Text = "Reset";
             this.resetSettings.UseVisualStyleBackColor = false;
             // 
             // GeneralSettingsControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.resetSettings);
@@ -117,10 +113,9 @@ namespace Rubberduck.UI.Settings
             this.Controls.Add(this.TitleLabel);
             this.Controls.Add(this.InstructionsLabel);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimumSize = new System.Drawing.Size(533, 492);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
             this.Name = "GeneralSettingsControl";
-            this.Size = new System.Drawing.Size(533, 492);
+            this.Size = new System.Drawing.Size(400, 400);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
