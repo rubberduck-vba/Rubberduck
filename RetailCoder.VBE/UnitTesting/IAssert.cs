b7c2b7b397e804b334e953dbc0cfca4f462b11eb
retailcoder <retailcoder@gmail.com>
2015-06-07 01:15:15 -0400

    folded UnitTesting back into Rubberduck main assembly. @ckuhn203 will want to rip my head off for that one, but this simplifies COM registration, is known to have worked for RD users for 13 releases, and has the side-effect of working on my machine. Consider this a "temp fix"... whatever that means.

---
+++ RetailCoder.VBE/UnitTesting/IAssert.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Rubberduck.UnitTesting
+{
+    [ComVisible(true)]
+    public interface IAssert
+    {
+        /// <summary>
+        /// Verifies that the specified condition is <c>true</c>. The assertion fails if the condition is <c>false</c>.
+        /// </summary>
+        /// <param name="condition">Any Boolean value or expression.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void IsTrue(bool condition, string message = null);
+
+        /// <summary>
+        /// Verifies that the specified condition is <c>false</c>. The assertion fails if the condition is <c>true</c>.
+        /// </summary>
+        /// <param name="condition">Any Boolean value or expression.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void IsFalse(bool condition, string message = null);
+
+        /// <summary>
+        /// Indicates that the assertion cannot be verified.
+        /// </summary>
+        /// <param name="message">An optional message to display.</param>
+        void Inconclusive(string message = null);
+
+        /// <summary>
+        /// Fails the assertion without checking any conditions.
+        /// </summary>
+        /// <param name="message">An optional message to display.</param>
+        void Fail(string message = null);
+
+        /// <summary>
+        /// Verifies that the specified object is <c>Nothing</c>. The assertion fails if it is not <c>Nothing</c>.
+        /// </summary>
+        /// <param name="value">The object to verify.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void IsNothing(object value, string message = null);
+
+        /// <summary>
+        /// Verifies that the specified object is not <c>Nothing</c>. The assertion fails if it is <c>Nothing</c>.
+        /// </summary>
+        /// <param name="value">The object to verify.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void IsNotNothing(object value, string message = null);
+
+        /// <summary>
+        /// Verifies that two specified objects are equal. The assertion fails if the objects are not equal.
+        /// </summary>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        /// <remarks>
+        /// <paramref name="expected"/> and <paramref name="actual"/> must be the same type.
+        /// </remarks>
+        void AreEqual(object expected, object actual, string message = null);
+
+        /// <summary>
+        /// Verifies that two specified objects are not equal. The assertion fails if the objects are equal.
+        /// </summary>
+        /// <param name="expected">The expected value.</param>
+        /// <param name="actual">The actual value.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        /// <remarks>
+        /// <paramref name="expected"/> and <paramref name="actual"/> must be the same type.
+        /// </remarks>
+        void AreNotEqual(object expected, object actual, string message = null);
+
+        /// <summary>
+        /// Verifies that two specified object variables refer to the same object. The assertion fails if they refer to different objects.
+        /// </summary>
+        /// <param name="expected">The expected reference.</param>
+        /// <param name="actual">The actual reference.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void AreSame(object expected, object actual, string message = null);
+
+        /// <summary>
+        /// Verifies that two specified object variables refer to different objects. The assertion fails if they refer to the same object.
+        /// </summary>
+        /// <param name="expected">The expected reference.</param>
+        /// <param name="actual">The actual reference.</param>
+        /// <param name="message">An optional message to display if the assertion fails.</param>
+        void AreNotSame(object expected, object actual, string message = null);
+    }
+}
---
+++ RetailCoder.VBE/UnitTesting/NewTestMethodCommand.cs
@@ -0,0 +1,80 @@
+using System.Linq;
+using Microsoft.Vbe.Interop;
+using Rubberduck.Parsing;
+using Rubberduck.Reflection;
+
+namespace Rubberduck.UnitTesting
+{
+    public static class NewTestMethodCommand
+    {
+        private static readonly string NamePlaceholder = "%METHODNAME%";
+        private static readonly string TestMethodBaseName = "TestMethod";
+
+        private static readonly string TestMethodTemplate = string.Concat(
+            "'@TestMethod\n",
+            "Public Sub ", NamePlaceholder, "() 'TODO: Rename test\n",
+            "    On Error GoTo TestFail\n",
+            "    \n",
+            "    'Arrange:\n\n",
+            "    'Act:\n\n",
+            "    'Assert:\n",
+            "    Assert.Inconclusive\n\n",
+            "TestExit:\n",
+            "    Exit Sub\n",
+            "TestFail:\n",
+            "    Assert.Fail \"Test raised an error: #\" & Err.Number & \" - \" & Err.Description\n",
+            "End Sub\n"
+            );
+
+        private static readonly string TestMethodExpectedErrorTemplate = string.Concat(
+            "'@TestMethod\n",
+            "Public Sub ", NamePlaceholder, "() 'TODO: Rename test\n",
+            "    Const ExpectedError As Long = 0 'TODO: Change to expected error number\n",
+            "    On Error GoTo TestFail\n",
+            "    \n",
+            "    'Arrange:\n\n",
+            "    'Act:\n\n",
+            "Assert:\n",
+            "    Assert.Fail \"Expected error was not raised.\"\n\n",
+            "TestExit:\n",
+            "    Exit Sub\n",
+            "TestFail:\n",
+            "    If Err.Number = ExpectedError Then\n",
+            "        Resume TestExit\n",
+            "    Else\n",
+            "        Resume Assert\n",
+            "    End If\n",
+            "End Sub\n"
+            );
+
+        public static void NewTestMethod(VBE vbe)
+        {
+            if (vbe.ActiveCodePane.CodeModule.HasAttribute<TestModuleAttribute>())
+            {
+                var module = vbe.ActiveCodePane.CodeModule;
+                var name = GetNextTestMethodName(module.Parent);
+                var method = TestMethodTemplate.Replace(NamePlaceholder, name);
+                module.InsertLines(module.CountOfLines, method);
+            }
+        }
+
+        public static void NewExpectedErrorTestMethod(VBE vbe)
+        {
+            if (vbe.ActiveCodePane.CodeModule.HasAttribute<TestModuleAttribute>())
+            {
+                var module = vbe.ActiveCodePane.CodeModule;
+                var name = GetNextTestMethodName(module.Parent);
+                var method = TestMethodExpectedErrorTemplate.Replace(NamePlaceholder, name);
+                module.InsertLines(module.CountOfLines, method);
+            }
+        }
+
+        private static string GetNextTestMethodName(VBComponent component)
+        {
+            var names = component.TestMethods().Select(test => test.QualifiedMemberName.MemberName);
+            var index = names.Count(n => n.StartsWith(TestMethodBaseName)) + 1;
+
+            return string.Concat(TestMethodBaseName, index);
+        }
+    }
+}
