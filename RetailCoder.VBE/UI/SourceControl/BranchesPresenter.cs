db4f29efabb0277a5cbe6d777a72eac2714f2f2a
hosch250 <hosch250@live.com>
2015-06-09 23:03:43 -0500

    Remove unneeded, problematic code

--- RetailCoder.VBE/UI/SourceControl/BranchesPresenter.cs
+++ RetailCoder.VBE/UI/SourceControl/BranchesPresenter.cs
@@ -86,8 +86,6 @@ namespace Rubberduck.UI.SourceControl
 
         public void RefreshView()
         {
-            if (_view.Local == null) { return; }
-
             _view.SelectedBranchChanged -= OnSelectedBranchChanged;
 
             _view.Local = this.Provider.Branches.Where(b => !b.IsRemote).Select(b => b.Name).ToList();
