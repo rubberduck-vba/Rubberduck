7a1acec52f48f2216a159e596e64c66aa6df30d6
hosch250 <hosch250@live.com>
2015-06-10 11:26:41 -0500

    Revert "Closes #604"
    This reverts commit e021a5ec629ca0ef4a16d83e70508ded98901c75.

--- RetailCoder.VBE/UI/SourceControl/BranchesPresenter.cs
+++ RetailCoder.VBE/UI/SourceControl/BranchesPresenter.cs
@@ -148,8 +148,7 @@ namespace Rubberduck.UI.SourceControl
 
         private void OnCreateBranchTextChanged(object sender, EventArgs e)
         {
-            _createView.OkButtonEnabled = !string.IsNullOrEmpty(_createView.UserInputText) && 
-                                          !_view.Local.Contains(_createView.UserInputText);
+            _createView.OkButtonEnabled = !string.IsNullOrEmpty(_createView.UserInputText);
         }
 
         private void OnShowMerge(object sender, EventArgs e)
