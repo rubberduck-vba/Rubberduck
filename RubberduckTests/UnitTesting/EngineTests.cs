f1624b7bfaf3f204f3cf56014c49aed22ab74b06
Hosch250 <hosch250@live.com>
2015-06-14 14:10:07 -0500

    Get tests working again

--- RubberduckTests/UnitTesting/EngineTests.cs
+++ RubberduckTests/UnitTesting/EngineTests.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using Microsoft.Vbe.Interop;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rubberduck.UnitTesting;
 using Rubberduck.VBEditor;
@@ -14,6 +17,8 @@ namespace RubberduckTests.UnitTesting
     {
         private TestEngine _engine;
         private Mock<IHostApplication> _hostAppMock;
+        private Mock<VBProject> _project;
+        private Mock<References> _references;
         private readonly QualifiedModuleName _moduleName = new QualifiedModuleName("VBAProject", "TestModule1");
 
         private TestMethod _successfulMethod;
@@ -25,13 +30,22 @@ namespace RubberduckTests.UnitTesting
         private int _eventCount;
 
         [TestInitialize]
-        public void Intialize()
+        public void Initialize()
         {
             _wasEventRaised = false;
             _eventCount = 0;
 
             _engine = new TestEngine();
             _hostAppMock = new Mock<IHostApplication>();
+
+            _references = new Mock<References>();
+            _references.Setup(r => r.GetEnumerator()).Returns(ReferenceList());
+            _references.As<IEnumerable>().Setup(r => r.GetEnumerator()).Returns(ReferenceList);
+            _references.Setup(r => r.AddFromFile(It.IsAny<string>()));
+
+            _project = new Mock<VBProject>();
+            _project.SetupProperty(p => p.Name, "VBAProject");
+            _project.SetupGet(p => p.References).Returns(_references.Object);
 
             _successfulMethod = new TestMethod(new QualifiedMemberName(_moduleName, "TestMethod1"), _hostAppMock.Object);
             _failedMethod = new TestMethod(new QualifiedMemberName(_moduleName, "TestMethod2"), _hostAppMock.Object);
@@ -47,6 +61,29 @@ namespace RubberduckTests.UnitTesting
             };
 
             _engine.AllTests = tests;
+        }
+
+        private static IEnumerator<Reference> ReferenceList()
+        {
+            var ref1 = new Mock<Reference>();
+            ref1.SetupGet(r => r.Name).Returns("VBA");
+            ref1.SetupGet(r => r.FullPath).Returns("C:\\Program Files\\Common Files\\Microsoft Shared\\VBA\\VBA7.1\\VBE7.DLL");
+            yield return ref1.Object;
+
+            var ref2 = new Mock<Reference>();
+            ref2.SetupGet(r => r.Name).Returns("Excel");
+            ref2.SetupGet(r => r.FullPath).Returns("C:\\Program Files\\Microsoft Office 15\\Root\\Office15\\EXCEL.EXE");
+            yield return ref2.Object;
+
+            var ref3 = new Mock<Reference>();
+            ref3.SetupGet(r => r.Name).Returns("stdole");
+            ref3.SetupGet(r => r.FullPath).Returns("C:\\Windows\\System32\\stdole2.tlb");
+            yield return ref3.Object;
+
+            var ref4 = new Mock<Reference>();
+            ref4.SetupGet(r => r.Name).Returns("Office");
+            ref4.SetupGet(r => r.FullPath).Returns("C:\\Program Files\\Common Files\\Microsoft Shared\\OFFICE15\\MSO.DLL");
+            yield return ref4.Object;
         }
 
         [TestMethod]
@@ -118,7 +155,7 @@ namespace RubberduckTests.UnitTesting
             var tests = _engine.AllTests.Keys;
 
             //act
-            _engine.Run(tests);
+            _engine.Run(tests, _project.Object);
 
             Assert.IsTrue(_wasEventRaised, "Module Intialize was not run.");
             Assert.AreEqual(1, _eventCount, "Module Intialzie expected to be run once.");
@@ -131,7 +168,7 @@ namespace RubberduckTests.UnitTesting
             _engine.ModuleCleanup += CatchEvent;
 
             //act
-            _engine.Run(_engine.AllTests.Keys);
+            _engine.Run(_engine.AllTests.Keys, _project.Object);
 
             //assert
             Assert.IsTrue(_wasEventRaised, "Module Cleanup was not run.");
@@ -146,7 +183,7 @@ namespace RubberduckTests.UnitTesting
             _engine.MethodInitialize += CatchEvent;
 
             //act
-            _engine.Run(_engine.AllTests.Keys);
+            _engine.Run(_engine.AllTests.Keys, _project.Object);
 
             //assert
             Assert.IsTrue(_wasEventRaised, "Method Intialize was not run.");
@@ -161,11 +198,11 @@ namespace RubberduckTests.UnitTesting
             _engine.MethodCleanup += CatchEvent;
 
             //act
-            _engine.Run(_engine.AllTests.Keys);
+            _engine.Run(_engine.AllTests.Keys, _project.Object);
 
             //assert
-            Assert.IsTrue(_wasEventRaised, "Method Intialize was not run.");
-            Assert.AreEqual(expectedCount, _eventCount, "Method Intialized was expected to be run {0} times", expectedCount);
+            Assert.IsTrue(_wasEventRaised, "Method Initialize was not run.");
+            Assert.AreEqual(expectedCount, _eventCount, "Method Initialized was expected to be run {0} times", expectedCount);
         }
 
         [TestMethod]
@@ -176,7 +213,7 @@ namespace RubberduckTests.UnitTesting
             _engine.TestComplete += EngineOnTestComplete;
 
             //act
-            _engine.Run(_engine.AllTests.Keys);
+            _engine.Run(_engine.AllTests.Keys, _project.Object);
 
             //assert
             Assert.IsTrue(_wasEventRaised, "TestComplete event was not raised.");
@@ -190,7 +227,7 @@ namespace RubberduckTests.UnitTesting
             _engine.MethodInitialize += CatchEvent;
 
             //act
-            _engine.Run(new List<TestMethod>());
+            _engine.Run(new List<TestMethod>(), _project.Object);
 
             //assert
             Assert.IsFalse(_wasEventRaised, "No methods should run when passed an empty list of tests.");
