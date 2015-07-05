26935c5e21effd1561b7a1c81dcab1ced0143c20
Christopher J. McClellan <ckuhn203@gmail.com>
2015-07-05 13:58:02 -0400

    Extracted IExtractMethodPresenter interface

--- RetailCoder.VBE/UI/CodeExplorer/CodeExplorerWindow.cs
+++ RetailCoder.VBE/UI/CodeExplorer/CodeExplorerWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
 using Microsoft.Vbe.Interop;
 using Rubberduck.Parsing.Symbols;
@@ -6,6 +7,7 @@ using Rubberduck.Properties;
 
 namespace Rubberduck.UI.CodeExplorer
 {
+    [ExcludeFromCodeCoverage]
     public partial class CodeExplorerWindow : UserControl, IDockableUserControl
     {
         private const string ClassId = "C5318B59-172F-417C-88E3-B377CDA2D809";
