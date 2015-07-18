d1d40c4bd073dcb1ae45aaddd7d98891089551f2
Hosch250 <hosch250@live.com>
2015-07-18 00:03:30 -0500

    Variable Not Used Inspection Tests

---
+++ RubberduckTests/Inspections/VariableNeverUsedInspectionTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Linq;
+using Microsoft.Vbe.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Inspections;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.VBEditor.VBEInterfaces.RubberduckCodePane;
+using RubberduckTests.Mocks;
+
+namespace RubberduckTests.Inspections
+{
+    [TestClass]
+    public class VariableNeverUsedInspectionTests : VbeTestBase
+    {
+        [TestMethod]
+        public void VariableNotAssigned_ReturnsResult()
+        {
+            const string inputCode =
+@"Sub Foo()
+    Dim var1 As String
+End Sub";
+
+            //Arrange
+            var builder = new MockVbeBuilder();
+            var project = builder.ProjectBuilder("TestProject1", vbext_ProjectProtection.vbext_pp_none)
+                .AddComponent("Class1", vbext_ComponentType.vbext_ct_ClassModule, inputCode)
+                .Build().Object;
+
+            var codePaneFactory = new RubberduckCodePaneFactory();
+            var parseResult = new RubberduckParser(codePaneFactory).Parse(project);
+
+            var inspection = new VariableNotUsedInspection();
+            var inspectionResults = inspection.GetInspectionResults(parseResult);
+
+            Assert.AreEqual(1, inspectionResults.Count());
+        }
+
+        [TestMethod]
+        public void UnassignedVariable_ReturnsResult_MultipleVariables()
+        {
+            const string inputCode =
+@"Sub Foo()
+    Dim var1 As String
+    Dim var2 As Date
+End Sub";
+
+            //Arrange
+            var builder = new MockVbeBuilder();
+            var project = builder.ProjectBuilder("TestProject1", vbext_ProjectProtection.vbext_pp_none)
+                .AddComponent("Class1", vbext_ComponentType.vbext_ct_ClassModule, inputCode)
+                .Build().Object;
+
+            var codePaneFactory = new RubberduckCodePaneFactory();
+            var parseResult = new RubberduckParser(codePaneFactory).Parse(project);
+
+            var inspection = new VariableNotUsedInspection();
+            var inspectionResults = inspection.GetInspectionResults(parseResult);
+
+            Assert.AreEqual(2, inspectionResults.Count());
+        }
+
+        [TestMethod]
+        public void UnassignedVariable_DoesNotReturnResult()
+        {
+            const string inputCode =
+@"Function Foo() As Boolean
+    Dim var1 as String
+    var1 = ""test""
+
+    Goo var1
+End Function
+
+Sub Goo(ByVal arg1 As String)
+End Sub";
+
+            //Arrange
+            var builder = new MockVbeBuilder();
+            var project = builder.ProjectBuilder("TestProject1", vbext_ProjectProtection.vbext_pp_none)
+                .AddComponent("Class1", vbext_ComponentType.vbext_ct_ClassModule, inputCode)
+                .Build().Object;
+
+            var codePaneFactory = new RubberduckCodePaneFactory();
+            var parseResult = new RubberduckParser(codePaneFactory).Parse(project);
+
+            var inspection = new VariableNotUsedInspection();
+            var inspectionResults = inspection.GetInspectionResults(parseResult);
+
+            Assert.AreEqual(0, inspectionResults.Count());
+        }
+
+        [TestMethod]
+        public void UnassignedVariable_ReturnsResult_MultipleVariables_SomeAssigned()
+        {
+            const string inputCode =
+@"Sub Foo()
+    Dim var1 as Integer
+    var1 = 8
+
+    Dim var2 as String
+    var2 = ""test""
+
+    Goo var1
+End Sub
+
+Sub Goo(ByVal arg1 As Integer)
+End Sub";
+
+            //Arrange
+            var builder = new MockVbeBuilder();
+            var project = builder.ProjectBuilder("TestProject1", vbext_ProjectProtection.vbext_pp_none)
+                .AddComponent("Class1", vbext_ComponentType.vbext_ct_ClassModule, inputCode)
+                .Build().Object;
+
+            var codePaneFactory = new RubberduckCodePaneFactory();
+            var parseResult = new RubberduckParser(codePaneFactory).Parse(project);
+
+            var inspection = new VariableNotUsedInspection();
+            var inspectionResults = inspection.GetInspectionResults(parseResult);
+
+            Assert.AreEqual(1, inspectionResults.Count());
+        }
+
+        [TestMethod]
+        public void InspectionType()
+        {
+            var inspection = new VariableNotUsedInspection();
+            Assert.AreEqual(CodeInspectionType.CodeQualityIssues, inspection.InspectionType);
+        }
+
+        [TestMethod]
+        public void InspectionName()
+        {
+            const string inspectionName = "VariableNotUsedInspection";
+            var inspection = new VariableNotUsedInspection();
+
+            Assert.AreEqual(inspectionName, inspection.Name);
+        }
+    }
+}
