9f143ff7b9e2394f0655f5eef269afde2786033a
Hosch250 <hosch250@live.com>
2015-07-19 23:31:34 -0500

    Remove utterly unnecessary catch

--- RetailCoder.VBE/UI/CodeInspections/CodeInspectionsDockablePresenter.cs
+++ RetailCoder.VBE/UI/CodeInspections/CodeInspectionsDockablePresenter.cs
@@ -176,20 +176,14 @@ namespace Rubberduck.UI.CodeInspections
             Control.EnableRefresh(false);
             Control.Cursor = Cursors.WaitCursor;
 
-            try
+            await Task.Run(() => RefreshAsync(token), token);
+            if (_results != null)
             {
-                await Task.Run(() => RefreshAsync(token), token);
-                if (_results != null)
-                {
-                    var results = _results.Select(item => new CodeInspectionResultGridViewItem(item));
+                var results = _results.Select(item => new CodeInspectionResultGridViewItem(item));
 
-                    Control.SetContent(new BindingList<CodeInspectionResultGridViewItem>(
-                    _gridViewSort.Sort(results, _gridViewSort.ColumnName,
-                        _gridViewSort.SortedAscending).ToList()));
-                }
-            }
-            catch (TaskCanceledException)
-            {
+                Control.SetContent(new BindingList<CodeInspectionResultGridViewItem>(
+                _gridViewSort.Sort(results, _gridViewSort.ColumnName,
+                    _gridViewSort.SortedAscending).ToList()));
             }
 
             Control.SetIssuesStatus(_issues, true);
