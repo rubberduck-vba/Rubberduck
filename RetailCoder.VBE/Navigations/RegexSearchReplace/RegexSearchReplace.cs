d9aa0a2cd160e33d26bf2a8ae5bceb0513567160
Hosch250 <hosch250@live.com>
2015-07-27 17:56:54 -0500

    All Opened Files works

--- RetailCoder.VBE/Navigations/RegexSearchReplace/RegexSearchReplace.cs
+++ RetailCoder.VBE/Navigations/RegexSearchReplace/RegexSearchReplace.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Rubberduck.Parsing.Symbols;
-using Rubberduck.UI;
-using Rubberduck.UI.IdentifierReferences;
+using Microsoft.Vbe.Interop;
 
 namespace Rubberduck.Navigations.RegexSearchReplace
 {
@@ -20,22 +18,18 @@ namespace Rubberduck.Navigations.RegexSearchReplace
         {
             var results = new List<RegexSearchResult>();
 
-            if (scope == RegexSearchReplaceScope.CurrentFile)
+            switch (scope)
             {
-                var module = _model.VBE.ActiveCodePane.CodeModule;
+                case RegexSearchReplaceScope.CurrentFile:
+                    results.AddRange(GetResultsFromModule(_model.VBE.ActiveCodePane.CodeModule, searchPattern));
+                    break;
 
-                for (var i = 1; i <= module.CountOfLines; i++)
-                {
-                    var matches =
-                        Regex.Matches(module.Lines[i, 1], searchPattern)
-                            .OfType<Match>()
-                            .Select(m => new RegexSearchResult(m, i)).ToList();
-
-                    if (matches.Any())
+                case RegexSearchReplaceScope.AllOpenedFiles:
+                    foreach (var codePane in _model.VBE.CodePanes.Cast<CodePane>().Where(codePane => ReferenceEquals(_model.VBE, codePane.VBE)))
                     {
-                        results.AddRange(matches);
+                        results.AddRange(GetResultsFromModule(codePane.CodeModule, searchPattern));
                     }
-                }
+                    break;
             }
 
             return results;
@@ -43,34 +37,44 @@ namespace Rubberduck.Navigations.RegexSearchReplace
 
         public void Replace(string searchPattern, string replaceValue, RegexSearchReplaceScope scope)
         {
-            if (scope == RegexSearchReplaceScope.CurrentFile)
-            {
-                var module = _model.VBE.ActiveCodePane.CodeModule;
-                var results = Find(searchPattern, scope);
+            var results = Find(searchPattern, scope);
 
-                if (results.Count > 0)
-                {
-                    var originalLine = module.Lines[results[0].Selection.StartLine, 1];
-                    var newLine = originalLine.Replace(results[0].Match.Value, replaceValue);
-                    module.ReplaceLine(results[0].Selection.StartLine, newLine);
-                }
-            }
+            if (results.Count <= 0) { return; }
+
+            var originalLine = results[0].Module.Lines[results[0].Selection.StartLine, 1];
+            var newLine = originalLine.Replace(results[0].Match.Value, replaceValue);
+            results[0].Module.ReplaceLine(results[0].Selection.StartLine, newLine);
         }
 
         public void ReplaceAll(string searchPattern, string replaceValue, RegexSearchReplaceScope scope)
         {
-            if (scope == RegexSearchReplaceScope.CurrentFile)
-            {
-                var module = _model.VBE.ActiveCodePane.CodeModule;
-                var results = Find(searchPattern, scope);
+            var results = Find(searchPattern, scope);
 
-                foreach (var result in results)
+            foreach (var result in results)
+            {
+                var originalLine = result.Module.Lines[result.Selection.StartLine, 1];
+                var newLine = originalLine.Replace(result.Match.Value, replaceValue);
+                result.Module.ReplaceLine(result.Selection.StartLine, newLine);
+            }
+        }
+
+        private IEnumerable<RegexSearchResult> GetResultsFromModule(CodeModule module, string searchPattern)
+        {
+            var results = new List<RegexSearchResult>();
+
+            for (var i = 1; i <= module.CountOfLines; i++)
+            {
+                var matches =
+                    Regex.Matches(module.Lines[i, 1], searchPattern)
+                        .OfType<Match>()
+                        .Select(m => new RegexSearchResult(m, module, i)).ToList();
+
+                if (matches.Any())
                 {
-                    var originalLine = module.Lines[result.Selection.StartLine, 1];
-                    var newLine = originalLine.Replace(result.Match.Value, replaceValue);
-                    module.ReplaceLine(result.Selection.StartLine, newLine);
+                    results.AddRange(matches);
                 }
             }
+            return results;
         }
 
         /*private void ShowResultsToolwindow(IEnumerable<Declaration> implementations, string name)
--- RetailCoder.VBE/Navigations/RegexSearchReplace/RegexSearchResult.cs
+++ RetailCoder.VBE/Navigations/RegexSearchReplace/RegexSearchResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using Microsoft.Vbe.Interop;
 using Rubberduck.VBEditor;
 
 namespace Rubberduck.Navigations.RegexSearchReplace
@@ -6,11 +7,13 @@ namespace Rubberduck.Navigations.RegexSearchReplace
     public class RegexSearchResult
     {
         public Match Match { get; private set; }
+        public CodeModule Module { get; private set; }
         public Selection Selection { get; private set; }
 
-        public RegexSearchResult(Match match, int line)
+        public RegexSearchResult(Match match, CodeModule module, int line)
         {
             Match = match;
+            Module = module;
             Selection = new Selection(line, match.Index, line, match.Index + match.Length);
         }
     }
