42f4877adda68eac82e21aae998c56b93fd0fefe
Mathieu Guindon <retailcoder@gmail.com>
2015-08-06 01:50:25 -0400

    implemented OptionsCommand

---
+++ RetailCoder.VBE/UI/Command/OptionsCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rubberduck.UI.Settings;
+
+namespace Rubberduck.UI.Command
+{
+    public class OptionsCommand : ICommand
+    {
+        public void Execute()
+        {
+            using (var window = new SettingsDialog())
+            {
+                window.ShowDialog();
+            }
+        }
+    }
+
+    public class OptionsMenuCommand : CommandMenuItemBase
+    {
+        public OptionsMenuCommand(ICommand command) : base(command)
+        {
+        }
+
+        public override string Key
+        {
+            get { return RubberduckUI.RubberduckMenu_Options; }
+        }
+
+        public override bool BeginGroup { get { return true; } }
+        public override int DisplayOrder { get { return 80; } }
+    }
+}
