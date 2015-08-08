5c8c5f033e8f51d1870f5e9fa86d4446e4addb87
Mathieu Guindon <retailcoder@gmail.com>
2015-08-08 02:07:54 -0400

    injected IGeneralConfigService into OptionsCommand

--- RetailCoder.VBE/UI/Command/OptionsCommand.cs
+++ RetailCoder.VBE/UI/Command/OptionsCommand.cs
@@ -2,15 +2,22 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rubberduck.Settings;
 using Rubberduck.UI.Settings;
 
 namespace Rubberduck.UI.Command
 {
     public class OptionsCommand : ICommand
     {
+        private readonly IGeneralConfigService _service;
+        public OptionsCommand(IGeneralConfigService service)
+        {
+            _service = service;
+        }
+
         public void Execute()
         {
-            using (var window = new SettingsDialog())
+            using (var window = new SettingsDialog(_service))
             {
                 window.ShowDialog();
             }
