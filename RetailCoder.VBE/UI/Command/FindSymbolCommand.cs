eb831e97c0352d62462032e0f9684122e3e1f63a
Hosch250 <hosch250@live.com>
2015-09-27 19:17:05 -0500

    I think this should work, but I can't run RD, so you'll have to check.

--- RetailCoder.VBE/UI/Command/FindSymbolCommand.cs
+++ RetailCoder.VBE/UI/Command/FindSymbolCommand.cs
@@ -1,5 +1,8 @@
-using System;
+using System.Linq;
 using System.Runtime.InteropServices;
+using Microsoft.Vbe.Interop;
+using Rubberduck.Parsing;
+using Rubberduck.UI.FindSymbol;
 
 namespace Rubberduck.UI.Command
 {
@@ -9,9 +12,27 @@ namespace Rubberduck.UI.Command
     [ComVisible(false)]
     public class FindSymbolCommand : CommandBase
     {
+        private readonly VBE _vbe;
+        private readonly IRubberduckParser _parser;
+        private readonly SearchResultIconCache _iconCache;
+
+        public FindSymbolCommand(VBE vbe, IRubberduckParser parser, SearchResultIconCache iconCache)
+        {
+            _vbe = vbe;
+            _parser = parser;
+            _iconCache = iconCache;
+        }
+
         public override void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            var progress = new ParsingProgressPresenter();
+            var result = progress.Parse(_parser, _vbe.ActiveVBProject);
+            var declarations = result.Declarations;
+            var vm = new FindSymbolViewModel(declarations.Items.Where(item => !item.IsBuiltIn), _iconCache);
+            using (var view = new FindSymbolDialog(vm))
+            {
+                view.ShowDialog();
+            }
         }
     }
 }
