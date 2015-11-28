e4e029009620023af214a6d00cad425d34b0f8bc
Mathieu Guindon <retailcoder@gmail.com>
2015-11-28 12:31:48 -0500

    Replaced Rubberduck.SmartIndenter VB project with a new C# project

--- Rubberduck.SmartIndenter/frmVbeOnKey.vb
+++
@@ -1,9 +0,0 @@
-﻿Option Explicit On
-
-Imports System.Windows.Forms;
-
-Public Class frmVbeOnKey
-    Private Sub Form_Closing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        UnHookAll
-    End Sub
-End Class
