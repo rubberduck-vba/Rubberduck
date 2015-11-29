00b319ca515d049cd65678f0341d414cf9a2191e
Mathieu Guindon <retailcoder@gmail.com>
2015-11-28 23:31:04 -0500

    Added IIndenter interface, implemented Indenter (project only; "IndentProcedure" and "IndentModule" not implemented)

---
+++ Rubberduck.SmartIndenter/IIndenter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Vbe.Interop;
+
+namespace Rubberduck.SmartIndenter
+{
+    public interface IIndenter
+    {
+        void Indent(VBProject project);
+        void Indent(VBComponent module);
+        void Indent(VBComponent module, string procedureName);
+    }
+}
