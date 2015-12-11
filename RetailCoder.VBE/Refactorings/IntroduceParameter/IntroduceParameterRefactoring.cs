cb834d4389ca14fe9ac8655e2d76794dfcf038df
Hosch250 <hosch250@live.com>
2015-12-11 14:13:39 -0600

    Remove variable (doesn't remove Dim for single statements)

--- RetailCoder.VBE/Refactorings/IntroduceParameter/IntroduceParameterRefactoring.cs
+++ RetailCoder.VBE/Refactorings/IntroduceParameter/IntroduceParameterRefactoring.cs
@@ -1,25 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rubberduck.Common;
 using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.UI;
 using Rubberduck.VBEditor;
 
-namespace Rubberduck.Refactorings.PromoteLocalToParameter
+namespace Rubberduck.Refactorings.IntroduceParameter
 {
     public class PromoteLocalToParameterRefactoring : IRefactoring
     {
+        private readonly IList<Declaration> _declarations;
         private readonly IActiveCodePaneEditor _editor;
+        private Declaration _targetDeclaration;
+        private IMessageBox _messageBox;
 
-        public PromoteLocalToParameterRefactoring(IActiveCodePaneEditor editor)
+        public PromoteLocalToParameterRefactoring (RubberduckParserState parseResult, IActiveCodePaneEditor editor, IMessageBox messageBox)
         {
+            _declarations = parseResult.AllDeclarations.ToList();
             _editor = editor;
+            _messageBox = messageBox;
         }
 
         public void Refactor ()
         {
-            GetParameterDefinition(_editor.GetSelection());
+            if (_targetDeclaration == null)
+            {
+                _messageBox.Show("Invalid selection...");   // todo: write a better message and localize it
+                return;
+            }
+
+            RemoveVariable();
         }
 
         public void Refactor (QualifiedSelection target)
         {
+            _targetDeclaration = _declarations.FindSelection(target, new [] {DeclarationType.Variable});
+            var v = _declarations.Where(i => !i.IsBuiltIn);
+
             _editor.SetSelection(target);
             Refactor();
         }
@@ -31,23 +50,38 @@ namespace Rubberduck.Refactorings.PromoteLocalToParameter
                 throw new ArgumentException("Invalid declaration type");
             }
 
+            _targetDeclaration = target;
             _editor.SetSelection(target.QualifiedSelection);
             Refactor();
         }
 
-        private void AddParameterToDefinition (Declaration target)
+        private void AddParameter ()
         {
-            
+            // insert string from GetParameterDefinition() into sub/function/... declaration
         }
 
-        private string GetParameterDefinition (QualifiedSelection? target)
+        private void RemoveVariable()
         {
-            if (target != null)
-            {
-                _editor.InsertLines(2, target.Value.QualifiedName.ComponentName);
-            }
+            var selection = new Selection(_targetDeclaration.Context.Start.Line,
+                                          _targetDeclaration.Context.Start.Column,
+                                          _targetDeclaration.Context.Stop.Line,
+                                          _targetDeclaration.Context.Stop.Column);
 
-            return null;
+            var lines =
+                _editor.GetLines(selection)
+                    .Replace(Environment.NewLine, string.Empty)
+                    .Replace(" _", string.Empty)
+                    .Remove(selection.StartColumn, _targetDeclaration.Context.GetText().Length);
+
+            _editor.DeleteLines(selection);
+            _editor.InsertLines(selection.StartLine, lines);
+        }
+
+        private string GetParameterDefinition ()
+        {
+            if (_targetDeclaration == null) { return null; }
+
+            return _targetDeclaration.IdentifierName + " As " + _targetDeclaration.AsTypeName;
         }
     }
 }
--- RetailCoder.VBE/UI/Command/Refactorings/RefactorIntroduceParameterCommand.cs
+++ RetailCoder.VBE/UI/Command/Refactorings/RefactorIntroduceParameterCommand.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Vbe.Interop;
 using Rubberduck.Parsing.VBA;
-using Rubberduck.Refactorings.PromoteLocalToParameter;
+using Rubberduck.Refactorings.IntroduceParameter;
 using Rubberduck.VBEditor;
 using Rubberduck.VBEditor.VBEInterfaces.RubberduckCodePane;
 
@@ -8,11 +8,13 @@ namespace Rubberduck.UI.Command.Refactorings
 {
     public class RefactorIntroduceParameterCommand : RefactorCommandBase
     {
+        private readonly RubberduckParserState _state;
         private readonly ICodePaneWrapperFactory _wrapperWrapperFactory;
 
         public RefactorIntroduceParameterCommand (VBE vbe, RubberduckParserState state, IActiveCodePaneEditor editor, ICodePaneWrapperFactory wrapperWrapperFactory)
             : base(vbe, editor)
         {
+            _state = state;
             _wrapperWrapperFactory = wrapperWrapperFactory;
         }
 
@@ -25,7 +27,7 @@ namespace Rubberduck.UI.Command.Refactorings
             var codePane = _wrapperWrapperFactory.Create(Vbe.ActiveCodePane);
             var selection = new QualifiedSelection(new QualifiedModuleName(codePane.CodeModule.Parent), codePane.Selection);
 
-            var refactoring = new PromoteLocalToParameterRefactoring(Editor);
+            var refactoring = new PromoteLocalToParameterRefactoring(_state, Editor, new MessageBox());
             refactoring.Refactor(selection);
         }
     }
