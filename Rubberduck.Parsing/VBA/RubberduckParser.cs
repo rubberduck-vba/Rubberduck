c4a756acc46da0fbefc92b5d39a0176e076fa795
hosch250 <hosch250@live.com>
2015-12-18 12:07:56 -0600

    Fix slight bug and add ConcurrentHashSet

--- Rubberduck.Parsing/VBA/RubberduckParser.cs
+++ Rubberduck.Parsing/VBA/RubberduckParser.cs
@@ -176,9 +176,8 @@ namespace Rubberduck.Parsing.VBA
             }
 
             _state.SetModuleState(component, ParserState.Resolving);
-            var declarations = _state.AllDeclarations;
 
-            var resolver = new IdentifierReferenceResolver(new QualifiedModuleName(component), declarations);
+            var resolver = new IdentifierReferenceResolver(new QualifiedModuleName(component), _state.AllDeclarations);
             var listener = new IdentifierReferenceListener(resolver, token);
             var walker = new ParseTreeWalker();
             try
