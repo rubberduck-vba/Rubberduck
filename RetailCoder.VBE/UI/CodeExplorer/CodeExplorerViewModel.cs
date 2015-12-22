810b421d1be1b2b6f8eb2ebba5a103549a13ee72
Mathieu Guindon <retailcoder@gmail.com>
2015-12-21 23:39:43 -0500

    added CodeExplorerViewModel

---
+++ RetailCoder.VBE/UI/CodeExplorer/CodeExplorerViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+
+namespace Rubberduck.UI.CodeExplorer
+{
+    public class CodeExplorerViewModel : ViewModelBase
+    {
+        private readonly RubberduckParserState _state;
+        private readonly ObservableCollection<ExplorerItemViewModel> _children;
+
+        public CodeExplorerViewModel(RubberduckParserState state, ObservableCollection<ExplorerItemViewModel> children)
+        {
+            _state = state;
+            _children = children;
+        }
+
+        private bool _isBusy;
+        public bool IsBusy { get { return _isBusy; } set { _isBusy = value; OnPropertyChanged(); } }
+
+
+    }
+
+    public class ExplorerItemViewModel : ViewModelBase
+    {
+        private readonly Declaration _declaration;
+        private readonly ObservableCollection<ExplorerItemViewModel> _children = new ObservableCollection<ExplorerItemViewModel>(); 
+
+        public ExplorerItemViewModel(Declaration declaration)
+        {
+            _declaration = declaration;
+        }
+
+        public void AddChild(ExplorerItemViewModel declaration)
+        {
+            _children.Add(declaration);
+        }
+
+        public void Clear()
+        {
+            _children.Clear();
+        }
+
+        public Declaration Declaration { get { return _declaration; } }
+    }
+}
---
+++ RetailCoder.VBE/UI/CodeExplorer/CodeExplorerViewModelBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+
+namespace Rubberduck.UI.CodeExplorer
+{
+    public class CodeExplorerViewModelBuilder
+    {
+        private readonly RubberduckParserState _state;
+
+        public CodeExplorerViewModelBuilder(RubberduckParserState state)
+        {
+            _state = state;
+        }
+
+        public IEnumerable<ExplorerItemViewModel> Build()
+        {
+            var userDeclarations = _state.AllDeclarations.Where(d => !d.IsBuiltIn).ToList();
+            foreach (var projectDeclaration in userDeclarations.Where(d => d.DeclarationType == DeclarationType.Project))
+            {
+                var project = projectDeclaration;
+                var projectItem = new ExplorerItemViewModel(project);
+                foreach (var componentDeclaration in userDeclarations.Where(d => ReferenceEquals(d.ParentDeclaration, project)))
+                {
+                    var component = componentDeclaration;
+                    yield return new ExplorerItemViewModel(component);
+                    foreach (var member in userDeclarations.Where(d => ReferenceEquals(d.ParentDeclaration, component)))
+                    {
+                        yield return new ExplorerItemViewModel(member);
+                        if (member.DeclarationType == DeclarationType.UserDefinedType)
+                        {
+                            
+                        }
+
+                        if (member.DeclarationType == DeclarationType.Enumeration)
+                        {
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
