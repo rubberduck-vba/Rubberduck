f006282c823376488e8cda2ab2339fe00a05b39a
Mathieu Guindon <retailcoder@gmail.com>
2015-12-28 13:51:01 -0500

    removed viewmodelbuilder

--- RetailCoder.VBE/UI/CodeExplorer/CodeExplorerViewModel.cs
+++ RetailCoder.VBE/UI/CodeExplorer/CodeExplorerViewModel.cs
@@ -11,31 +11,49 @@ namespace Rubberduck.UI.CodeExplorer
     public class CodeExplorerViewModel : ViewModelBase
     {
         private readonly RubberduckParserState _state;
-        private readonly ObservableCollection<ExplorerItemViewModel> _children;
 
-        public CodeExplorerViewModel(RubberduckParserState state, ObservableCollection<ExplorerItemViewModel> children)
+        public CodeExplorerViewModel(RubberduckParserState state)
         {
             _state = state;
-            _children = children;
+            _state.StateChanged += ParserState_StateChanged;
+            _state.ModuleStateChanged += ParserState_ModuleStateChanged;
         }
 
         private bool _isBusy;
         public bool IsBusy { get { return _isBusy; } set { _isBusy = value; OnPropertyChanged(); } }
 
+        private void ParserState_StateChanged(object sender, EventArgs e)
+        {
+            IsBusy = _state.Status == ParserState.Parsing;
+        }
 
+        private void ParserState_ModuleStateChanged(object sender, Parsing.ParseProgressEventArgs e)
+        {
+            
+        }
     }
 
-    public class ExplorerItemViewModel : ViewModelBase
+    public class ExplorerProjectItemViewModel : ViewModelBase
+    {
+        
+    }
+
+    public class ExplorerComponentItemViewModel : ViewModelBase
+    {
+        
+    }
+
+    public class ExplorerMemberViewModel : ViewModelBase
     {
         private readonly Declaration _declaration;
-        private readonly ConcurrentStack<ExplorerItemViewModel> _children = new ConcurrentStack<ExplorerItemViewModel>(); 
+        private readonly ConcurrentStack<ExplorerMemberViewModel> _children = new ConcurrentStack<ExplorerMemberViewModel>(); 
 
-        public ExplorerItemViewModel(Declaration declaration)
+        public ExplorerMemberViewModel(Declaration declaration)
         {
             _declaration = declaration;
         }
 
-        public void AddChild(ExplorerItemViewModel declaration)
+        public void AddChild(ExplorerMemberViewModel declaration)
         {
             _children.Push(declaration);
         }
