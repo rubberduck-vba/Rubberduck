f6413e9aa41fdc850b11e230a500d0c381b75f65
hosch250 <hosch250@live.com>
2015-12-28 11:00:05 -0600

    Basic implementation.  Now, how do I wire this thing up?

---
+++ RetailCoder.VBE/AutoSave/AutoSave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Vbe.Interop;
+
+namespace Rubberduck.AutoSave
+{
+    public class AutoSave : IDisposable
+    {
+        private static VBE _vbe;
+        private static readonly Timer Timer = new Timer(Save);
+
+        public AutoSave(VBE vbe, uint time)
+        {
+            _vbe = vbe;
+            Timer.Change(0, time);
+        }
+
+        public static void Save(object foo)
+        {
+            _vbe.ActiveVBProject.SaveAs(_vbe.ActiveVBProject.Name + "_" + DateTime.Now);
+        }
+
+        public void Dispose()
+        {
+            Timer.Dispose();
+        }
+    }
+}
