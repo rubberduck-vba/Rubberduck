dbbb414006c15c4794b6f7fce6260d1d62d947ee
hosch250 <hosch250@live.com>
2016-02-03 22:51:40 -0600

    Untracked...

---
+++ RetailCoder.VBE/UI/Command/Refactorings/CodePaneRefactorRenameCommand.cs
@@ -0,0 +1,45 @@
+using Microsoft.Vbe.Interop;
+using Rubberduck.VBEditor;
+using Rubberduck.VBEditor.VBEInterfaces.RubberduckCodePane;
+using System.Runtime.InteropServices;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.Refactorings.Rename;
+using Rubberduck.UI.Refactorings;
+
+namespace Rubberduck.UI.Command.Refactorings
+{
+    [ComVisible(false)]
+    public class CodePaneRefactorRenameCommand : RefactorCommandBase
+    {
+        private readonly RubberduckParserState _state;
+        private readonly ICodePaneWrapperFactory _wrapperWrapperFactory;
+
+        public CodePaneRefactorRenameCommand(VBE vbe, RubberduckParserState state, IActiveCodePaneEditor editor, ICodePaneWrapperFactory wrapperWrapperFactory) 
+            : base (vbe, editor)
+        {
+            _state = state;
+            _wrapperWrapperFactory = wrapperWrapperFactory;
+        }
+
+        public override void Execute(object parameter)
+        {
+            using (var view = new RenameDialog())
+            {
+                var factory = new RenamePresenterFactory(Vbe, view, _state, new MessageBox(), _wrapperWrapperFactory);
+                var refactoring = new RenameRefactoring(factory, Editor, new MessageBox(), _state);
+
+                var target = parameter as Declaration;
+
+                if (target == null)
+                {
+                    refactoring.Refactor();
+                }
+                else
+                {
+                    refactoring.Refactor(target);
+                }
+            }
+        }
+    }
+}
