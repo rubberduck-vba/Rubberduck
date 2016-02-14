0509a96f3ff59650afb0a4b1dd9b69e4621210ad
Mathieu Guindon <retailcoder@gmail.com>
2016-02-14 00:01:24 -0500

    added more folder icons

--- RetailCoder.VBE/Common/DeclarationExtensions.cs
+++ RetailCoder.VBE/Common/DeclarationExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using Microsoft.Vbe.Interop;
 using Rubberduck.Annotations;
