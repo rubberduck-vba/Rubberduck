37fb1aa257dbffea104c62410d0d180b1b4c25ec
hosch250 <hosch250@live.com>
2016-02-16 23:14:46 -0600

    General settings tests

--- RetailCoder.VBE/UI/Settings/GeneralSettingsViewModel.cs
+++ RetailCoder.VBE/UI/Settings/GeneralSettingsViewModel.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using Rubberduck.Common;
 using Rubberduck.Settings;
 
 namespace Rubberduck.UI.Settings
