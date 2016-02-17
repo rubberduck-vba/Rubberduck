cf23df19f16e1460637877c60131a7d97228531e
hosch250 <hosch250@live.com>
2016-02-16 23:42:30 -0600

    Automate getting inspections label.

---
+++ RubberduckTests/Settings/SettingsControlTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Inspections;
+using Rubberduck.Settings;
+using Rubberduck.UI.Settings;
+
+namespace RubberduckTests.Settings
+{
+    [TestClass]
+    public class SettingsControlTests
+    {
+        [TestMethod]
+        public void DefaultViewIsGeneralSettings()
+        {
+            var configLoader = new ConfigurationLoader(new List<IInspection>());
+            var viewModel = new SettingsControlViewModel(configLoader);
+
+            Assert.AreEqual(SettingsViews.GeneralSettings, viewModel.SelectedSettingsView.View);
+        }
+
+        [TestMethod]
+        public void PassedInViewIsSelected()
+        {
+            var configLoader = new ConfigurationLoader(new List<IInspection>());
+            var viewModel = new SettingsControlViewModel(configLoader, SettingsViews.TodoSettings);
+
+            Assert.AreEqual(SettingsViews.TodoSettings, viewModel.SelectedSettingsView.View);
+        }
+    }
+}
