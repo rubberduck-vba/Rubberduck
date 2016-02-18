88ab91f3af66e62632aae77c8e16676c2e39387c
hosch250 <hosch250@live.com>
2016-02-18 13:48:14 -0600

    Move indenter sample to VM

--- RetailCoder.VBE/UI/Settings/IndenterSettingsViewModel.cs
+++ RetailCoder.VBE/UI/Settings/IndenterSettingsViewModel.cs
@@ -23,6 +23,17 @@ namespace Rubberduck.UI.Settings
             IndentFirstCommentBlock = config.UserSettings.IndenterSettings.IndentFirstCommentBlock;
             IndentFirstDeclarationBlock = config.UserSettings.IndenterSettings.IndentFirstDeclarationBlock;
             IndentSpaces = config.UserSettings.IndenterSettings.IndentSpaces;
+
+            PropertyChanged += IndenterSettingsViewModel_PropertyChanged;
+        }
+
+        void IndenterSettingsViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            // ReSharper disable once ExplicitCallerInfoArgument
+            if (e.PropertyName != "IndenterData")
+            {
+                OnPropertyChanged("IndenterData");
+            }
         }
 
         #region Properties
@@ -248,6 +259,17 @@ namespace Rubberduck.UI.Settings
                     OnPropertyChanged();
                 }
             }
+        }
+
+        private readonly string _indenterData =
+@"Sub Foo()
+    ' Do something here
+    ' xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ' comment here
+End Sub";
+
+        public string IndenterData
+        {
+            get { return _indenterData; /* SmartIndenter.Indent(_indenterData); */ }
         }
 
         #endregion
