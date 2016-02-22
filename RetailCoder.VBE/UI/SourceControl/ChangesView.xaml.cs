c7949c1947d9c0907456bf91c6cff456d0aa59c6
hosch250 <hosch250@live.com>
2016-02-21 23:57:49 -0600

    Changes View UI

---
+++ RetailCoder.VBE/UI/SourceControl/ChangesView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Rubberduck.UI.SourceControl
+{
+    /// <summary>
+    /// Interaction logic for ChangesView.xaml
+    /// </summary>
+    public partial class ChangesView
+    {
+        public ChangesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
