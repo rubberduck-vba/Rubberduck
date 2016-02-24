d91f9f6673fa7c80681d84959f1b509f189c73b9
hosch250 <hosch250@live.com>
2016-02-23 20:17:29 -0600

    Unsynced commits view

---
+++ RetailCoder.VBE/UI/SourceControl/UnsyncedCommitsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Rubberduck.UI.SourceControl
+{
+    /// <summary>
+    /// Interaction logic for UnsyncedCommitsView.xaml
+    /// </summary>
+    public partial class UnsyncedCommitsView
+    {
+        public UnsyncedCommitsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
