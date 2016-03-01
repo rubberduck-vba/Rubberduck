4ca50a5d146035897e82c91af21cb85c2b717e15
hosch250 <hosch250@live.com>
2016-02-29 19:44:02 -0600

    Remove unneeded usings.

--- RetailCoder.VBE/UI/SourceControl/BranchesView.xaml.cs
+++ RetailCoder.VBE/UI/SourceControl/BranchesView.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-
-namespace Rubberduck.UI.SourceControl
+﻿namespace Rubberduck.UI.SourceControl
 {
     /// <summary>
     /// Interaction logic for BranchesView.xaml
