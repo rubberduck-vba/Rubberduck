56d8523ea7de62ee947f79a4091fb5f523bb4274
Andrin Meier <autoboosh@gmail.com>
2016-03-04 15:13:39 +0100

    add vbaparser tests based on real world code

---
+++ RubberduckTests/Grammar/VBAParserValidityTests.cs
@@ -0,0 +1,44 @@
+﻿using Antlr4.Runtime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Parsing.Grammar;
+using Rubberduck.Parsing.Symbols;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RubberduckTests.Grammar
+{
+    [TestClass]
+    public class VBAParserValidityTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"Testfiles\")]
+        public void TestTest()
+        {
+            foreach (var testfile in GetTestFiles())
+            {
+                AssertParseResult(testfile, Parse(testfile).module().GetText());
+            }
+        }
+
+        private void AssertParseResult(string originalCode, string materializedParseTree)
+        {
+            Assert.AreEqual(originalCode, materializedParseTree);
+        }
+
+        private IEnumerable<string> GetTestFiles()
+        {
+            return Directory.EnumerateFiles("Grammar").Select(file => File.ReadAllText(file)).ToList();
+        }
+
+        private VBAParser.StartRuleContext Parse(string code)
+        {
+            var stream = new AntlrInputStream(code);
+            var lexer = new VBALexer(stream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new VBAParser(tokens);
+            parser.AddErrorListener(new ExceptionErrorListener());
+            return parser.startRule();
+        }
+    }
+}
