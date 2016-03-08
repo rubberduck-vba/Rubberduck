223c033176e896b5866151a66c7549ae200a5828
Mathieu Guindon <retailcoder@gmail.com>
2016-03-07 23:37:10 -0500

    reverted broken inspector

--- RetailCoder.VBE/Inspections/Inspector.cs
+++ RetailCoder.VBE/Inspections/Inspector.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,107 +9,99 @@ using Rubberduck.Settings;
 
 namespace Rubberduck.Inspections
 {
-    public class Inspector : IInspector, IDisposable
+    namespace Rubberduck.Inspections
     {
-        private readonly IGeneralConfigService _configService;
-        private readonly IEnumerable<IInspection> _inspections;
-
-        public Inspector(IGeneralConfigService configService, IEnumerable<IInspection> inspections)
+        public class Inspector : IInspector, IDisposable
         {
-            _inspections = inspections;
+            private readonly IGeneralConfigService _configService;
+            private readonly IEnumerable<IInspection> _inspections;
 
-            _configService = configService;
-            configService.LanguageChanged += ConfigServiceLanguageChanged;
-            UpdateInspectionSeverity();
-        }
-
-        private void ConfigServiceLanguageChanged(object sender, EventArgs e)
-        {
-            UpdateInspectionSeverity();
-        }
-
-        private void UpdateInspectionSeverity()
-        {
-            var config = _configService.LoadConfiguration();
-
-            foreach (var inspection in _inspections)
+            public Inspector(IGeneralConfigService configService, IEnumerable<IInspection> inspections)
             {
-                foreach (var setting in config.UserSettings.CodeInspectionSettings.CodeInspections)
+                _inspections = inspections;
+
+                _configService = configService;
+                configService.LanguageChanged += ConfigServiceLanguageChanged;
+                UpdateInspectionSeverity();
+            }
+
+            private void ConfigServiceLanguageChanged(object sender, EventArgs e)
+            {
+                UpdateInspectionSeverity();
+            }
+
+            private void UpdateInspectionSeverity()
+            {
+                var config = _configService.LoadConfiguration();
+
+                foreach (var inspection in _inspections)
                 {
-                    if (inspection.Description == setting.Description)
+                    foreach (var setting in config.UserSettings.CodeInspectionSettings.CodeInspections)
                     {
-                        inspection.Severity = setting.Severity;
+                        if (inspection.Description == setting.Description)
+                        {
+                            inspection.Severity = setting.Severity;
+                        }
                     }
                 }
             }
-        }
 
-        public async Task<IList<ICodeInspectionResult>> FindIssuesAsync(RubberduckParserState state, CancellationToken token)
-        {
-            if (state == null || !state.AllUserDeclarations.Any())
+            public async Task<IList<ICodeInspectionResult>> FindIssuesAsync(RubberduckParserState state, CancellationToken token)
             {
-                return new ICodeInspectionResult[]{};
-            }
-
-            await Task.Yield();
-
-            UpdateInspectionSeverity();
-            //OnReset();
-
-            var allIssues = new ConcurrentBag<ICodeInspectionResult>();
-
-            var inspections = _inspections.Where(inspection => inspection.Severity != CodeInspectionSeverity.DoNotShow)
-                .Select(inspection =>
-                    new Task(() =>
-                    {
-                        token.ThrowIfCancellationRequested();
-                        Console.WriteLine("Running inspection {0} in thread {1}", inspection.Name, Thread.CurrentThread.ManagedThreadId);
-                        var inspectionResults = inspection.GetInspectionResults();
-                        var results = inspectionResults as IList<InspectionResultBase> ?? inspectionResults.ToList();
-
-                        if (results.Any())
-                        {
-                            //OnIssuesFound(results);
-                            Console.WriteLine("Inspection '{0}' returned {1} results.", inspection.Name, results.Count);
-
-                            foreach (var inspectionResult in results)
-                            {
-                                allIssues.Add(inspectionResult);
-                            }
-                        }
-                    })).ToArray();
-
-            try
-            {
-                var stopwatch = Stopwatch.StartNew();
-                Console.WriteLine("Starting code inspections in thread {0}", Thread.CurrentThread.ManagedThreadId);
-                foreach (var inspection in inspections)
+                if (state == null || !state.AllUserDeclarations.Any())
                 {
-                    await inspection;
+                    return new ICodeInspectionResult[] { };
                 }
 
-                //Task.WaitAll(inspections);
-                Console.WriteLine("Code inspections completed ({0}ms) in thread {1}", stopwatch.ElapsedMilliseconds, Thread.CurrentThread.ManagedThreadId);
+                await Task.Yield();
+
+                UpdateInspectionSeverity();
+                //OnReset();
+
+                var allIssues = new ConcurrentBag<ICodeInspectionResult>();
+
+                var inspections = _inspections.Where(inspection => inspection.Severity != CodeInspectionSeverity.DoNotShow)
+                    .Select(inspection =>
+                        new Task(() =>
+                        {
+                            token.ThrowIfCancellationRequested();
+                            var inspectionResults = inspection.GetInspectionResults();
+                            var results = inspectionResults as IList<InspectionResultBase> ?? inspectionResults.ToList();
+
+                            if (results.Any())
+                            {
+                                //OnIssuesFound(results);
+
+                                foreach (var inspectionResult in results)
+                                {
+                                    allIssues.Add(inspectionResult);
+                                }
+                            }
+                        })).ToArray();
+
+                foreach (var inspection in inspections)
+                {
+                    inspection.Start();
+                }
+
+                Task.WaitAll(inspections);
+
+                return allIssues.ToList();
             }
-            catch (AggregateException exceptions)
+
+            public void Dispose()
             {
-                Console.WriteLine(exceptions);
+                Dispose(true);
             }
-            return allIssues.ToList();
-        }
 
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposing) { return; }
-
-            if (_configService != null)
+            protected virtual void Dispose(bool disposing)
             {
-                _configService.LanguageChanged -= ConfigServiceLanguageChanged;
+                if (!disposing) { return; }
+
+                if (_configService != null)
+                {
+                    _configService.LanguageChanged -= ConfigServiceLanguageChanged;
+                }
             }
         }
     }
