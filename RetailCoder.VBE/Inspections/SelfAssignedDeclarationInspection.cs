e4ebaca34878edce2ab8dd74de7c1850f42674b9
Mathieu Guindon <retailcoder@gmail.com>
2016-04-21 21:54:18 -0400

    restricted SelfAssignedDeclarationInspection to non-ValueType variables with a specified type

--- RetailCoder.VBE/Inspections/SelfAssignedDeclarationInspection.cs
+++ RetailCoder.VBE/Inspections/SelfAssignedDeclarationInspection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Rubberduck.Parsing.Grammar;
 using Rubberduck.Parsing.VBA;
 using Rubberduck.Parsing.Symbols;
 
@@ -16,10 +17,27 @@ namespace Rubberduck.Inspections
         public override string Description { get { return InspectionsUI.SelfAssignedDeclarationInspectionResultFormat; } }
         public override CodeInspectionType InspectionType { get { return CodeInspectionType.CodeQualityIssues; } }
 
+        private static readonly IEnumerable<string> ValueTypes = new[]
+        {
+            Tokens.Boolean,
+            Tokens.Byte,
+            Tokens.Currency,
+            Tokens.Date,
+            Tokens.Decimal,
+            Tokens.Double,
+            Tokens.Integer,
+            Tokens.Long,
+            Tokens.LongLong,
+            Tokens.Single,
+            Tokens.String
+        };
+
         public override IEnumerable<InspectionResultBase> GetInspectionResults()
         {
             return UserDeclarations
                 .Where(declaration => declaration.IsSelfAssigned 
+                    && declaration.IsTypeSpecified()
+                    && !ValueTypes.Contains(declaration.AsTypeName)
                     && declaration.DeclarationType == DeclarationType.Variable
                     && declaration.ParentScope == declaration.QualifiedName.QualifiedModuleName.ToString()
                     && declaration.ParentDeclaration != null
