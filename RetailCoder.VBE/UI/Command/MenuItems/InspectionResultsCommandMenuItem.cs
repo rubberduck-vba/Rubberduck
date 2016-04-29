8069bc626484b6fb51ae8c7d7c8e25961f68ba47
Abraham Hosch <hosch250@live.com>
2016-04-29 11:27:38 -0500

    Always let explorers show.

--- RetailCoder.VBE/UI/Command/MenuItems/InspectionResultsCommandMenuItem.cs
+++ RetailCoder.VBE/UI/Command/MenuItems/InspectionResultsCommandMenuItem.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows.Input;
-using Rubberduck.Parsing.VBA;
 using Rubberduck.UI.Command.MenuItems.ParentMenus;
 
 namespace Rubberduck.UI.Command.MenuItems
@@ -13,10 +12,5 @@ namespace Rubberduck.UI.Command.MenuItems
 
         public override string Key { get { return "RubberduckMenu_CodeInspections"; } }
         public override int DisplayOrder { get { return (int)RubberduckMenuItemDisplayOrder.CodeInspections; } }
-
-        public override bool EvaluateCanExecute(RubberduckParserState state)
-        {
-            return state.Status == ParserState.Ready;
-        }
     }
 }
