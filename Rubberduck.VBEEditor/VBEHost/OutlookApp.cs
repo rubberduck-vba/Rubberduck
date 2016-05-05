93de58aa89f7a1741eee6a20206fd6b85fee8880
ThunderFrame <ThunderXFrame@gmail.com>
2016-05-05 20:02:42 +1000

    adds notes and placeholder for Outllook's Application.Run alternative
    not implemented - need to write to events in the ThisOutlookSession
    before this will work.

--- Rubberduck.VBEEditor/VBEHost/OutlookApp.cs
+++ Rubberduck.VBEEditor/VBEHost/OutlookApp.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+//using Microsoft.Office.Interop.Outlook;
 
 namespace Rubberduck.VBEditor.VBEHost
 {
@@ -16,6 +17,13 @@ namespace Rubberduck.VBEditor.VBEHost
             // note: does not work. http://stackoverflow.com/q/31954364/1188513
             //var app = Application.GetType();
             //app.InvokeMember(qualifiedMemberName.MemberName, BindingFlags.InvokeMethod | BindingFlags.Default, null, Application, null);
+            //Application.Run(qualifiedMemberName.ToString());
+
+            //note: this will work, but not implemented yet http://stackoverflow.com/questions/31954364#36889671
+            //TaskItem taskitem = Application.CreateItem(OlItemType.olTaskItem);
+            //taskitem.Subject = "Rubberduck";
+            //taskitem.Body = qualifiedMemberName.MemberName;
+
             throw new NotImplementedException();
         }
     }
