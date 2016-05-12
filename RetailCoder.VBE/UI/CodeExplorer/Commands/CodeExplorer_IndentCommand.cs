71351b9d7dc82cbb59fd7082da6f399e3ffd5083
Abraham Hosch <hosch250@live.com>
2016-05-12 11:12:43 -0500

    Add NoIndent annotation.  Adjust commands to account for it.  Add menu item to add it.

--- RetailCoder.VBE/UI/CodeExplorer/Commands/CodeExplorer_IndentCommand.cs
+++ RetailCoder.VBE/UI/CodeExplorer/Commands/CodeExplorer_IndentCommand.cs
@@ -2,6 +2,9 @@ using Rubberduck.Navigation.CodeExplorer;
 using Rubberduck.Parsing.VBA;
 using Rubberduck.SmartIndenter;
 using Rubberduck.UI.Command;
+using System.Linq;
+using Rubberduck.Parsing.Annotations;
+using Rubberduck.Parsing.Symbols;
 
 namespace Rubberduck.UI.CodeExplorer.Commands
 {
@@ -20,6 +23,15 @@ namespace Rubberduck.UI.CodeExplorer.Commands
 
         public override bool CanExecute(object parameter)
         {
+            if (parameter is CodeExplorerComponentViewModel)
+            {
+                var node = (CodeExplorerComponentViewModel)parameter;
+                if (node.Declaration.Annotations.Any(a => a.AnnotationType == Parsing.Annotations.AnnotationType.NoIndent))
+                {
+                    return false;
+                }
+            }
+
             return _state.Status == ParserState.Ready && !(parameter is CodeExplorerCustomFolderViewModel) &&
                    !(parameter is CodeExplorerErrorNodeViewModel);
         }
@@ -35,7 +47,15 @@ namespace Rubberduck.UI.CodeExplorer.Commands
 
             if (node is CodeExplorerProjectViewModel)
             {
-                _indenter.Indent(node.QualifiedSelection.Value.QualifiedName.Project);
+                var moduleTypes = new[] {DeclarationType.ClassModule, DeclarationType.Module};
+                var components = _state.AllUserDeclarations.Where(c =>
+                            moduleTypes.Contains(c.DeclarationType) &&
+                            c.Annotations.All(a => a.AnnotationType != AnnotationType.NoIndent));
+
+                foreach (var component in components)
+                {
+                    _indenter.Indent(component.QualifiedName.QualifiedModuleName.Component);
+                }
             }
 
             if (node is CodeExplorerComponentViewModel)
--- RetailCoder.VBE/UI/Command/IndentCurrentModuleCommand.cs
+++ RetailCoder.VBE/UI/Command/IndentCurrentModuleCommand.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using Rubberduck.Parsing.Annotations;
+using Rubberduck.Parsing.Symbols;
 using Rubberduck.Settings;
 using Rubberduck.SmartIndenter;
 
@@ -12,6 +15,12 @@ namespace Rubberduck.UI.Command
         public IndentCurrentModuleCommand(IIndenter indenter)
         {
             _indenter = indenter;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            var target = parameter as Declaration;
+            return target != null && target.Annotations.All(a => a.AnnotationType != AnnotationType.NoIndent);
         }
 
         public override void Execute(object parameter)
