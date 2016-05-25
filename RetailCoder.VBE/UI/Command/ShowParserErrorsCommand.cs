e26d60cb10905f5cb4967aebdc9258e609533d92
Abraham Hosch <hosch250@live.com>
2016-05-25 11:14:16 -0500

    Display only one parser error tab, even with multiple clicks.

--- RetailCoder.VBE/UI/Command/ShowParserErrorsCommand.cs
+++ RetailCoder.VBE/UI/Command/ShowParserErrorsCommand.cs
@@ -42,8 +42,15 @@ namespace Rubberduck.UI.Command
                 return;
             }
 
+            var oldTab = _viewModel.SelectedTab;
+            
             _viewModel.AddTab(viewModel);
             _viewModel.SelectedTab = viewModel;
+
+            if (oldTab != null)
+            {
+                oldTab.CloseCommand.Execute(null);
+            }
 
             try
             {
