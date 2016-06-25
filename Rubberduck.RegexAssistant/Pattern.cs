6be77a3c0e3fa3285c06aab2430afe03bb6694ef
Vogel612 <Vogel612@gmx.de>
2016-06-25 23:39:56 +0200

    Added Anchors to Pattern

--- Rubberduck.RegexAssistant/Pattern.cs
+++ Rubberduck.RegexAssistant/Pattern.cs
@@ -11,12 +11,20 @@ namespace Rubberduck.RegexAssistant
         IRegularExpression RootExpression;
         MatcherFlags Flags;
 
+        private readonly bool _hasStartAnchor;
+        private readonly bool _hasEndAnchor;
+
         public Pattern(string expression, bool ignoreCase, bool global)
         {
             Flags = ignoreCase ? MatcherFlags.IgnoreCase : 0;
             Flags = global ? Flags | MatcherFlags.Global : Flags;
 
-            RootExpression = RegularExpression.Parse(expression);
+            _hasEndAnchor = expression[expression.Length - 1].Equals('$');
+            _hasStartAnchor = expression[0].Equals('^');
+
+            int start = _hasStartAnchor ? 1 : 0;
+            int end = (_hasEndAnchor ? 1 : 0) + start + 1;
+            RootExpression = RegularExpression.Parse(expression.Substring(start, expression.Length - end));
         }
 
     }
