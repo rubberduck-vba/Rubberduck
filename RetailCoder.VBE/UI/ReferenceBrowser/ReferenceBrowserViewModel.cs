4b0c7a4f710441675e465d8bae92848798aea45f
ross <rossknudsen@gmail.com>
2016-07-07 20:58:25 +1200

    Removed LibrariesViewModel and derived classes and used ICollectionView instead. Databind selected item in ReferenceListControl in XAML rather than binding through view model property.

--- RetailCoder.VBE/UI/ReferenceBrowser/ReferenceBrowserViewModel.cs
+++ RetailCoder.VBE/UI/ReferenceBrowser/ReferenceBrowserViewModel.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Windows.Data;
 using Microsoft.Vbe.Interop;
 
 namespace Rubberduck.UI.ReferenceBrowser
@@ -8,56 +9,52 @@ namespace Rubberduck.UI.ReferenceBrowser
     public class ReferenceBrowserViewModel : ViewModelBase
     {
         private readonly VBE _vbe;
+        private readonly RegisteredLibraryModelService _service;
+        private readonly ObservableCollection<RegisteredLibraryViewModel> _vbaProjectReferences;
+        private readonly ObservableCollection<RegisteredLibraryViewModel> _registeredComReferences;
 
         public ReferenceBrowserViewModel(VBE vbe, RegisteredLibraryModelService service)
         {
             _vbe = vbe;
+            _service = service;
             
-            ComReferences = new RegisteredComLibrariesViewModel(service, vbe);
-            VbaProjectReferences = new VbaProjectLibrariesViewModel(vbe);
+            _registeredComReferences = new ObservableCollection<RegisteredLibraryViewModel>();
+            ComReferences = new CollectionViewSource {Source = _registeredComReferences}.View;
+            ComReferences.SortDescriptions.Add(new SortDescription(nameof(RegisteredLibraryViewModel.Name), ListSortDirection.Ascending));
+
+            _vbaProjectReferences = new ObservableCollection<RegisteredLibraryViewModel>();
+            VbaProjectReferences = new CollectionViewSource {Source = _vbaProjectReferences }.View;
+
+            BuildTypeLibraryReferenceViewModels();
+            BuildVbaProjectReferenceViewModels();
         }
 
-        public LibrariesViewModel ComReferences { get; }
+        public ICollectionView ComReferences { get; }
 
-        public LibrariesViewModel VbaProjectReferences { get; }
+        public ICollectionView VbaProjectReferences { get; }
 
-        private class RegisteredComLibrariesViewModel : LibrariesViewModel
+        private void BuildTypeLibraryReferenceViewModels()
         {
-            public RegisteredComLibrariesViewModel(RegisteredLibraryModelService service, VBE vbe)
+            var list = _service.GetAllRegisteredLibraries()
+                .Select(l => new RegisteredLibraryViewModel(l, _vbe.ActiveVBProject));
+
+            foreach (var vm in list)
             {
-                Build(service, vbe);
-            }
-
-            private async void Build(RegisteredLibraryModelService service, VBE vbe)
-            {
-                var models = service.GetAllRegisteredLibraries();
-
-                var list = models
-                    .Select(l => new RegisteredLibraryViewModel(l, vbe.ActiveVBProject))
-                    .ToList();
-
-                await Task.Run(() => list.Sort());
-
-                foreach (var vm in list)
-                {
-                    _registeredLibraries.Add(vm);
-                }
+                _registeredComReferences.Add(vm);
             }
         }
 
-        private class VbaProjectLibrariesViewModel : LibrariesViewModel
+        public void BuildVbaProjectReferenceViewModels()
         {
-            public VbaProjectLibrariesViewModel(VBE vbe)
+            var vbaReferences = _vbe.ActiveVBProject.References
+                .OfType<Reference>()
+                .Where(r => r.Type == vbext_RefKind.vbext_rk_Project);
+
+            foreach (var reference in vbaReferences)
             {
-                foreach (var reference in vbe.ActiveVBProject.References.OfType<Reference>())
-                {
-                    if (reference.Type == vbext_RefKind.vbext_rk_Project)
-                    {
-                        var model = new VbaProjectReferenceModel(reference);
-                        var vm = new RegisteredLibraryViewModel(model, vbe.ActiveVBProject);
-                        _registeredLibraries.Add(vm);
-                    }
-                }
+                var model = new VbaProjectReferenceModel(reference);
+                var vm = new RegisteredLibraryViewModel(model, _vbe.ActiveVBProject);
+                _vbaProjectReferences.Add(vm);
             }
         }
     }
