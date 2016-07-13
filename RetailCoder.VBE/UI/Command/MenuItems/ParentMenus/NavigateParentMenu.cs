04dd638c3da2d162f15f8e594f2c28d193444988
Vogel612 <Vogel612@gmx.de>
2016-07-14 01:24:10 +0200

    Fixed compilation errors

--- RetailCoder.VBE/UI/Command/MenuItems/ParentMenus/NavigateParentMenu.cs
+++ RetailCoder.VBE/UI/Command/MenuItems/ParentMenus/NavigateParentMenu.cs
@@ -15,7 +15,6 @@ namespace Rubberduck.UI.Command.MenuItems.ParentMenus
     public enum NavigationMenuItemDisplayOrder
     {
         CodeExplorer,
-        //ToDoExplorer,
         RegexSearchReplace,
         FindSymbol,
         FindAllReferences,
