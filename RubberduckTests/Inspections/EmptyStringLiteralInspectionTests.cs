e54dc44a607e8e2a529781468f47d8fc7e250ba5
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 19:56:12 -0400

    fixed EmptyStringLiteralInspectionTests

--- RubberduckTests/Inspections/EmptyStringLiteralInspectionTests.cs
+++ RubberduckTests/Inspections/EmptyStringLiteralInspectionTests.cs
@@ -187,7 +187,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -229,7 +229,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
