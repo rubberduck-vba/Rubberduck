279174aed21258c2bfa2a19ad4fa7f9a3ea544da
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 19:57:09 -0400

    fixed FunctionReturnValueNotUsedInspectionTests

--- RubberduckTests/Inspections/FunctionReturnValueNotUsedInspectionTests.cs
+++ RubberduckTests/Inspections/FunctionReturnValueNotUsedInspectionTests.cs
@@ -505,7 +505,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            string actual = module.Lines();
+            string actual = new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines();
             Assert.AreEqual(expectedCode, actual);
         }
 
@@ -551,7 +551,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            string actual = module.Lines();
+            string actual = new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines();
             Assert.AreEqual(expectedCode, actual);
         }
 
@@ -608,7 +608,7 @@ End Function";
 
             var project = vbe.Object.VBProjects.Item(0);
             var interfaceModule = project.VBComponents.Item(0).CodeModule;
-            string actualInterface = interfaceModule.Lines();
+            string actualInterface = new Rubberduck.VBEditor.DisposableWrappers.CodeModule(interfaceModule).Lines();
             Assert.AreEqual(expectedInterfaceCode, actualInterface, "Interface");
         }
 
@@ -650,7 +650,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
