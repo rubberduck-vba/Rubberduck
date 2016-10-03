0d0b3377944f5733de60d24f0d5f511907cabbff
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 19:59:04 -0400

    fixed ImplicitPublicMemberInspectionTests

--- RubberduckTests/Inspections/ImplicitPublicMemberInspectionTests.cs
+++ RubberduckTests/Inspections/ImplicitPublicMemberInspectionTests.cs
@@ -209,7 +209,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -247,7 +247,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
