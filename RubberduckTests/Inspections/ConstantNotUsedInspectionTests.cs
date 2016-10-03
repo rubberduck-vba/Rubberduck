e4ca0f500705c5b47fe4e528a7307beed149b7d9
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 19:55:52 -0400

    fixed ConstantNotUsedInspectionTests

--- RubberduckTests/Inspections/ConstantNotUsedInspectionTests.cs
+++ RubberduckTests/Inspections/ConstantNotUsedInspectionTests.cs
@@ -188,7 +188,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -224,7 +224,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
