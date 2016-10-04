f785e127b6c1c2f281f53cc8825943e1a2bb451a
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 20:03:11 -0400

    fixed ObsoleteGlobalInspectionTests

--- RubberduckTests/Inspections/ObsoleteGlobalInspectionTests.cs
+++ RubberduckTests/Inspections/ObsoleteGlobalInspectionTests.cs
@@ -166,7 +166,7 @@ Global var1 As Integer";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -198,7 +198,7 @@ Global var1 As Integer";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
