7991b7e2651bbc81c83e09395156bdf4f4919adc
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 20:00:52 -0400

    fixed MultilineParameterInspectionTests

--- RubberduckTests/Inspections/MultilineParameterInspectionTests.cs
+++ RubberduckTests/Inspections/MultilineParameterInspectionTests.cs
@@ -193,7 +193,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -235,7 +235,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
