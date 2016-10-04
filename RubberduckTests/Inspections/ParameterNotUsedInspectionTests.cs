1b62a0032b7f15b05d1ac71d7c56a0beb31fc263
Mathieu Guindon <retailcoder@gmail.com>
2016-10-03 20:05:43 -0400

    fixed ParameterNotUsedInspectionTests

--- RubberduckTests/Inspections/ParameterNotUsedInspectionTests.cs
+++ RubberduckTests/Inspections/ParameterNotUsedInspectionTests.cs
@@ -212,7 +212,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.First().Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
@@ -246,7 +246,7 @@ End Sub";
 
             inspectionResults.First().QuickFixes.Single(s => s is IgnoreOnceQuickFix).Fix();
 
-            Assert.AreEqual(expectedCode, module.Lines());
+            Assert.AreEqual(expectedCode, new Rubberduck.VBEditor.DisposableWrappers.CodeModule(module).Lines());
         }
 
         [TestMethod]
