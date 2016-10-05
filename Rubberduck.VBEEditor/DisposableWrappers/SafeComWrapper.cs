5ffeacc7594d4aa92662caeb8054e6e8a8347b2f
Mathieu Guindon <retailcoder@gmail.com>
2016-10-05 01:37:24 -0400

    COMException thrown on exit due to double-releasing of a CommandBar - menu actions work again

--- Rubberduck.VBEEditor/DisposableWrappers/SafeComWrapper.cs
+++ Rubberduck.VBEEditor/DisposableWrappers/SafeComWrapper.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 
 namespace Rubberduck.VBEditor.DisposableWrappers
 {
