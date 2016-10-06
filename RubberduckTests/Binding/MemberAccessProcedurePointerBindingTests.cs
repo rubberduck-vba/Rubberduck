687b1585da24f8b6bab9cacb80398271ed848cec
Mathieu Guindon <retailcoder@gmail.com>
2016-10-06 15:47:36 -0400

    fixed MemberAccessProcedurePointerBindingTests

--- RubberduckTests/Binding/MemberAccessProcedurePointerBindingTests.cs
+++ RubberduckTests/Binding/MemberAccessProcedurePointerBindingTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Vbe.Interop;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rubberduck.Parsing.Symbols;
 using Rubberduck.Parsing.VBA;
 using RubberduckTests.Mocks;
@@ -8,6 +7,8 @@ using System.Linq;
 using Moq;
 using Rubberduck.Parsing;
 using System.Threading;
+using Rubberduck.VBEditor.SafeComWrappers;
+using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
 namespace RubberduckTests.Binding
 {
@@ -21,9 +22,9 @@ namespace RubberduckTests.Binding
             public void ProceduralModuleWithAccessibleMember()
             {
                 var builder = new MockVbeBuilder();
-                var enclosingProjectBuilder = builder.ProjectBuilder(BINDING_TARGET_NAME, vbext_ProjectProtection.vbext_pp_none);
+                var enclosingProjectBuilder = builder.ProjectBuilder(BINDING_TARGET_NAME, ProjectProtection.Unprotected);
                 string code = CreateCaller(TEST_CLASS_NAME) + Environment.NewLine + CreateCallee();
-                enclosingProjectBuilder.AddComponent(TEST_CLASS_NAME, vbext_ComponentType.vbext_ct_StdModule, code);
+                enclosingProjectBuilder.AddComponent(TEST_CLASS_NAME, ComponentType.StandardModule, code);
                 var enclosingProject = enclosingProjectBuilder.Build();
                 builder.AddProject(enclosingProject);
                 var vbe = builder.Build();
@@ -38,7 +39,7 @@ namespace RubberduckTests.Binding
                 Assert.AreEqual(1, declaration.References.Count(), "Function should have reference");
             }
 
-            private static RubberduckParserState Parse(Moq.Mock<VBE> vbe)
+            private static RubberduckParserState Parse(Mock<IVBE> vbe)
             {
                 var parser = MockParser.Create(vbe.Object, new RubberduckParserState(new Mock<ISinks>().Object));
                 parser.Parse(new CancellationTokenSource());
