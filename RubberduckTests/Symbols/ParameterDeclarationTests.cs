492cce61999ba4f1f5d8e9e82e6e0c833074e243
Max Doerner <maxdoerner@gmx.net>
2016-12-12 00:15:58 +0100

    Tests for various special declaration types.

---
+++ RubberduckTests/Symbols/ParameterDeclarationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.VBEditor;
+
+namespace RubberduckTests.Symbols
+{
+    [TestClass]
+    public class ParameterDeclarationTests
+    {
+        [TestMethod]
+        public void ParametersHaveDeclarationTypeParameter()
+        {
+            var paramter = GetTestParameter("testParam", false, false, false);
+
+            Assert.IsTrue(paramter.DeclarationType.HasFlag(DeclarationType.Parameter));
+        }
+
+            private static ParameterDeclaration GetTestParameter(string name, bool isOptional, bool isByRef, bool isParamArray)
+            {
+                var qualifiedParameterName = new QualifiedMemberName(StubQualifiedModuleName(), name);
+                return new ParameterDeclaration(qualifiedParameterName, null, "test", null,"test", isOptional,isByRef, false, isParamArray);
+            }
+
+                private static QualifiedModuleName StubQualifiedModuleName()
+                {
+                    return new QualifiedModuleName("dummy", "dummy", "dummy");
+                }
+
+
+        [TestMethod]
+        public void ParametersHaveImpliciteAccessibility()
+        {
+            var paramter = GetTestParameter("testParam", false, false, false);
+
+            Assert.IsTrue(paramter.Accessibility.HasFlag(Accessibility.Implicit));
+        }
+
+
+        [TestMethod]
+        public void IsParamArrayCanBeSetPublicly()
+        {
+            var paramter = GetTestParameter("testParam", false, false, false);
+            paramter.IsParamArray = true;
+
+            Assert.IsTrue(paramter.IsParamArray);
+        }
+
+    }
+}
---
+++ RubberduckTests/Symbols/SubroutineDeclarationTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.VBEditor;
+using Rubberduck.Parsing.VBA;
+
+namespace RubberduckTests.Symbols
+{
+    [TestClass]
+    public class SubroutineDeclarationTests
+    {
+        [TestMethod]
+        public void SubroutinesHaveDeclarationTypeProcedure()
+        {
+            var subroutine = GetTestSub("testSub", null);
+
+            Assert.IsTrue(subroutine.DeclarationType.HasFlag(DeclarationType.Procedure));
+        }
+
+            private static SubroutineDeclaration GetTestSub(string name, Attributes attributes)
+            {
+                var qualifiedName = new QualifiedMemberName(StubQualifiedModuleName(), name);
+                return new SubroutineDeclaration(qualifiedName, null, null, "test", Accessibility.Implicit, null, Selection.Home, false, null, attributes);
+            }
+
+                private static QualifiedModuleName StubQualifiedModuleName()
+                {
+                    return new QualifiedModuleName("dummy", "dummy", "dummy");
+                }
+
+
+        [TestMethod]
+        public void ByDefaultSubroutinesDoNotHaveParameters()
+        {
+            var subroutine = GetTestSub("testSub", null);
+
+            Assert.IsFalse(subroutine.Parameters.Any());
+        }
+
+
+        [TestMethod]
+        public void ParametersReturnsTheParametersAddedViaAddParameters()
+        {
+            var subroutine = GetTestSub("testSub", null);
+            var inputParameter = GetTestParameter("testParameter", false, false, false);
+            subroutine.AddParameter(inputParameter);
+            var returnedParameter = subroutine.Parameters.SingleOrDefault();
+
+            Assert.AreEqual(returnedParameter, inputParameter);
+        }
+
+            private static ParameterDeclaration GetTestParameter(string name, bool isOptional, bool isByRef, bool isParamArray)
+            {
+                var qualifiedParameterName = new QualifiedMemberName(StubQualifiedModuleName(), name);
+                return new ParameterDeclaration(qualifiedParameterName, null, "test", null, "test", isOptional, isByRef, false, isParamArray);
+            }
+
+
+        [TestMethod]
+        public void ByDefaultSubroutinesAreNotDefaultMembers()
+        {
+            var subroutine = GetTestSub("testSub", null);
+
+            Assert.IsFalse(subroutine.IsDefaultMember);
+        }
+
+
+        [TestMethod]
+        public void SubroutinesAreDefaultMembersIfTheyHaveTheDefaultMemberAttribute()
+        {
+            var attributes = new Attributes();
+            attributes.AddDefaultMemberAttribute("testSub");
+            var subroutine = GetTestSub("testSub", attributes);
+
+            Assert.IsTrue(subroutine.IsDefaultMember);
+        }
+
+    }
+}
