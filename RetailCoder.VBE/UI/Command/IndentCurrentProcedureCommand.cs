e901b84acb4a157af5bdad9b8377a85727997112
comintern <brent.mckibbin@gmail.com>
2016-12-12 19:44:01 -0600

    Reparse after indent, fix IndentCurrentProcedure for procs without trailing whitespace.

--- RetailCoder.VBE/UI/Command/IndentCurrentProcedureCommand.cs
+++ RetailCoder.VBE/UI/Command/IndentCurrentProcedureCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using NLog;
+using Rubberduck.Parsing.VBA;
 using Rubberduck.Settings;
 using Rubberduck.SmartIndenter;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
@@ -11,11 +12,14 @@ namespace Rubberduck.UI.Command
     {
         private readonly IVBE _vbe;
         private readonly IIndenter _indenter;
+        private readonly RubberduckParserState _state;
 
-        public IndentCurrentProcedureCommand(IVBE vbe, IIndenter indenter) : base(LogManager.GetCurrentClassLogger())
+        public IndentCurrentProcedureCommand(IVBE vbe, IIndenter indenter, RubberduckParserState state)
+            : base(LogManager.GetCurrentClassLogger())
         {
             _vbe = vbe;
             _indenter = indenter;
+            _state = state;
         }
 
         public override RubberduckHotkey Hotkey
@@ -31,6 +35,7 @@ namespace Rubberduck.UI.Command
         protected override void ExecuteImpl(object parameter)
         {
             _indenter.IndentCurrentProcedure();
+            _state.OnParseRequested(this, _vbe.ActiveCodePane.CodeModule.Parent);
         }
     }
 }
