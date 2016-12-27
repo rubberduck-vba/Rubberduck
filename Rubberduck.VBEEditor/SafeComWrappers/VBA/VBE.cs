4f43877476d1efcb513af7340d9f66615b5e6ef9
comintern <brent.mckibbin@gmail.com>
2016-12-27 17:23:00 -0600

    Disable CommandBars.Release() again to synch.

--- Rubberduck.VBEEditor/SafeComWrappers/VBA/VBE.cs
+++ Rubberduck.VBEEditor/SafeComWrappers/VBA/VBE.cs
@@ -106,7 +106,7 @@ namespace Rubberduck.VBEditor.SafeComWrappers.VBA
             {
                 VBProjects.Release();
                 CodePanes.Release();
-                CommandBars.Release();
+                //CommandBars.Release();
                 Windows.Release();
                 AddIns.Release();
                 base.Release(final);
