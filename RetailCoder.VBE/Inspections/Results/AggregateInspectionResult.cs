66fb27d1783cb60f611b928225904659664305b2
Vogel612 <Vogel612@gmx.de>
2017-01-08 02:25:31 +0100

    clear development debris

--- RetailCoder.VBE/Inspections/Results/AggregateInspectionResult.cs
+++ RetailCoder.VBE/Inspections/Results/AggregateInspectionResult.cs
@@ -3,12 +3,12 @@ using Rubberduck.Inspections.Resources;
 using Rubberduck.VBEditor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rubberduck.Inspections.Results
 {
     class AggregateInspectionResult: IInspectionResult
     {
-        private readonly IEnumerable<QuickFixBase> _quickFixes;
         private readonly List<IInspectionResult> _results;
 
         public AggregateInspectionResult(List<IInspectionResult> encapsulatedResults)
@@ -18,17 +18,14 @@ namespace Rubberduck.Inspections.Results
                 throw new InvalidOperationException("Cannot aggregate results without results, wtf?");
             }
             _results = encapsulatedResults;
-            _quickFixes = new QuickFixBase[]{};
-            // If there were any reasonable way to provide this mess with a fix, we'd put it here
         }
-
-        //public override IEnumerable<QuickFixBase> QuickFixes { get { return _quickFixes; } }
+        
 
         public string Description
         {
             get
             {
-                return string.Format(InspectionsUI.AggregateInspectionResultFormat, _results[0].Inspection.Meta, _results.Count);
+                return string.Format(InspectionsUI.AggregateInspectionResultFormat, _results[0].Inspection.Description, _results.Count);
             }
         }
 
@@ -52,25 +49,48 @@ namespace Rubberduck.Inspections.Results
         {
             get
             {
-                throw new NotImplementedException();
+                return Enumerable.Empty<QuickFixBase>();
             }
         }
 
         public int CompareTo(object obj)
         {
-            throw new NotImplementedException();
+            if (obj == this)
+            {
+                return 0;
+            }
+            IInspectionResult result = obj as IInspectionResult;
+            return result == null ? -1 : CompareTo(result);
         }
 
         public int CompareTo(IInspectionResult other)
         {
-            throw new NotImplementedException();
+            if (other == this)
+            {
+                return 0;
+            }
+            AggregateInspectionResult result = other as AggregateInspectionResult;
+            if (result == null)
+            {
+                return -1;
+            }
+            if (_results.Count != result._results.Count) {
+                return _results.Count - result._results.Count;
+            }
+            for (int i = 0; i < _results.Count; i++)
+            {
+                if (_results[i].CompareTo(result._results[i]) != 0)
+                {
+                    return _results[i].CompareTo(result._results[i]);
+                }
+            }
+            return 0;
         }
 
         public object[] ToArray()
         {
-            throw new NotImplementedException();
+            var module = QualifiedSelection.QualifiedName;
+            return new object[] { Inspection.Severity.ToString(), module.ProjectName, module.ComponentName, Description, QualifiedSelection.Selection.StartLine, QualifiedSelection.Selection.StartColumn };
         }
-
-        
     }
 }
