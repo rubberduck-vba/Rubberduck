58b650b94247ce2f6c71683e1e2f9e87919a95f7
comintern <brent.mckibbin@gmail.com>
2017-02-16 20:15:10 -0600

    Fixes for focus events to catch designer windows. Still needs work in App.cs

--- Rubberduck.VBEEditor/Events/WindowChangedEventArgs.cs
+++ Rubberduck.VBEEditor/Events/WindowChangedEventArgs.cs
@@ -13,12 +13,14 @@ namespace Rubberduck.VBEditor.Events
 
         public IntPtr Hwnd { get; private set; }
         public IWindow Window { get; private set; }
+        public ICodePane CodePane { get; private set; }
         public FocusType EventType { get; private set; }
 
-        public WindowChangedEventArgs(IntPtr hwnd, IWindow window, FocusType type)
+        public WindowChangedEventArgs(IntPtr hwnd, IWindow window, ICodePane pane, FocusType type)
         {
             Hwnd = hwnd;
             Window = window;
+            CodePane = pane;
             EventType = type;
         }
     }
