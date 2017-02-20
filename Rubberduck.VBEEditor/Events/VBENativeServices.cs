eb45a04efd034efa45e543c27f9264a9ae8cb9a7
comintern <brent.mckibbin@gmail.com>
2017-02-19 19:04:20 -0600

    Test for treeview focus before firing OnWindowFocusChange for selection changes.

--- Rubberduck.VBEEditor/Events/VBENativeServices.cs
+++ Rubberduck.VBEEditor/Events/VBENativeServices.cs
@@ -93,8 +93,7 @@ namespace Rubberduck.VBEditor.Events
             {
                 //Test to see if it was a selection change in the project window.
                 var parent = User32.GetParent(hwnd);
-                //TODO: Need to test to see if the Project window is active before raising this.
-                if (parent != IntPtr.Zero && parent.ToWindowType() == WindowType.Project)
+                if (parent != IntPtr.Zero && parent.ToWindowType() == WindowType.Project && hwnd == User32.GetFocus())
                 {
                     FocusDispatcher(_vbe, new WindowChangedEventArgs(parent, null, null, FocusType.ChildFocus));
                 }                
