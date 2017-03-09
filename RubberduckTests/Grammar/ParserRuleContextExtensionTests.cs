81c04aff071ade1e5ff172c2538d4ab081ec6631
Abraham Hosch <hosch250@live.com>
2017-03-09 14:21:10 -0600

    Grammar tests

--- RubberduckTests/Grammar/ParserRuleContextExtensionTests.cs
+++ RubberduckTests/Grammar/ParserRuleContextExtensionTests.cs
@@ -1,55 +1,50 @@
-//using System.Linq;
-//using System.Threading;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Rubberduck.Parsing;
-//using Rubberduck.Parsing.Grammar;
-//using Rubberduck.Parsing.VBA;
-//using Rubberduck.VBEditor;
-//using Rubberduck.VBEditor.SafeComWrappers.Abstract;
-//using RubberduckTests.Mocks;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Parsing;
+using Rubberduck.Parsing.Grammar;
+using Rubberduck.VBEditor;
+using Rubberduck.VBEditor.SafeComWrappers.Abstract;
+using RubberduckTests.Mocks;
 
-//namespace RubberduckTests.Grammar
-//{
-//    //https://github.com/rubberduck-vba/Rubberduck/issues/2164
-//    [TestClass]
-//    public class ParserRuleContextExtensionTests
-//    {
-//        [TestMethod]
-//        [TestCategory("Inspections")]
-//        public void Evil_Code_Selection_Not_Evil()
-//        {
-//            const string inputCode =
-//@" _
-// _
-// Function _
-// _
-// Foo _
-// _
-// ( _
-// _
-// fizz _
-// _
-// ) As Boolean
+namespace RubberduckTests.Grammar
+{
+    //https://github.com/rubberduck-vba/Rubberduck/issues/2164
+    [TestClass]
+    public class ParserRuleContextExtensionTests
+    {
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void Evil_Code_Selection_Not_Evil()
+        {
+            const string inputCode =
+@" _
+ _
+ Function _
+ _
+ Foo _
+ _
+ ( _
+ _
+ fizz _
+ _
+ ) As Boolean
 
-// End _
-// _
-// Function";
+ End _
+ _
+ Function";
 
-//            //Arrange
-//            var builder = new MockVbeBuilder();
-//            IVBComponent component;
-//            var vbe = builder.BuildFromSingleStandardModule(inputCode, out component);
-//            var parser = MockParser.Create(vbe.Object, new RubberduckParserState(vbe.Object));
+            //Arrange
+            IVBComponent component;
+            var vbe = MockVbeBuilder.BuildFromSingleStandardModule(inputCode, out component);
 
-//            parser.Parse(new CancellationTokenSource());
-//            if (parser.State.Status >= ParserState.Error) { Assert.Inconclusive("Parser Error"); }
+            var state = MockParser.CreateAndParse(vbe.Object);
 
-//            var declaration = parser.State.AllDeclarations.Single(d => d.IdentifierName.Equals("Foo"));
+            var declaration = state.AllDeclarations.Single(d => d.IdentifierName.Equals("Foo"));
 
-//            var actual = ((VBAParser.FunctionStmtContext)declaration.Context).GetProcedureSelection();
-//            var expected = new Selection(3, 2, 11, 14);
+            var actual = ((VBAParser.FunctionStmtContext)declaration.Context).GetProcedureSelection();
+            var expected = new Selection(3, 2, 11, 14);
 
-//            Assert.AreEqual(actual, expected);
-//        }
-//    }
-//}
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
