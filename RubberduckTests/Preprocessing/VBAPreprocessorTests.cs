e247d45ed70ebc7b42b37d317c4cfb9fb8d7a59f
Abraham Hosch <hosch250@live.com>
2017-03-09 14:14:30 -0600

    Preprocessor stuff

--- RubberduckTests/Preprocessing/VBAPreprocessorTests.cs
+++ RubberduckTests/Preprocessing/VBAPreprocessorTests.cs
@@ -1,66 +1,58 @@
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Rubberduck.Parsing.VBA;
-//using RubberduckTests.Mocks;
-//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Threading;
-//using Rubberduck.VBEditor.SafeComWrappers.Abstract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RubberduckTests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
-//namespace RubberduckTests.Preprocessing
-//{
-//    [TestClass]
-//    public class VBAPreprocessorTests
-//    {
-//        [TestMethod]
-//        [DeploymentItem(@"Testfiles\")]
-//        public void TestPreprocessor()
-//        {
-//            foreach (var testfile in GetTestFiles())
-//            {
-//                var filename = testfile.Item1;
-//                var code = testfile.Item2;
-//                var expectedProcessed = testfile.Item3;
-//                var actualProcessed = Parse(code);
-//                AssertParseResult(filename, expectedProcessed, actualProcessed);
-//            }
-//        }
+namespace RubberduckTests.Preprocessing
+{
+    [TestClass]
+    public class VBAPreprocessorTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"Testfiles\")]
+        public void TestPreprocessor()
+        {
+            foreach (var testfile in GetTestFiles())
+            {
+                var filename = testfile.Item1;
+                var code = testfile.Item2;
+                var expectedProcessed = testfile.Item3;
+                var actualProcessed = Parse(code);
+                AssertParseResult(filename, expectedProcessed, actualProcessed);
+            }
+        }
 
-//        private void AssertParseResult(string filename, string originalCode, string materializedParseTree)
-//        {
-//            Assert.AreEqual(originalCode, materializedParseTree, string.Format("{0} mismatch detected.", filename));
-//        }
+        private void AssertParseResult(string filename, string originalCode, string materializedParseTree)
+        {
+            Assert.AreEqual(originalCode, materializedParseTree, string.Format("{0} mismatch detected.", filename));
+        }
 
-//        private IEnumerable<Tuple<string, string, string>> GetTestFiles()
-//        {
-//            // Reference_Module_1 = raw, unprocessed code.
-//            // Reference_Module_1_Processed = result of preprocessor.
-//            var all = Directory.EnumerateFiles("Preprocessor").ToList();
-//            var rawAndProcessed = all
-//                .Where(file => !file.Contains("_Processed"))
-//                .Select(file => Tuple.Create(file, all.First(f => f.Contains(Path.GetFileNameWithoutExtension(file)) && f.Contains("_Processed")))).ToList();
-//            return rawAndProcessed
-//                .Select(file =>
-//                    Tuple.Create(file.Item1, File.ReadAllText(file.Item1), File.ReadAllText(file.Item2))).ToList();
-//        }
+        private IEnumerable<Tuple<string, string, string>> GetTestFiles()
+        {
+            // Reference_Module_1 = raw, unprocessed code.
+            // Reference_Module_1_Processed = result of preprocessor.
+            var all = Directory.EnumerateFiles("Preprocessor").ToList();
+            var rawAndProcessed = all
+                .Where(file => !file.Contains("_Processed"))
+                .Select(file => Tuple.Create(file, all.First(f => f.Contains(Path.GetFileNameWithoutExtension(file)) && f.Contains("_Processed")))).ToList();
+            return rawAndProcessed
+                .Select(file =>
+                    Tuple.Create(file.Item1, File.ReadAllText(file.Item1), File.ReadAllText(file.Item2))).ToList();
+        }
 
-//        private string Parse(string code)
-//        {
-//            var builder = new MockVbeBuilder();
-//            IVBComponent component;
-//            var vbe = builder.BuildFromSingleStandardModule(code, out component);
-//            var state = new RubberduckParserState(vbe.Object);
-//            var parser = MockParser.Create(vbe.Object, state);
-//            parser.Parse(new CancellationTokenSource());
-//            if (parser.State.Status == ParserState.Error)
-//            {
-//                Assert.Inconclusive("Parser Error");
-//            }
-//            var tree = state.GetParseTree(component);
-//            var parsed = tree.GetText();
-//            var withoutEOF = parsed.Substring(0, parsed.Length - 5);
-//            return withoutEOF;
-//        }
-//    }
-//}
+        private string Parse(string code)
+        {
+            IVBComponent component;
+            var vbe = MockVbeBuilder.BuildFromSingleStandardModule(code, out component);
+            
+            var state = MockParser.CreateAndParse(vbe.Object);
+            var tree = state.GetParseTree(component);
+            var parsed = tree.GetText();
+            var withoutEOF = parsed.Substring(0, parsed.Length - 5);
+            return withoutEOF;
+        }
+    }
+}
