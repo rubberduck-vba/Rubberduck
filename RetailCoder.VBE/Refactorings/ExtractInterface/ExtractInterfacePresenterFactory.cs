79ef63c9aec2caa0c647f6717bd3b3dd32786140
Abraham Hosch <hosch250@live.com>
2017-03-16 16:54:16 -0500

    Replace Extract Interface dialog

--- RetailCoder.VBE/Refactorings/ExtractInterface/ExtractInterfacePresenterFactory.cs
+++ RetailCoder.VBE/Refactorings/ExtractInterface/ExtractInterfacePresenterFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using Rubberduck.Parsing.VBA;
+using Rubberduck.UI.Refactorings;
+using Rubberduck.UI.Refactorings.ExtractInterface;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
 namespace Rubberduck.Refactorings.ExtractInterface
@@ -7,10 +9,10 @@ namespace Rubberduck.Refactorings.ExtractInterface
     public class ExtractInterfacePresenterFactory : IRefactoringPresenterFactory<ExtractInterfacePresenter>
     {
         private readonly IVBE _vbe;
-        private readonly IExtractInterfaceDialog _view;
+        private readonly IRefactoringDialog<ExtractInterfaceViewModel> _view;
         private readonly RubberduckParserState _state;
 
-        public ExtractInterfacePresenterFactory(IVBE vbe, RubberduckParserState state, IExtractInterfaceDialog view)
+        public ExtractInterfacePresenterFactory(IVBE vbe, RubberduckParserState state, IRefactoringDialog<ExtractInterfaceViewModel> view)
         {
             _vbe = vbe;
             _view = view;
