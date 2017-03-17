3e2fdd6535781c6354c76161660fae99354efe33
Abraham Hosch <hosch250@live.com>
2017-03-17 11:59:56 -0500

    Clean up

--- RetailCoder.VBE/UI/CodeExplorer/Commands/AddClassModuleCommand.cs
+++ RetailCoder.VBE/UI/CodeExplorer/Commands/AddClassModuleCommand.cs
@@ -1,81 +1,28 @@
-using System.Runtime.InteropServices;
 using NLog;
 using Rubberduck.Navigation.CodeExplorer;
-using Rubberduck.Parsing.Symbols;
 using Rubberduck.UI.Command;
 using Rubberduck.VBEditor.SafeComWrappers;
-using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
 namespace Rubberduck.UI.CodeExplorer.Commands
 {
     [CodeExplorerCommand]
     public class AddClassModuleCommand : CommandBase
     {
-        private readonly IVBE _vbe;
+        private readonly AddComponentCommand _addComponentCommand;
 
-        public AddClassModuleCommand(IVBE vbe) : base(LogManager.GetCurrentClassLogger())
+        public AddClassModuleCommand(AddComponentCommand addComponentCommand) : base(LogManager.GetCurrentClassLogger())
         {
-            _vbe = vbe;
+            _addComponentCommand = addComponentCommand;
         }
 
         protected override bool CanExecuteImpl(object parameter)
         {
-            try
-            {
-                return GetDeclaration(parameter) != null || _vbe.VBProjects.Count == 1;
-            }
-            catch (COMException)
-            {
-                return false;
-            }
+            return _addComponentCommand.CanAddComponent(parameter as CodeExplorerItemViewModel);
         }
 
         protected override void ExecuteImpl(object parameter)
         {
-            var folderAnnotation = $"'@Folder(\"{GetFolder(parameter)}\")";
-
-            if (parameter != null)
-            {
-                var components = GetDeclaration(parameter).Project.VBComponents;
-                var newComponent = components.Add(ComponentType.ClassModule);
-                newComponent.CodeModule.AddFromString(folderAnnotation);
-            }
-            else
-            {
-                var project = _vbe.ActiveVBProject;
-                var components = project.VBComponents;
-                var newComponent = components.Add(ComponentType.ClassModule);
-                newComponent.CodeModule.AddFromString(folderAnnotation);
-            }
-        }
-
-        private Declaration GetDeclaration(object parameter)
-        {
-            var node = parameter as CodeExplorerItemViewModel;
-            while (node != null && !(node is ICodeExplorerDeclarationViewModel))
-            {
-                node = node.Parent;
-            }
-
-            return ((ICodeExplorerDeclarationViewModel) node)?.Declaration;
-        }
-
-        private string GetFolder(object parameter)
-        {
-            if (parameter == null)
-            {
-                return "VBAProject";
-            }
-
-            var declarationNode = parameter as ICodeExplorerDeclarationViewModel;
-            if (declarationNode != null)
-            {
-                return string.IsNullOrEmpty(declarationNode.Declaration.CustomFolder)
-                    ? "VBAProject"
-                    : declarationNode.Declaration.CustomFolder.Replace("\"", string.Empty);
-            }
-
-            return ((CodeExplorerCustomFolderViewModel)parameter).FullPath;
+            _addComponentCommand.AddComponent(parameter as CodeExplorerItemViewModel, ComponentType.ClassModule);
         }
     }
 }
