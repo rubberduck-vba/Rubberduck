6c7418a2eb26f415b53c6aa43ac11369d69dc0c0
Abraham Hosch <hosch250@live.com>
2017-04-05 19:20:58 -0500

    Update IgnoreOnceQuickFix

--- Rubberduck.Inspections/QuickFixes/CompositeCodeInspectionFix.cs
+++ Rubberduck.Inspections/QuickFixes/CompositeCodeInspectionFix.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections.Generic;
-using Rubberduck.Inspections.Abstract;
+﻿//using System.Collections.Generic;
+//using Rubberduck.Inspections.Abstract;
 
-namespace Rubberduck.Inspections.QuickFixes
-{
-    public sealed class CompositeCodeInspectionFix : QuickFixBase
-    {
-        private readonly QuickFixBase _root;
-        private readonly List<QuickFixBase> _children;
+//namespace Rubberduck.Inspections.QuickFixes
+//{
+//    public sealed class CompositeCodeInspectionFix : QuickFixBase
+//    {
+//        private readonly QuickFixBase _root;
+//        private readonly List<QuickFixBase> _children;
 
-        public CompositeCodeInspectionFix(QuickFixBase root)
-            : base(root.Context, root.Selection, root.Description)
-        {
-            _root = root;
-            _children = new List<QuickFixBase>();
-        }
+//        public CompositeCodeInspectionFix(QuickFixBase root)
+//            : base(root.Context, root.Selection, root.Description)
+//        {
+//            _root = root;
+//            _children = new List<QuickFixBase>();
+//        }
 
-        public void AddChild(QuickFixBase quickFix)
-        {
-            _children.Add(quickFix);
-        }
+//        public void AddChild(QuickFixBase quickFix)
+//        {
+//            _children.Add(quickFix);
+//        }
 
-        public void Fix(IInspectionResult result)
-        {
-            _root.Fix();
-            _children.ForEach(child => child.Fix());
-        }
-    }
-}
+//        public void Fix(IInspectionResult result)
+//        {
+//            _root.Fix();
+//            _children.ForEach(child => child.Fix());
+//        }
+//    }
+//}
--- Rubberduck.Inspections/QuickFixes/IgnoreOnceQuickFix.cs
+++ Rubberduck.Inspections/QuickFixes/IgnoreOnceQuickFix.cs
@@ -1,54 +1,77 @@
-using Antlr4.Runtime;
-using Rubberduck.Inspections.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Rubberduck.Parsing.Grammar;
+using Rubberduck.Parsing.Inspections.Abstract;
 using Rubberduck.Parsing.Inspections.Resources;
 using Rubberduck.Parsing.VBA;
-using Rubberduck.VBEditor;
 
 namespace Rubberduck.Inspections.QuickFixes
 {
     public class IgnoreOnceQuickFix : IQuickFix
     {
-        private readonly string _annotationText;
+        private static readonly HashSet<Type> _supportedInspections = new HashSet<Type>();
 
-        public IgnoreOnceQuickFix(ParserRuleContext context, QualifiedSelection selection, string inspectionName) 
-            : base(context, selection, InspectionsUI.IgnoreOnce)
+        public IgnoreOnceQuickFix(IEnumerable<IInspection> inspections)
         {
-            _annotationText = "'" + Annotations.AnnotationMarker +
-                              Annotations.IgnoreInspection + ' ' + inspectionName;
+            _supportedInspections.UnionWith(inspections.Select(i => i.GetType()));
         }
 
-        public override bool CanFixInModule { get { return false; } } // not quite "once" if applied to entire module
-        public override bool CanFixInProject { get { return false; } } // use "disable this inspection" instead of ignoring across the project
+        public static IReadOnlyCollection<Type> SupportedInspections => _supportedInspections.ToList();
+
+        public static void AddSupportedInspectionType(Type inspectionType)
+        {
+            if (!inspectionType.GetInterfaces().Contains(typeof(IInspection)))
+            {
+                throw new ArgumentException("Type must implement IInspection", nameof(inspectionType));
+            }
+
+            _supportedInspections.Add(inspectionType);
+        }
+
+        public static void RemoveSupportedInspectionType(Type inspectionType)
+        {
+            if (!inspectionType.GetInterfaces().Contains(typeof(IInspection)))
+            {
+                throw new ArgumentException("Type must implement IInspection", nameof(inspectionType));
+            }
+
+            _supportedInspections.Remove(inspectionType);
+        }
+
+        public bool CanFixInProcedure => false;
+        public bool CanFixInModule => false;
+        public bool CanFixInProject => false;
 
         public void Fix(IInspectionResult result)
         {
-            var module = Selection.QualifiedName.Component.CodeModule;
-            {
-                var insertLine = Selection.Selection.StartLine;
-                while (insertLine != 1 && module.GetLines(insertLine - 1, 1).EndsWith(" _"))
-                {
-                    insertLine--;
-                }
-                var codeLine = insertLine == 1 ? string.Empty : module.GetLines(insertLine - 1, 1);
-                var annotationText = _annotationText;
-                var ignoreAnnotation = "'" + Annotations.AnnotationMarker + Annotations.IgnoreInspection;
+            var annotationText = "'" + Annotations.AnnotationMarker + Annotations.IgnoreInspection + ' ' + result.Inspection;
 
-                int commentStart;
-                if (codeLine.HasComment(out commentStart) && codeLine.Substring(commentStart).StartsWith(ignoreAnnotation))
-                {
-                    var indentation = codeLine.Length - codeLine.TrimStart().Length;
-                    annotationText = string.Format("{0}{1},{2}", 
-                                                   new string(' ', indentation), 
-                                                   _annotationText,
-                                                   codeLine.Substring(indentation + ignoreAnnotation.Length));
-                    module.ReplaceLine(insertLine - 1, annotationText);
-                }
-                else
-                {
-                    module.InsertLines(insertLine, annotationText);
-                }
+            var module = result.QualifiedSelection.QualifiedName.Component.CodeModule;
+            var insertLine = result.QualifiedSelection.Selection.StartLine;
+            while (insertLine != 1 && module.GetLines(insertLine - 1, 1).EndsWith(" _"))
+            {
+                insertLine--;
             }
+            var codeLine = insertLine == 1 ? string.Empty : module.GetLines(insertLine - 1, 1);
+            var ignoreAnnotation = "'" + Annotations.AnnotationMarker + Annotations.IgnoreInspection;
+
+            int commentStart;
+            if (codeLine.HasComment(out commentStart) && codeLine.Substring(commentStart).StartsWith(ignoreAnnotation))
+            {
+                var indentation = codeLine.Length - codeLine.TrimStart().Length;
+                annotationText = $"{new string(' ', indentation)}{annotationText},{codeLine.Substring(indentation + ignoreAnnotation.Length)}";
+                module.ReplaceLine(insertLine - 1, annotationText);
+            }
+            else
+            {
+                module.InsertLines(insertLine, annotationText);
+            }
+        }
+
+        public string Description(IInspectionResult result)
+        {
+            return InspectionsUI.IgnoreOnce;
         }
     }
 }
