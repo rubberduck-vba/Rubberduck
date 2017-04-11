2204314323aa6c58b298935e1f05554627491099
Brian Zenger <bzngr@outlook.com>
2017-04-11 07:39:49 -0700

    Fixes #2925

--- RetailCoder.VBE/Root/RubberduckModule.cs
+++ RetailCoder.VBE/Root/RubberduckModule.cs
@@ -306,6 +306,11 @@ namespace Rubberduck.Root
             foreach (var quickFix in quickFixes)
             {
                 Bind<IQuickFix>().To(quickFix).InSingletonScope();
+                if (quickFix.Name.Equals("AssignedByValParameterMakeLocalCopyQuickFix"))
+                {
+                    Bind<IAssignedByValParameterQuickFixDialogFactory>().To<AssignedByValParameterQuickFixDialogFactory>()
+                        .WhenInjectedInto(quickFix);
+                }
             }
         }
 
