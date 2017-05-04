cef482530c421c89123fdacdc13d992d2d510527
Abraham Hosch <hosch250@live.com>
2017-05-04 13:45:13 -0500

    Split inspection results into Declaration/Reference/Context results

---
+++ Rubberduck.Inspections/Results/DeclarationInspectionResult.cs
@@ -0,0 +1,36 @@
+﻿using Rubberduck.Inspections.Abstract;
+using Rubberduck.Parsing.Inspections.Abstract;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.VBEditor;
+
+namespace Rubberduck.Inspections.Results
+{
+    class DeclarationInspectionResult : InspectionResultBase
+    {
+        public DeclarationInspectionResult(IInspection inspection, string description, Declaration target) :
+            base(inspection,
+                 description,
+                 target.QualifiedName.QualifiedModuleName,
+                 target.Context,
+                 target,
+                 target.QualifiedSelection,
+                 GetQualifiedMemberName(target))
+        {
+        }
+        
+        private static QualifiedMemberName? GetQualifiedMemberName(Declaration target)
+        {
+            if (string.IsNullOrEmpty(target?.QualifiedName.QualifiedModuleName.ComponentName))
+            {
+                return null;
+            }
+
+            if (target.DeclarationType.HasFlag(DeclarationType.Member))
+            {
+                return target.QualifiedName;
+            }
+
+            return GetQualifiedMemberName(target.ParentDeclaration);
+        }
+    }
+}
---
+++ Rubberduck.Inspections/Results/IdentifierReferenceInspectionResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Rubberduck.Inspections.Abstract;
+using Rubberduck.Parsing;
+using Rubberduck.Parsing.Inspections.Abstract;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.VBEditor;
+
+namespace Rubberduck.Inspections.Results
+{
+    class IdentifierReferenceInspectionResult : InspectionResultBase
+    {
+        public IdentifierReferenceInspectionResult(IInspection inspection, string description, RubberduckParserState state, IdentifierReference reference) :
+            base(inspection,
+                 description,
+                 reference.QualifiedModuleName,
+                 reference.Context,
+                 reference.Declaration,
+                 new QualifiedSelection(reference.QualifiedModuleName, reference.Context.GetSelection()),
+                 GetQualifiedMemberName(state, reference))
+        {
+        }
+
+        private static QualifiedMemberName? GetQualifiedMemberName(RubberduckParserState state, IdentifierReference reference)
+        {
+            var members = state.DeclarationFinder.Members(reference.QualifiedModuleName);
+            return members.SingleOrDefault(m => m.Selection.Contains(reference.Selection))?.QualifiedName;
+        }
+    }
+}
