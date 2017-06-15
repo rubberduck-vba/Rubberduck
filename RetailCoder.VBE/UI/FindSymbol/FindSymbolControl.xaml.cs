036324b1660d92181ec2b9945ae87b19add9fdd6
IvenBach <ivenbach@gmail.com>
2017-06-15 00:17:48 -0700

    Addresses issue #3037, and issue #3031.

--- RetailCoder.VBE/UI/FindSymbol/FindSymbolControl.xaml.cs
+++ RetailCoder.VBE/UI/FindSymbol/FindSymbolControl.xaml.cs
@@ -37,7 +37,8 @@ namespace Rubberduck.UI.FindSymbol
 
         private void UIElement_OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter && ViewModel.CanExecute())
+            //if (e.Key == Key.Enter && ViewModel.CanExecute())
+            if (e.Key == Key.Enter && ViewModel.CanExecute()) 
             {
                 ViewModel.Execute();
                 e.Handled = true;
