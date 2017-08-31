650e47f9a41c4ba96c757d422d7c60f42efdf570
Pieter Geerkens <pgeerkens@hotmail.com>
2017-08-30 23:01:02 -0400

    Added copyright notice to all files.

--- Rubberduck.RibbonDispatcher/Abstract/IActionItem.cs
+++ Rubberduck.RibbonDispatcher/Abstract/IActionItem.cs
@@ -1,4 +1,7 @@
-﻿namespace Rubberduck.RibbonDispatcher.Abstract {
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                Copyright (c) 2017 Pieter Geerkens                              //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Rubberduck.RibbonDispatcher.Abstract {
     /// <summary>The total interface (required to be) exposed externally by RibbonButton objects.</summary>
     public interface IActionItem {
         /// <summary>TODO</summary>
