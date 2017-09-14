18f0ca2bd046a47c3c3c87a6dd6b50f81a1f2453
Brian Zenger <bzngr@outlook.com>
2017-09-14 16:07:04 -0700

    WIP initial modifications
    Handles some use cases.  Does not handle 'IS' statements.

---
+++ Rubberduck.Inspections/Concrete/UnreachableCaseInspection.cs
@@ -0,0 +1,298 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using Rubberduck.Inspections.Abstract;
+using Rubberduck.Inspections.Results;
+using Rubberduck.Parsing;
+using Rubberduck.Parsing.Grammar;
+using Rubberduck.Parsing.Inspections.Abstract;
+using Rubberduck.Parsing.Inspections.Resources;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.VBEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Rubberduck.Inspections.Concrete
+{
+    internal class UnreachableCaseInspection : ParseTreeInspectionBase
+    {
+        private string _typeName;
+        private List<long> _priorLongCaseClauseValues = new List<long>();
+        private List<string> _priorStringCaseClauseValues = new List<string>();
+
+        private readonly string _unreachableCaseInspectionResultFormat = "Unreachable or conflicting Case block";
+        public UnreachableCaseInspection(RubberduckParserState state)
+            : base(state, CodeInspectionSeverity.Suggestion) { }
+
+        public override IInspectionListener Listener { get; } =
+            new UnreachableCaseInspectionListener();
+
+        public override Type Type => typeof(UnreachableCaseInspection);
+
+        public override CodeInspectionType InspectionType => CodeInspectionType.CodeQualityIssues;
+
+
+        public override IEnumerable<IInspectionResult> GetInspectionResults()
+        {
+            var selectCaseContexts = Listener.Contexts
+                .Where(result => !IsIgnoringInspectionResultFor(result.ModuleName, result.Context.Start.Line));
+            //.Select(result => new QualifiedContextInspectionResult(this,
+            //                                        _unreachableCaseInspectionResultFormat,
+            //                                        result));
+
+
+            var inspResults = new List<IInspectionResult>();
+            foreach( var selectStmt in selectCaseContexts)
+            {
+                inspResults.AddRange(GetUnreachableCaseBlocks(selectStmt));
+            }
+            return inspResults;
+        }
+
+        private IEnumerable<IInspectionResult> GetUnreachableCaseBlocks(QualifiedContext<ParserRuleContext> selectCaseStmt)
+        {
+            _priorLongCaseClauseValues = new List<long>();
+            _priorStringCaseClauseValues = new List<string>();
+            var theRef = GetTheSelectCaseReference(selectCaseStmt);
+            var theDec = theRef.Declaration;
+            _typeName = theDec.AsTypeName;
+
+            var caseClauses = ParserRuleContextHelper.GetDescendents<VBAParser.CaseClauseContext>(selectCaseStmt.Context);
+
+            var qualifiedCaseClauses = new List<QualifiedContext<ParserRuleContext>>();
+            caseClauses.ForEach(clause => qualifiedCaseClauses.Add(new QualifiedContext<ParserRuleContext>(selectCaseStmt.MemberName, clause as ParserRuleContext)));
+
+            return InspectCaseClauses(selectCaseStmt, qualifiedCaseClauses);
+        }
+
+        private IdentifierReference GetTheSelectCaseReference(QualifiedContext<ParserRuleContext> selectCaseStmt)
+        {
+            var selectCaseExpr = ParserRuleContextHelper.GetChild<VBAParser.SelectExpressionContext>(selectCaseStmt.Context);
+
+            var allRefs = new List<IdentifierReference>();
+            foreach (var dec in State.DeclarationFinder.MatchName(selectCaseExpr.GetText()))
+            {
+                allRefs.AddRange(dec.References);
+            }
+
+            var theRef = allRefs.Where(rf => ParserRuleContextHelper.HasParent(rf.Context, selectCaseStmt.Context));
+
+            Debug.Assert(theRef.Count() == 1);
+
+            return theRef.First();
+        }
+
+        private List<IInspectionResult> InspectCaseClauses(QualifiedContext<ParserRuleContext> selectCaseStmt, List<QualifiedContext<ParserRuleContext>> caseClauses)
+        {
+            Func<string, string, bool> compareEqualStrings = delegate (string s, string t)
+            { return s.Equals(t, StringComparison.InvariantCulture); };
+
+            Func<long, long, bool> compareEqualLongs = delegate (long s, long t)
+            { return s == t; };
+
+            Func<long, long, bool> isGreaterThanLong = delegate (long s, long test)
+            { return s > test; };
+
+            var stringClauseEvaluations = new List<KeyValuePair<string, Func<string, string, bool>>>();
+            var longClauseEvaluations = new List<KeyValuePair<long, Func<long, long, bool>>>();
+
+            var inspResults = new List<IInspectionResult>();
+ 
+            foreach (var caseClause in caseClauses)
+            {
+                var rangeClauses = ParserRuleContextHelper.GetDescendents<VBAParser.RangeClauseContext>(caseClause.Context as ParserRuleContext);
+                foreach (var ctxt in rangeClauses )
+                {
+         
+                    if (_typeName.Equals("String"))
+                    {
+                        var literalExpressionContexts = ParserRuleContextHelper.GetDescendents<VBAParser.LiteralExpressionContext>(ctxt as ParserRuleContext);
+                        foreach (var child in literalExpressionContexts)
+                        {
+                            var text = child.GetText();
+                            bool addInspResult = false;
+                            foreach(var cTest in stringClauseEvaluations)
+                            {
+                                if(cTest.Value(cTest.Key, text))
+                                {
+                                    addInspResult = true;
+                                }
+                            }
+                            if (addInspResult ) //PriorCaseClauseContainsValue(text))
+                            {
+                                inspResults = AddInspectionResult(caseClause, inspResults);
+                            }
+                            else
+                            {
+                                var clauseTest = new KeyValuePair<string, Func<string, string, bool>>(text, compareEqualStrings);
+                                stringClauseEvaluations.Add(clauseTest);
+                                AddPriorCaseClauseValue(text);
+                            }
+                        }
+                    }
+                    if (_typeName.Equals("Long"))
+                    {
+                        bool hasStartEndContexts = false;
+                        var selectStartValue = ParserRuleContextHelper.GetChild<VBAParser.SelectStartValueContext>(ctxt);
+                        if (selectStartValue != null)
+                        {
+                            hasStartEndContexts = true;
+                            bool unparsableLong = false;
+                            var selectEndValue = ParserRuleContextHelper.GetChild<VBAParser.SelectEndValueContext>(ctxt);
+                            long startSelectValue;
+                            if (!long.TryParse(selectStartValue.GetText(), out startSelectValue))
+                            {
+                                unparsableLong = true;
+                                inspResults = AddInspectionResult(caseClause, inspResults);
+                            }
+                            long endSelectValue;
+                            if (!long.TryParse(selectEndValue.GetText(), out endSelectValue))
+                            {
+                                unparsableLong = true;
+                                inspResults = AddInspectionResult(caseClause, inspResults);
+                            }
+                            if (!unparsableLong)
+                            {
+
+                                for (long selectVal = startSelectValue; selectVal <= endSelectValue; selectVal++)
+                                {
+                                    bool addInspResult = false;
+                                    foreach (var cTest in longClauseEvaluations)
+                                    {
+                                        if (cTest.Value(cTest.Key, selectVal))
+                                        {
+                                            addInspResult = true;
+                                        }
+                                    }
+                                    if (addInspResult ) //PriorCaseClauseContainsValue(selectVal))
+                                    {
+                                        inspResults = AddInspectionResult(caseClause, inspResults);
+                                    }
+                                    else
+                                    {
+                                        var clauseTest = new KeyValuePair<long, Func<long, long, bool>>(selectVal, compareEqualLongs);
+                                        longClauseEvaluations.Add(clauseTest);
+                                        AddPriorCaseClauseValue(selectVal);
+                                    }
+                                }
+                            }
+                        }
+
+                        //bool hasComparisonOperatorContexts = false;
+                        //var comparisonOperatorContexts = ParserRuleContextHelper.GetDescendents<VBAParser.ComparisonOperatorContext>(ctxt as ParserRuleContext);
+                        //if (comparisonOperatorContexts.Any() && !hasStartEndContexts)
+                        //{
+                        //    hasComparisonOperatorContexts = true;
+                        //    foreach (var child in comparisonOperatorContexts)
+                        //    {
+                        //        string theOp = child.GetText();
+                        //        var theValue = ParserRuleContextHelper.GetDescendent<VBAParser.LExprContext>(child.Parent);
+                        //        if (PriorCaseClauseContainsValue(theValue))
+                        //        {
+                        //            inspResults = AddInspectionResult(caseClause, inspResults);
+                        //        }
+                        //        else
+                        //        {
+                        //            AddPriorCaseClauseValue(theValue);
+                        //        }
+                        //    }
+                        //}
+
+                        var literalExpressionContexts = ParserRuleContextHelper.GetDescendents<VBAParser.LiteralExpressionContext>(ctxt as ParserRuleContext);
+                        if( literalExpressionContexts.Any() & !hasStartEndContexts)
+                        {
+                            foreach (var child in literalExpressionContexts)
+                            {
+                                long theValue;
+                                if (!long.TryParse(child.GetText(), out theValue))
+                                {
+                                    inspResults = AddInspectionResult(caseClause, inspResults);
+                                }
+                                else
+                                {
+                                    bool addInspResult = false;
+                                    foreach (var cTest in longClauseEvaluations)
+                                    {
+                                        if (cTest.Value(cTest.Key, theValue))
+                                        {
+                                            addInspResult = true;
+                                        }
+                                    }
+                                    if (addInspResult ) //PriorCaseClauseContainsValue(theValue))
+                                    {
+                                        inspResults = AddInspectionResult(caseClause, inspResults);
+                                    }
+                                    else
+                                    {
+                                        var clauseTest = new KeyValuePair<long, Func<long, long, bool>>(theValue, compareEqualLongs);
+                                        longClauseEvaluations.Add(clauseTest);
+                                        AddPriorCaseClauseValue(theValue);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return inspResults;
+        }
+
+        private List<IInspectionResult> AddInspectionResult(QualifiedContext<ParserRuleContext> result, List<IInspectionResult> inspResults)
+        {
+            inspResults.Add(new QualifiedContextInspectionResult(this,
+                    _unreachableCaseInspectionResultFormat,
+                    result));
+            return inspResults;
+        }
+
+        private void AddPriorCaseClauseValue(long theValue)
+        {
+            _priorLongCaseClauseValues.Add(theValue);
+        }
+
+        private bool PriorCaseClauseContainsValue(long theValue)
+        {
+            return _priorLongCaseClauseValues.Contains(theValue);
+        }
+
+        private void AddPriorCaseClauseValue(string theValue)
+        {
+            _priorStringCaseClauseValues.Add(theValue);
+        }
+
+        private bool PriorCaseClauseContainsValue(string theValue)
+        {
+            return _priorStringCaseClauseValues.Contains(theValue);
+        }
+
+        private void VBATypes()
+        {
+            string[] types = {"Boolean", "Integer",
+                    "Long", "Single","Double(negative)",
+                    "Double(positive)", "Currency",
+            "Date", "String", "Object", "Variant", "User defined" };
+        }
+
+        public class UnreachableCaseInspectionListener : VBAParserBaseListener, IInspectionListener
+        {
+            private readonly List<QualifiedContext<ParserRuleContext>> _contexts = new List<QualifiedContext<ParserRuleContext>>();
+            public IReadOnlyList<QualifiedContext<ParserRuleContext>> Contexts => _contexts;
+
+            public QualifiedModuleName CurrentModuleName { get; set; }
+
+            public void ClearContexts()
+            {
+                _contexts.Clear();
+            }
+
+            public override void EnterSelectCaseStmt([NotNull] VBAParser.SelectCaseStmtContext context)
+            {
+                _contexts.Add(new QualifiedContext<ParserRuleContext>(CurrentModuleName, context));
+            }
+        }
+    }
+}
---
+++ RubberduckTests/Inspections/UnreachableCaseInspectionTests.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rubberduck.Inspections.Concrete;
+using RubberduckTests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RubberduckTests.Inspections
+{
+    [TestClass]
+    public class UnreachableCaseInspectionTests
+    {
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_SingleUnreachableCase()
+        {
+            const string inputCode =
+@"Sub Foo(caseNum As Long)
+
+Const x As String =""Bar""
+Select Case x
+  Case ""Foo"", ""Bar""
+    'Do FooBar
+  Case ""Food""
+    'Unreachable
+  Case ""Bar""
+    'Unreachable
+  Case ""Foodie""
+    'Unreachable
+End Select
+                
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_MultipleUnreachableCases()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x As String =""Bar""
+Select Case x
+  Case ""Foo"", ""Bar""
+    'Do FooBar
+  Case ""Foo""
+    'Unreachable
+  Case ""Bar""
+    'Unreachable
+  Case ""Foo""
+    'Unreachable
+End Select
+                
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 3);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_ReusedSelectExpressionVariable()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x As String =""Bar""
+Select Case x
+  Case ""Foo"", ""Bar""
+    'Do FooBar
+  Case ""Foo""
+    'Unreachable
+  Case ""Bar""
+    'Unreachable
+  Case ""Foo""
+    'Unreachable
+End Select
+                
+Select Case x
+  Case ""Foo"", ""Bar""
+    'Do FooBar
+  Case ""Foo""
+    'Unreachable
+  Case ""Bar""
+    'Unreachable
+  Case ""Food""
+End Select
+                
+End Sub
+
+Private Function AppendBar(x As String) As String
+    AppendBar = x + ""Bar""
+End Function";
+
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 5);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_NumberRange()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x as Long = 7
+
+Select Case x
+  Case 1 to 100
+    'Do FooBar
+  Case 50
+    'Unreachable
+End Select
+
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_SimpleLongCollision()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x as Long = 7
+
+Select Case x
+  Case 1,2,5
+    'Do FooBar
+  Case 2
+    'Unreachable
+End Select
+
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_StringWhereLongShouldBe()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x as Long = 7
+
+Select Case x
+  Case 1 to 49
+    'Do FooBar
+  Case 50
+    'Reachable
+  Case ""Test""
+    'Unreachable
+End Select
+
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_MultipleRanges()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x As Long = 7
+Const maxNumber As Long = 5000
+
+Select Case x
+  Case 1 To 4, 7 To 9, 11, 13, 15 To 20
+    'Do FooBar
+  Case 8
+    'Unreachable
+  Case 11
+    'Unreachable
+  Case 17
+    'Unreachable
+  Case 21
+    'Reachable
+End Select
+
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 3);
+        }
+
+        [TestMethod]
+        [TestCategory("Inspections")]
+        public void UnreachableCaseInspection_IsStmt()
+        {
+            const string inputCode =
+@"Sub Foo()
+
+Const x As Long = 7
+'Const maxNumber As Long = 5000
+
+Select Case x
+  Case Is > 5000
+    'Do FooBar
+  Case 7
+    'Do Foobar again
+  Case 10000
+    'Unreachable
+End Select
+
+End Sub";
+            CheckActualUnreachableBlockCountEqualsExpected(inputCode, 1);
+        }
+
+        private void CheckActualUnreachableBlockCountEqualsExpected(string inputCode, int expectedCount)
+        {
+            var vbe = MockVbeBuilder.BuildFromSingleStandardModule(inputCode, out var _);
+            var state = MockParser.CreateAndParse(vbe.Object);
+
+            var inspection = new UnreachableCaseInspection(state);
+            var inspector = InspectionsHelper.GetInspector(inspection);
+            var actualResults = inspector.FindIssuesAsync(state, CancellationToken.None).Result;
+
+            Assert.AreEqual(expectedCount, actualResults.Count());
+        }
+    }
+}
