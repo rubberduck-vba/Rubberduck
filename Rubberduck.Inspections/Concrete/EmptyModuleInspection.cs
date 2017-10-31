a4919abbce9ee66f61e8ca1f136744567099be62
Max Doerner <maxdoerner@gmx.net>
2017-10-31 19:21:43 +0100

    Emptz module inspection skeleton and tests.

---
+++ Rubberduck.Inspections/Concrete/EmptyModuleInspection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rubberduck.Inspections.Abstract;
+using Rubberduck.Parsing.Inspections.Abstract;
+using Rubberduck.Parsing.Inspections.Resources;
+using Rubberduck.Parsing.VBA;
+
+namespace Rubberduck.Inspections.Concrete
+{
+    class EmptyModuleInspection : InspectionBase
+    {
+        public EmptyModuleInspection(RubberduckParserState state, CodeInspectionSeverity defaultSeverity = CodeInspectionSeverity.Hint) 
+        :base(state, defaultSeverity)
+        { }
+
+        public override CodeInspectionType InspectionType => CodeInspectionType.MaintainabilityAndReadabilityIssues;
+
+        protected override IEnumerable<IInspectionResult> DoGetInspectionResults()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
