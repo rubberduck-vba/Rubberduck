e7b50d464b0274055a4baa4f13bad64a38c8497c
IvenBach <ivenbach@gmail.com>
2017-11-01 20:54:54 -0700

    Closes #3489

--- RetailCoder.VBE/UI/About/AboutControl.xaml.cs
+++ RetailCoder.VBE/UI/About/AboutControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Rubberduck.UI.About
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Rubberduck.UI.About
 {
     /// <summary>
     /// Interaction logic for AboutControl.xaml
@@ -8,6 +11,26 @@
         public AboutControl()
         {
             InitializeComponent();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            bool isControlCPressed = (Keyboard.IsKeyDown(Key.C) && (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)));
+            if (isControlCPressed)
+            {
+                CopyVersionInfoToClipboard();
+            }
+        }
+
+        private void CopyVersionInfoToClipboard()
+        {
+            Clipboard.SetText(this.Version.Text);
+            System.Windows.MessageBox.Show("Version information copied to clipboard.", "Copy successfull");
+        }
+
+        private void CopyVersionInfo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CopyVersionInfoToClipboard();
         }
     }
 }
