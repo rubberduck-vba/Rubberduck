dd7b92c64d9e5c9977e754bdc85a78cbab2a2621
IvenBach <IvenBach@gmail.com>
2017-12-22 11:02:06 -0800

    Add converter

---
+++ RetailCoder.VBE/UI/Settings/Converters/BooleanToInvalidConfigurationIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Rubberduck.UI.Settings.Converters
+{
+    public class BooleanToInvalidConfigurationIconConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? string.Empty : "IsNotValid";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
