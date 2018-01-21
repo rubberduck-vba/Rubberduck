941038c3c384855be5dbaa892db9e9a76df41f64
Wayne Phillips <wayne.phillips@gmail.com>
2018-01-21 20:42:15 +0000

    changed DisposableList to inherit from IList<T> rather than List<T>

--- Rubberduck.VBEEditor/ComManagement/TypeLibs.cs
+++ Rubberduck.VBEEditor/ComManagement/TypeLibs.cs
@@ -122,9 +122,11 @@ namespace Rubberduck.VBEditor.ComManagement.TypeLibs
         }
     }
 
-    public class DisposableList<T> : List<T>, IDisposable
+    public class DisposableList<T> : IList<T>, IDisposable
         where T : IDisposable
     {
+        private readonly IList<T> _list = new List<T>();
+
         public void Dispose() => ((IDisposable)this).Dispose();
 
         private bool _isDisposed;
@@ -133,10 +135,30 @@ namespace Rubberduck.VBEditor.ComManagement.TypeLibs
             if (_isDisposed) return;
             _isDisposed = true;
 
-            foreach (var element in this)
+            foreach (var element in _list)
             {
-                ((IDisposable)element).Dispose();
+                element.Dispose();
             }
+        }
+
+        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Add(T item) => _list.Add(item);
+        public void Clear() => _list.Clear();
+        public bool Contains(T item) => _list.Contains(item);
+        public void CopyTo(T[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
+        public bool Remove(T item) => _list.Remove(item);
+        public int Count { get => _list.Count; }
+        public bool IsReadOnly { get => _list.IsReadOnly; }
+        
+        public int IndexOf(T item) => _list.IndexOf(item);
+        public void Insert(int index, T item) => _list.Insert(index, item);
+        public void RemoveAt(int index) => _list.RemoveAt(index);
+        public T this[int index]
+        {
+            get => _list[index];
+            set => _list[index] = value;
         }
     }
 
