e88e41af8b2a3276f9075a2a51476143382d6058
Max Doerner <maxdoerner@gmx.net>
2018-02-16 17:46:37 +0100

    Made the ComSafeManager return a WeakComSafe.

---
+++ Rubberduck.VBEEditor/ComManagement/ComSafeManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rubberduck.VBEditor.ComManagement
+{
+    public static class ComSafeManager
+    {
+        private static Lazy<IComSafe> _comSafe = new Lazy<IComSafe>(NewComSafe);
+
+        public static IComSafe GetCurrentComSafe()
+        {
+            return _comSafe.Value;
+        }
+
+        public static void ResetComSafe()
+        {
+            _comSafe = new Lazy<IComSafe>(NewComSafe);
+        }
+
+        private static IComSafe NewComSafe()
+        {
+            return new WeakComSafe();
+        }
+    }
+}
