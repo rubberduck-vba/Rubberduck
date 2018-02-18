fb21d14e6ceafc1ce55e4b0e944f9ea1c5a99101
Max Doerner <maxdoerner@gmx.net>
2018-02-18 22:26:27 +0100

    Merged DerivedComSafeTests into the same file as the abstract base test class.

--- RubberduckTests/VBEditor/ComSafeTests.cs
+++ RubberduckTests/VBEditor/ComSafeTests.cs
@@ -187,5 +187,23 @@ namespace RubberduckTests.VBEditor
 
             Assert.IsFalse(result);
         }
+
+        [TestFixture()]
+        public class StrongComSafeTests : ComSafeTestBase
+        {
+            protected override IComSafe TestComSafe()
+            {
+                return new StrongComSafe();
+            }
+        }
+
+        [TestFixture()]
+        public class WeakComSafeTests : ComSafeTestBase
+        {
+            protected override IComSafe TestComSafe()
+            {
+                return new WeakComSafe();
+            }
+        }
     }
 }
