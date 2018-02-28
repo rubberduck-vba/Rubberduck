00f0a6c2b909672a546b6302423c73e26218a24c
Max Doerner <maxdoerner@gmx.net>
2018-02-28 22:54:06 +0100

    Added DisposalActionContainer.

---
+++ Rubberduck.VBEEditor/Utility/DisposalActionContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rubberduck.VBEditor.Utility
+{
+    public sealed class DisposalActionContainer<T>: IDisposable
+    {
+        public T Value { get; }
+        private readonly Action _disposalAction;
+
+        public DisposalActionContainer(T value, Action disposalAction)
+        {
+            Value = value;
+            _disposalAction = disposalAction;
+        }
+
+        private bool _isDisposed = false;
+        private readonly object _disposalLockObject = new object();
+        public void Dispose()
+        {
+            lock (_disposalLockObject)
+            {
+                if (_isDisposed)
+                {
+                    return;
+                }
+                _isDisposed = true;
+            }
+
+            _disposalAction.Invoke();
+        }
+    }
+
+    public static class DisposalActionContainer
+    {
+        public static DisposalActionContainer<T> Create<T>(T value, Action disposalAction)
+        {
+            return new DisposalActionContainer<T>(value, disposalAction);
+        }
+    }
+}
