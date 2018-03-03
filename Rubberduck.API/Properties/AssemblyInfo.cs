d418f7aa94cebe3a895119ee173b20a264d2150c
Vogel612 <vogel612@gmx.de>
2018-03-03 23:56:19 +0100

    Add Assembly Metainformation to Rd.API and retarget to .NET4.5

--- Rubberduck.API/Properties/AssemblyInfo.cs
+++ Rubberduck.API/Properties/AssemblyInfo.cs
@@ -6,13 +6,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rubberduck.API")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("API for programmatic access to Rubberduck's Code Analysis features.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rubberduck-VBA")]
 [assembly: AssemblyProduct("Rubberduck.API")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCulture("en")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("RubberduckTests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
