8c36cd0d01ad4c91de64ea220fcdb0cbddc10bfe
bclothier <bgclothier@gmail.com>
2018-03-02 22:23:35 -0600

    Set up check for Compile On Demand

--- RetailCoder.VBE/UI/Command/ReparseCommand.cs
+++ RetailCoder.VBE/UI/Command/ReparseCommand.cs
@@ -8,6 +8,7 @@ using Rubberduck.Parsing.VBA;
 using Rubberduck.Settings;
 using Rubberduck.SettingsProvider;
 using Rubberduck.UI.CodeExplorer.Commands;
+using Rubberduck.VBERuntime;
 using Rubberduck.VBEditor.ComManagement.TypeLibsAPI;
 using Rubberduck.VBEditor.SafeComWrappers;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
@@ -20,13 +21,15 @@ namespace Rubberduck.UI.Command
     {
         private readonly IVBE _vbe;
         private readonly IVBETypeLibsAPI _typeLibApi;
+        private readonly IVBESettings _vbeSettings;
         private readonly IMessageBox _messageBox;
         private readonly RubberduckParserState _state;
         private readonly GeneralSettings _settings;
 
-        public ReparseCommand(IVBE vbe, IConfigProvider<GeneralSettings> settingsProvider, RubberduckParserState state, IVBETypeLibsAPI typeLibApi, IMessageBox messageBox) : base(LogManager.GetCurrentClassLogger())
+        public ReparseCommand(IVBE vbe, IConfigProvider<GeneralSettings> settingsProvider, RubberduckParserState state, IVBETypeLibsAPI typeLibApi, IVBESettings vbeSettings, IMessageBox messageBox) : base(LogManager.GetCurrentClassLogger())
         {
             _vbe = vbe;
+            _vbeSettings = vbeSettings;
             _typeLibApi = typeLibApi;
             _state = state;
             _settings = settingsProvider.Create();
@@ -46,6 +49,11 @@ namespace Rubberduck.UI.Command
         {
             if (_settings.CompileBeforeParse)
             {
+                if (!VerifyCompileOnDemand())
+                {
+                    return;
+                }
+
                 if (CompileAllProjects(out var failedNames))
                 {
                     if (!PromptUserToContinue(failedNames))
@@ -55,6 +63,20 @@ namespace Rubberduck.UI.Command
                 }
             }
             _state.OnParseRequested(this);
+        }
+
+        private bool VerifyCompileOnDemand()
+        {
+            //Command_Reparse_CompileOnDemandEnabled
+
+            if (_vbeSettings.CompileOnDemand)
+            {
+                return DialogResult.Yes == _messageBox.Show(RubberduckUI.Command_Reparse_CompileOnDemandEnabled,
+                           RubberduckUI.Command_Reparse_CompileOnDemandEnabled_Caption, MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+            }
+
+            return true;
         }
 
         private bool CompileAllProjects(out List<string> failedNames)
