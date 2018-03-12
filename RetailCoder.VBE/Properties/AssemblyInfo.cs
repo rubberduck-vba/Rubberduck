2e23e7fec6630c5a329dc38dd4d689abe3f3e590
bclothier <bgclothier@gmail.com>
2018-03-12 01:50:38 -0500

    First working version of RegistryEntryBuilder Add Wix project to make sure of nuget package for heat.exe Add Powershell script to support automation at the build time for extracting the registry data

--- RetailCoder.VBE/Properties/AssemblyInfo.cs
+++ RetailCoder.VBE/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Rubberduck;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e07c841c-14b4-4890-83e9-8c80b06dd59d")]
+[assembly: Guid(RubberduckGuid.RubberduckTypeLibGuid)]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Rubberduck.Deployment/BuildRegistryScript.ps1
@@ -0,0 +1,53 @@
+﻿param (
+	[Parameter(Mandatory=$true)][string]$builderAssemblyPath,
+	[Parameter(Mandatory=$true)][string]$netToolsDir,
+	[Parameter(Mandatory=$true)][string]$wixToolsDir,
+	[Parameter(Mandatory=$true)][string]$sourceDir,
+	[Parameter(Mandatory=$true)][string]$targetDir,
+	[Parameter(Mandatory=$true)][string]$filesToExtract
+)
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop";
+
+try
+{
+	$separator = "|"
+	$option = [System.StringSplitOptions]::RemoveEmptyEntries
+	$files = $filesToExtract.Split($separator, $option);
+
+	foreach($file in $files)
+	{
+		$sourceDll = $sourceDir + $file;
+		$targetDll = $targetDir + $file;
+		$sourceTlb = $sourceDir + ($file -replace ".dll", ".tlb");
+		$targetTlb = $targetDir + ($file -replace ".dll", ".tlb");
+		$dllXml = $targetDll + ".xml"
+		$tlbXml = $targetTlb + ".xml"
+
+		$cmd = "{0}tlbexp.exe" -f $netToolsDir;
+		# & "C:\GitHub\Rubberduck\Rubberduck\Rubberduck.Deployment\echoargs.exe" ""$sourceDll"" /win32 /out:""$sourceTlb"";
+		& $cmd ""$sourceDll"" /win32 /out:""$sourceTlb"";
+
+		$cmd = "{0}heat.exe" -f $wixToolsDir;
+		& $cmd file ""$sourceDll"" -out ""$dllXml"";
+		& $cmd file ""$sourceTlb"" -out ""$tlbXml"";
+
+		$bitness = 0;
+	
+		[System.Reflection.Assembly]::LoadFrom($builderAssemblyPath);
+		$builder = New-Object Rubberduck.Deployment.RegistryEntryBuilder
+	
+		$out = $builder.Parse($tlbXml, $dllXml, $bitness);
+
+		$out | Format-Table | Out-String |% {Write-Host $_};
+	}
+}
+catch
+{
+	Write-Host -Foreground Red -Background Black ($_);
+}
+
+# for debugging locally
+# Write-Host "Press any key to continue...";
+# [void][System.Console]::ReadKey($true);
