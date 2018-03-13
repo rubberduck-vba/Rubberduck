49581bb0de86635104df73c804421339548de012
bclothier <bgclothier@gmail.com>
2018-03-12 22:48:33 -0500

    Provide explanatory comment for the powershell script

--- Rubberduck.Deployment/BuildRegistryScript.ps1
+++ Rubberduck.Deployment/BuildRegistryScript.ps1
@@ -1,4 +1,22 @@
-﻿param (
+﻿# The parameters should be supplied by the Build event of the project
+# in order to take macros from Visual Studio to avoid hard-coding
+# the paths. To simplify the process, the project should have a 
+# reference to the projects that needs to be registered, so that 
+# their DLL files will be present in the $(TargetDir) macro. 
+#
+# TODO: Figure a better way to locate the SDK tool where the TlbExp.exe
+#	    is located.
+#
+# Possible syntax for Post Build event of the project to invoke this:
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe 
+#  -command "$(ProjectDir)BuildRegistryScript.ps1 
+#  -builderAssemblyPath '$(TargetPath)' 
+#  -netToolsDir 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\' 
+#  -wixToolsDir '$(SolutionDir)packages\WiX.Toolset.3.9.1208.0\tools\wix\' 
+#  -sourceDir '$(TargetDir)' 
+#  -targetDir '$(TargetDir)' 
+#  -filesToExtract 'Rubberduck.dll'"
+param (
 	[Parameter(Mandatory=$true)][string]$builderAssemblyPath,
 	[Parameter(Mandatory=$true)][string]$netToolsDir,
 	[Parameter(Mandatory=$true)][string]$wixToolsDir,
@@ -12,6 +30,7 @@ $ErrorActionPreference = "Stop";
 
 try
 {
+	# Allow multiple DLL files to be registered if necessary
 	$separator = "|"
 	$option = [System.StringSplitOptions]::RemoveEmptyEntries
 	$files = $filesToExtract.Split($separator, $option);
@@ -25,8 +44,12 @@ try
 		$dllXml = $targetDll + ".xml"
 		$tlbXml = $targetTlb + ".xml"
 
-		$cmd = "{0}tlbexp.exe" -f $netToolsDir;
+		# Use for debugging issues with passing parameters to the external programs
+		# Note that it is not legal to have syntax like `& $cmdIncludingArguments` or `& $cmd $args`
+		# For simplicity, the arguments are pass in literally.
 		# & "C:\GitHub\Rubberduck\Rubberduck\Rubberduck.Deployment\echoargs.exe" ""$sourceDll"" /win32 /out:""$sourceTlb"";
+		
+		$cmd = "{0}tlbexp.exe" -f $netToolsDir;
 		& $cmd ""$sourceDll"" /win32 /out:""$sourceTlb"";
 
 		$cmd = "{0}heat.exe" -f $wixToolsDir;
@@ -39,6 +62,8 @@ try
 		$builder = New-Object Rubberduck.Deployment.RegistryEntryBuilder
 	
 		$out = $builder.Parse($tlbXml, $dllXml, $bitness);
+
+		# TODO: Do something more than just printing it to the output...
 
 		$out | Format-Table | Out-String |% {Write-Host $_};
 	}
