8cd98d394a001d8a956beda548bdf347e94215ea
Mathieu Guindon <retailcoder@gmail.com>
2018-04-08 12:51:24 -0400

    update version to 2.2

--- Rubberduck.Main/Properties/AssemblyInfo.cs
+++ Rubberduck.Main/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using Rubberduck;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
 //[assembly: AssemblyFileVersion("2.1.0.0")]
