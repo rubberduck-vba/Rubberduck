ba6a6a1f986283eabf58e2cfe517eb0bdc52a750
Vogel612 <vogel612@gmx.de>
2018-04-19 19:28:47 +0200

    Move CodeMetrics Core classes into separate files

---
+++ Rubberduck.Core/CodeAnalysis/CodeMetrics/AggregationLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rubberduck.CodeAnalysis.CodeMetrics
+{
+    public enum AggregationLevel
+    {
+        Project = 1 << 0,
+        Module = 1 << 1,
+        Member = 1 << 2,
+        Declaration = 1 << 3,
+    }
+}
