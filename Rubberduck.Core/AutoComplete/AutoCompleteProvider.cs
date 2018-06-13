03a13d6685cc395d05d797ea572d7c033f8b8dde
Mathieu Guindon <retailcoder@gmail.com>
2018-06-13 13:32:21 -0400

    log IAutoComplete registration

--- Rubberduck.Core/AutoComplete/AutoCompleteProvider.cs
+++ Rubberduck.Core/AutoComplete/AutoCompleteProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Rubberduck.Settings;
+using NLog;
 
 namespace Rubberduck.AutoComplete
 {
@@ -11,6 +12,8 @@ namespace Rubberduck.AutoComplete
 
     public class AutoCompleteProvider : IAutoCompleteProvider
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        
         public AutoCompleteProvider(IEnumerable<IAutoComplete> autoCompletes)
         {
             var defaults = new DefaultSettings<AutoCompleteSettings>().Default;
@@ -23,6 +26,7 @@ namespace Rubberduck.AutoComplete
             }
 
             AutoCompletes = autoCompletes;
+            _logger.Trace($"{AutoCompletes.Count()} IAutoComplete implementations registered.");
         }
 
         public IEnumerable<IAutoComplete> AutoCompletes { get; }
