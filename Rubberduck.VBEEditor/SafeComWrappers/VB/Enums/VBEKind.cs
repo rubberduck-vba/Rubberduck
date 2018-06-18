a6cdf6b23b09ea5b6cfdb013dd97a220b7862785
Andy <mansellan@gmail.com>
2018-06-18 22:44:02 +0100

    Added extra xml docs to enum VBEKind

--- Rubberduck.VBEEditor/SafeComWrappers/VB/Enums/VBEKind.cs
+++ Rubberduck.VBEEditor/SafeComWrappers/VB/Enums/VBEKind.cs
@@ -1,5 +1,13 @@
 ﻿namespace Rubberduck.VBEditor.SafeComWrappers
 {
+    /// <summary>
+    /// Describes the kind of VB Editor.
+    /// </summary>
+    /// <remarks>
+    /// This is not the same as vbext_ProjectType, exposed by VBProject.Type.
+    /// Although the member names are similar, VBProject.Type describes the type of project (either
+    /// a hosted project (VBA) or a standalone project (ocx, dll, etc.), not the type of IDE.
+    /// </remarks>
     public enum VBEKind
     {
         /// <summary>
