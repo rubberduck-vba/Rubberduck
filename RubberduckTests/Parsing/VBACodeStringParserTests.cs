af66193a624f371bcc991b520bac966597a0fcc7
bclothier <bgclothier@gmail.com>
2018-07-04 08:36:04 -0500

    Introduce CodeStringParser

---
+++ RubberduckTests/Parsing/VBACodeStringParserTests.cs
@@ -0,0 +1,78 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using NUnit.Framework;
+using Rubberduck.Parsing.Rewriter;
+using Rubberduck.Parsing.Symbols.ParsingExceptions;
+using Rubberduck.Parsing.VBA;
+
+namespace RubberduckTests.Parsing
+{
+    [TestFixture]
+    public class VBACodeStringParserTests
+    {
+        [Test]
+        [Category("VBACodeStringParser_Tests")]
+        public void CanParse()
+        {
+            const string inputCode = @"
+Public Sub Foo
+    MsgBox ""hi""
+End Sub";
+            var parser = new VBACodeStringParser("test", inputCode);
+            Assert.IsInstanceOf<IParseTree>(parser.ParseTree);
+        }
+
+        [Test]
+        [Category("VBACodeStringParser_Tests")]
+        public void CannotParse()
+        {
+            const string inputCode = @"
+Public Sub Foo
+    MsgBox ""hi""";
+
+            Assert.Throws<MainParseSyntaxErrorException>(() =>
+            {
+                var parser = new VBACodeStringParser("test", inputCode);
+            });
+        }
+
+        [Test]
+        [Category("VBACodeStringParser_Tests")]
+        public void CannotParse_CodeSnippet()
+        {
+            const string inputCode = @"MsgBox ""hi""";
+
+            Assert.Throws<MainParseSyntaxErrorException>(() =>
+            {
+                var parser = new VBACodeStringParser("test", inputCode);
+            });
+        }
+
+        [Test]
+        [Category("VBACodeStringParser_Tests")]
+        public void ParseTreeIsValid()
+        {
+            const string inputCode = @"
+Public Sub Foo
+    MsgBox ""hi""
+End Sub";
+            var parser = new VBACodeStringParser("test", inputCode);
+            var tree = parser.ParseTree;
+
+            Assert.AreEqual(inputCode + "<EOF>", tree.GetChild(0).GetText());
+        }
+
+        [Test]
+        [Category("VBACodeStringParser_Tests")]
+        public void GetRewriter()
+        {
+            const string inputCode = @"
+Public Sub Foo
+    MsgBox ""hi""
+End Sub";
+            var parser = new VBACodeStringParser("test", inputCode);
+            
+            Assert.IsInstanceOf<TokenStreamRewriter>(parser.Rewriter);
+        }
+    }
+}
