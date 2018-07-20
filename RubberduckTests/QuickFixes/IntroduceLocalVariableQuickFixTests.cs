9cd3e5ea878555d3884b1170ef0cb7165852f608
Max Doerner <maxdoerner@gmx.net>
2018-07-20 23:27:12 +0200

    Added failing tests for the IntroduceLocalVariableQuickfix covering issue #4215.

---
+++ RubberduckTests/QuickFixes/IntroduceLocalVariableQuickFixTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using Rubberduck.Inspections.Concrete;
+using Rubberduck.Inspections.QuickFixes;
+using Rubberduck.Parsing.Inspections;
+using RubberduckTests.Inspections;
+using RubberduckTests.Mocks;
+using System.Linq;
+using System.Threading;
+
+namespace RubberduckTests.QuickFixes
+{
+    [TestFixture]
+    public class IntroduceLocalVariableQuickFixTests
+    {
+        [Test]
+        [Category("QuickFixes")]
+        public void IntroduceLocalVariable_QuickFixWorks()
+        {
+            var inputCode =
+                @"Public Sub Foo()
+    Dim bar As Collection
+    For Each fooBar In Foo
+        fooBar.Whatever
+    Next
+End Sub";
+
+            var expectedCode =
+                @"Public Sub Foo()
+    Dim bar As Collection
+    Dim fooBar As Variant
+    For Each fooBar In Foo
+        fooBar.Whatever
+    Next
+End Sub";
+
+            TestInsertLocalVariableQuickFix(expectedCode, inputCode);
+        }
+
+        [Test]
+        [Category("QuickFixes")]
+        public void IntroduceLocalVariable_QuickFixWorks_MultipleEnclosingBlocks()
+        {
+            var inputCode =
+                @"Public Sub Foo()
+    Dim bar As Collection
+    With bar
+        For Each fooBar In Foo.Items
+            fooBar.Whatever
+        Next
+    End With
+End Sub";
+
+            var expectedCode =
+                @"Public Sub Foo()
+    Dim bar As Collection
+    With bar
+        Dim fooBar As Variant
+        For Each fooBar In Foo.Items
+            fooBar.Whatever
+        Next
+    End With
+End Sub";
+
+            TestInsertLocalVariableQuickFix(expectedCode, inputCode);
+        }
+
+        private void TestInsertLocalVariableQuickFix(string expectedCode, string inputCode)
+        {
+            var vbe = MockVbeBuilder.BuildFromSingleStandardModule(inputCode, out var component);
+            var state = MockParser.CreateAndParse(vbe.Object);
+
+            var inspection = new UndeclaredVariableInspection(state) { Severity = CodeInspectionSeverity.Warning };
+            var inspector = InspectionsHelper.GetInspector(inspection);
+            var inspectionResults = inspector.FindIssuesAsync(state, CancellationToken.None).Result;
+
+            new IntroduceLocalVariableQuickFix(state).Fix(inspectionResults.First());
+            var actualCode = state.GetRewriter(component).GetText();
+            Assert.AreEqual(expectedCode, actualCode);
+        }
+    }
+}
