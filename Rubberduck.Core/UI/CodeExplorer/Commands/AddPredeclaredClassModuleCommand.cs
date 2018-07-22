35afc8d0776c19a376e98bf819f481d397a696f9
ThunderFrame <ThunderXFrame@gmail.com>
2018-07-23 00:05:39 +1000

    builds Predeclared Class Module Text

--- Rubberduck.Core/UI/CodeExplorer/Commands/AddPredeclaredClassModuleCommand.cs
+++ Rubberduck.Core/UI/CodeExplorer/Commands/AddPredeclaredClassModuleCommand.cs
@@ -2,6 +2,7 @@ using NLog;
 using Rubberduck.Navigation.CodeExplorer;
 using Rubberduck.UI.Command;
 using Rubberduck.VBEditor.SafeComWrappers;
+using System.IO;
 
 namespace Rubberduck.UI.CodeExplorer.Commands
 {
@@ -22,7 +23,25 @@ namespace Rubberduck.UI.CodeExplorer.Commands
 
         protected override void OnExecute(object parameter)
         {
-            _addComponentCommand.AddComponent(parameter as CodeExplorerItemViewModel, ComponentType.ClassModule);
+            string moduleText = CreatePreclaredClassModule();
+            _addComponentCommand.AddComponent(parameter as CodeExplorerItemViewModel, moduleText);
+        }
+
+        private string CreatePreclaredClassModule()
+        {
+            string moduleText = @"
+VERSION 1.0 CLASS
+BEGIN
+  MultiUse = -1  'True
+END
+Attribute VB_GlobalNameSpace = False
+Attribute VB_Creatable = False
+Attribute VB_PredeclaredId = True
+Attribute VB_Exposed = False
+Attribute VB_Ext_KEY = ""Rubberduck"",""Predeclared Class Module""
+Option Explicit
+";
+            return moduleText;
         }
     }
 }
