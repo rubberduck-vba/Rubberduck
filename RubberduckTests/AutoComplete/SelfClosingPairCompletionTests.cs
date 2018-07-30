a6ac1cc8154a7204a49b3b614bf113af0ead6f5d
Mathieu Guindon <retailcoder@gmail.com>
2018-07-30 00:00:12 -0400

    added CodeString

--- RubberduckTests/AutoComplete/SelfClosingPairCompletionTests.cs
+++ RubberduckTests/AutoComplete/SelfClosingPairCompletionTests.cs
@@ -7,10 +7,11 @@ namespace RubberduckTests.AutoComplete
     [TestFixture]
     public class SelfClosingPairCompletionTests
     {
-        private (string Code, Selection zPosition) Run(SelfClosingPair pair, (string,Selection) original, char input)
+        private CodeString Run(SelfClosingPair pair, CodeString original, char input)
         {
             var sut = new SelfClosingPairCompletionService();
-            return sut.Execute(pair, original, input);
+            var result = sut.Execute(pair, (original.Code, original.CaretPosition), input);
+            return new CodeString(result.Code, result.CaretPosition);
         }
 
         [Test]
@@ -23,9 +24,7 @@ namespace RubberduckTests.AutoComplete
 
             var result = Run(pair, original, input);
 
-            Assert.AreEqual(
-                expected.Code.InsertPseudoCaret(expected.zPosition).Code, 
-                result.Code.InsertPseudoCaret(result.zPosition).Code);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
@@ -37,10 +36,7 @@ namespace RubberduckTests.AutoComplete
             var expected = @"foo = MsgBox((|))".RemovePseudoCaret();
 
             var result = Run(pair, original, input);
-
-            Assert.AreEqual(
-                expected.Code.InsertPseudoCaret(expected.zPosition).Code, 
-                result.Code.InsertPseudoCaret(result.zPosition).Code);
+            Assert.AreEqual(expected, result);
         }
     }
 }
--- RubberduckTests/CodeStringExtensions.cs
+++ RubberduckTests/CodeStringExtensions.cs
@@ -3,9 +3,64 @@ using Rubberduck.VBEditor;
 
 namespace RubberduckTests
 {
+    public struct CodeString
+    {
+        /// <summary>
+        /// Creates a new <c>CodeString</c>
+        /// </summary>
+        /// <param name="code">Code string</param>
+        /// <param name="zPosition">Zero-based caret position</param>
+        public CodeString(string code, Selection zPosition)
+        {
+            Code = code;
+            CaretPosition = zPosition;
+        }
+
+        /// <summary>
+        /// The code string.
+        /// </summary>
+        public string Code { get; }
+        /// <summary>
+        /// Zero-based caret position in the code string.
+        /// </summary>
+        public Selection CaretPosition { get; }
+
+        public static implicit operator string(CodeString codeString)
+        {
+            return codeString.Code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var other = (CodeString)obj;
+            return Code.Equals(other.Code) && CaretPosition.Equals(other.CaretPosition);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Compute(Code, CaretPosition);
+        }
+
+        public override string ToString()
+        {
+            var hasCaret = Code.RemovePseudoCaret().CaretPosition != default;
+            if (hasCaret)
+            {
+                return Code;
+            }
+
+            return Code.InsertPseudoCaret(CaretPosition);
+        }
+    }
+
     public static class CodeStringExtensions
     {
-        public static (string Code, Selection zPosition) RemovePseudoCaret(this string code)
+        public static CodeString RemovePseudoCaret(this string code)
         {
             var zPosition = new Selection();
             var lines = code.Split('\n');
@@ -22,15 +77,15 @@ namespace RubberduckTests
             }
 
             var newCode = string.Join("\n", lines);
-            return (newCode, zPosition);
+            return new CodeString(newCode, zPosition);
         }
 
-        public static (string Code, Selection zPosition) InsertPseudoCaret(this string code, Selection zPosition)
+        public static CodeString InsertPseudoCaret(this string code, Selection zPosition)
         {
             var lines = code.Split('\n');
             var line = lines[zPosition.StartLine];
             lines[zPosition.StartLine] = line.Insert(zPosition.StartColumn, "|");
-            return (string.Join("\n", lines), zPosition);
+            return new CodeString(string.Join("\n", lines), zPosition);
         }
     }
 }
