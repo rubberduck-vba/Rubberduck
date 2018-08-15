8e5bdb68992b119759f1a30fb8ae9ac9cbb8d72d
IvenBach <IvenBach@gmail.com>
2018-08-15 09:59:39 -0700

    Add filter icon & update resource strings

--- Rubberduck.Resources/RubberduckUI.Designer.cs
+++ Rubberduck.Resources/RubberduckUI.Designer.cs
@@ -2010,11 +2010,56 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Filter by Description:.
+        ///   Looks up a localized string similar to Error.
         /// </summary>
-        public static string InspectionSettings_FilterDescription {
+        public static string InspectionResults_FilterByError {
             get {
-                return ResourceManager.GetString("InspectionSettings_FilterDescription", resourceCulture);
+                return ResourceManager.GetString("InspectionResults_FilterByError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hint.
+        /// </summary>
+        public static string InspectionResults_FilterByHint {
+            get {
+                return ResourceManager.GetString("InspectionResults_FilterByHint", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Suggestion.
+        /// </summary>
+        public static string InspectionResults_FilterBySuggestion {
+            get {
+                return ResourceManager.GetString("InspectionResults_FilterBySuggestion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Warning.
+        /// </summary>
+        public static string InspectionResults_FilterByWarning {
+            get {
+                return ResourceManager.GetString("InspectionResults_FilterByWarning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Description:.
+        /// </summary>
+        public static string InspectionSettings_FilterByDescription {
+            get {
+                return ResourceManager.GetString("InspectionSettings_FilterByDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Severity:.
+        /// </summary>
+        public static string InspectionSettings_FilterBySeverity {
+            get {
+                return ResourceManager.GetString("InspectionSettings_FilterBySeverity", resourceCulture);
             }
         }
         
