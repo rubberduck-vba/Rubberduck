fea3ff3f3eadcc33ec345d3067f82d8799781f4d
Vogel612 <vogel612@gmx.de>
2018-08-16 20:38:35 +0200

    Expose UnitTesting assembly internals to IoC

--- Rubberduck.UnitTesting/Properties/AssemblyInfo.cs
+++ Rubberduck.UnitTesting/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("RubberduckTests")]
+[assembly: InternalsVisibleTo("Rubberduck")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
