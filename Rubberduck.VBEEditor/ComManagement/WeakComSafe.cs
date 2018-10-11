99498433efb061fe09e7f6e4f5e84f3945f4bccd
bclothier <bgclothier@gmail.com>
2018-10-11 08:18:40 -0500

    Introduce Serialize method to the weak COM Safe class.

--- Rubberduck.VBEEditor/ComManagement/WeakComSafe.cs
+++ Rubberduck.VBEEditor/ComManagement/WeakComSafe.cs
@@ -3,13 +3,17 @@ using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
+#if DEBUG
+using System.Linq;
+using System.Runtime.InteropServices;
+#endif
+
 namespace Rubberduck.VBEditor.ComManagement
 {
     public class WeakComSafe : IComSafe
     {
         //We use weak references to allow the GC to reclaim RCWs earlier if possible.
-        private readonly ConcurrentDictionary<int, WeakReference<ISafeComWrapper>> _comWrapperCache = new ConcurrentDictionary<int, WeakReference<ISafeComWrapper>>();
-
+        private readonly ConcurrentDictionary<int, (DateTime insertTime, WeakReference<ISafeComWrapper> weakRef)> _comWrapperCache = new ConcurrentDictionary<int, (DateTime, WeakReference<ISafeComWrapper>)>();
 
         public void Add(ISafeComWrapper comWrapper)
         {
@@ -17,8 +21,8 @@ namespace Rubberduck.VBEditor.ComManagement
             {
                 _comWrapperCache.AddOrUpdate(
                     GetComWrapperObjectHashCode(comWrapper), 
-                    key => new WeakReference<ISafeComWrapper>(comWrapper), 
-                    (key, value) => new WeakReference<ISafeComWrapper>(comWrapper));
+                    key => (DateTime.UtcNow, new WeakReference<ISafeComWrapper>(comWrapper)), 
+                    (key, value) => (DateTime.UtcNow, new WeakReference<ISafeComWrapper>(comWrapper)));
             }
 
         }
@@ -47,7 +51,7 @@ namespace Rubberduck.VBEditor.ComManagement
 
             foreach (var weakReference in _comWrapperCache.Values)
             {
-                if(weakReference.TryGetTarget(out var comWrapper))
+                if(weakReference.weakRef.TryGetTarget(out var comWrapper))
                 {
                     comWrapper.Dispose();
                 }
@@ -55,5 +59,57 @@ namespace Rubberduck.VBEditor.ComManagement
 
             _comWrapperCache.Clear();
         }
+
+#if DEBUG
+        /// <summary>
+        /// Provide a serialized list of the COM Safe
+        /// to make it easy to analyze what is inside
+        /// the COM Safe at the different points of
+        /// the session's lifetime.
+        /// </summary>
+        public void Serialize()
+        {
+            using (var stream = System.IO.File.AppendText($"comSafeOutput {DateTime.UtcNow:yyyyMMddhhmmss}.csv"))
+            {
+                stream.WriteLine("Ordinal\tKey\tCOM Wrapper Type\tWrapping Null?\tIUnknown Pointer Address");
+                var i = 0;
+                foreach (var kvp in _comWrapperCache.OrderBy(kvp => kvp.Value.insertTime))
+                {
+                    var line = kvp.Value.weakRef.TryGetTarget(out var target) 
+                        ? $"{i++}\t{kvp.Key}\t\"{target.GetType().FullName}\"\t\"{target.IsWrappingNullReference}\"\t\"{(target.IsWrappingNullReference ? "null" : GetPtrAddress(target.Target))}\"" 
+                        : $"{i++}\t{kvp.Key}\t\"null\"\t\"null\"\t\"null\"";
+                    stream.WriteLine(line);
+                }
+            }
+        }
+
+        private static string GetPtrAddress(object target)
+        {
+            if (target == null)
+            {
+                return IntPtr.Zero.ToString();
+            }
+
+            if (!Marshal.IsComObject(target))
+            {
+                return "Not a COM object";
+            }
+
+            var pointer = IntPtr.Zero;
+            try
+            {
+                pointer = Marshal.GetIUnknownForObject(target);
+            }
+            finally
+            {
+                if (pointer != IntPtr.Zero)
+                {
+                    Marshal.Release(pointer);
+                }
+            }
+
+            return pointer.ToString();
+        }
+#endif
     }
 }
