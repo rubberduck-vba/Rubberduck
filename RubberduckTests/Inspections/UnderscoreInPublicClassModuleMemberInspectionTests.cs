70b53ca91628a1614371ac7610679e3ca3f47034
Abraham Hosch <hosch250@live.com>
2018-10-12 13:55:51 -0500

    Ignore interface implementations

--- RubberduckTests/Inspections/UnderscoreInPublicClassModuleMemberInspectionTests.cs
+++ RubberduckTests/Inspections/UnderscoreInPublicClassModuleMemberInspectionTests.cs
@@ -135,5 +135,37 @@ End Sub";
                 Assert.AreEqual(0, inspectionResults.Count());
             }
         }
+
+        [Test]
+        [Category("Inspections")]
+        public void ImplementsInterface()
+        {
+            const string inputCode1 =
+       @"Public Sub Foo()
+End Sub";
+
+            //Expectation
+            const string inputCode2 =
+                @"Implements Class1
+
+Public Sub Class1_Foo()
+    Err.Raise 5 'TODO implement interface member
+End Sub
+";
+            var builder = new MockVbeBuilder();
+            var project = builder.ProjectBuilder("TestProject1", ProjectProtection.Unprotected)
+                .AddComponent("Class1", ComponentType.ClassModule, inputCode1)
+                .AddComponent("Class2", ComponentType.ClassModule, inputCode2)
+                .Build();
+            var vbe = builder.AddProject(project).Build();
+            using (var state = MockParser.CreateAndParse(vbe.Object))
+            {
+                var inspection = new UnderscoreInPublicClassModuleMemberInspection(state);
+                var inspector = InspectionsHelper.GetInspector(inspection);
+                var inspectionResults = inspector.FindIssuesAsync(state, CancellationToken.None).Result;
+
+                Assert.AreEqual(0, inspectionResults.Count());
+            }
+        }
     }
 }
