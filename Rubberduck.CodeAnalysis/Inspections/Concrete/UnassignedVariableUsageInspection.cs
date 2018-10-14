3c002ee25d60ec415d9448264b7c8f76ee18cab9
comintern <brent.mckibbin@gmail.com>
2018-10-14 17:56:15 -0500

    Add support for VB6 versions of Len and LenB, simplify inspection, add tests.

--- Rubberduck.CodeAnalysis/Inspections/Concrete/UnassignedVariableUsageInspection.cs
+++ Rubberduck.CodeAnalysis/Inspections/Concrete/UnassignedVariableUsageInspection.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Antlr4.Runtime;
 using Rubberduck.Inspections.Abstract;
 using Rubberduck.Inspections.Results;
-using Rubberduck.Parsing;
 using Rubberduck.Parsing.Inspections.Abstract;
 using Rubberduck.Resources.Inspections;
 using Rubberduck.Parsing.Symbols;
@@ -18,6 +16,15 @@ namespace Rubberduck.Inspections.Concrete
         public UnassignedVariableUsageInspection(RubberduckParserState state)
             : base(state) { }
 
+        //See https://github.com/rubberduck-vba/Rubberduck/issues/2010 for why these are being excluded.
+        private static readonly List<string> IgnoredFunctions = new List<string>
+        {
+            "VBE7.DLL;VBA.Strings.Len",
+            "VBE7.DLL;VBA.Strings.LenB",
+            "VBA6.DLL;VBA.Strings.Len",
+            "VBA6.DLL;VBA.Strings.LenB"
+        };
+
         protected override IEnumerable<IInspectionResult> DoGetInspectionResults()
         {
             var declarations = State.DeclarationFinder.UserDeclarations(DeclarationType.Variable)
@@ -27,42 +34,16 @@ namespace Rubberduck.Inspections.Concrete
                     && !declaration.IsSelfAssigned
                     && !declaration.References.Any(reference => reference.IsAssignment));
 
-            //See https://github.com/rubberduck-vba/Rubberduck/issues/2010 for why these are being excluded.
-            //TODO: These need to be modified to correctly work in VB6.
-            var lenFunction = BuiltInDeclarations.SingleOrDefault(s => s.DeclarationType == DeclarationType.Function && s.Scope.Equals("VBE7.DLL;VBA.Strings.Len"));
-            var lenbFunction = BuiltInDeclarations.SingleOrDefault(s => s.DeclarationType == DeclarationType.Function && s.Scope.Equals("VBE7.DLL;VBA.Strings.LenB"));
+            var excludedDeclarations = BuiltInDeclarations.Where(decl => IgnoredFunctions.Contains(decl.QualifiedName.ToString())).ToList();
 
-            return declarations.Where(d => d.References.Any() &&
-                                           !DeclarationReferencesContainsReference(lenFunction, d) &&
-                                           !DeclarationReferencesContainsReference(lenbFunction, d))
-                               .SelectMany(d => d.References)
-                               .Where(r => !r.IsIgnoringInspectionResultFor(AnnotationName))
-                               .Select(r => new IdentifierReferenceInspectionResult(this,
-                                                                 string.Format(InspectionResults.UnassignedVariableUsageInspection, r.IdentifierName),
-                                                                 State,
-                                                                 r)).ToList();
-        }
-
-        private bool DeclarationReferencesContainsReference(Declaration parentDeclaration, Declaration target)
-        {
-            if (parentDeclaration == null)
-            {
-                return false;
-            }
-            
-            foreach (var targetReference in target.References)
-            {
-                foreach (var reference in parentDeclaration.References)
-                {
-                    var context = (ParserRuleContext) reference.Context.Parent;
-                    if (context.GetSelection().Contains(targetReference.Selection))
-                    {
-                        return true;
-                    }
-                }
-            }
-            
-            return false;
+            return declarations.Except(excludedDeclarations)
+                .Where(d => d.References.Any())
+                .SelectMany(d => d.References)
+                .Where(r => !r.IsIgnoringInspectionResultFor(AnnotationName))
+                .Select(r => new IdentifierReferenceInspectionResult(this,
+                    string.Format(InspectionResults.UnassignedVariableUsageInspection, r.IdentifierName),
+                    State,
+                    r)).ToList();
         }
     }
 }
