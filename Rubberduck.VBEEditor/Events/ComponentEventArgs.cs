0f2351ef353093e2f3de7c94ec122af9c1e3a56e
bclothier <bgclothier@gmail.com>
2018-10-24 17:56:24 -0500

    Remove unused functions and clarify the naming of new member

--- Rubberduck.VBEEditor/Events/ComponentEventArgs.cs
+++ Rubberduck.VBEEditor/Events/ComponentEventArgs.cs
@@ -1,47 +1,15 @@
 using System;
-using Rubberduck.VBEditor.ComManagement;
-using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
 namespace Rubberduck.VBEditor.Events
 {
     public class ComponentEventArgs : EventArgs
     {
-        private readonly QualifiedModuleName _qmn;
-
-        public ComponentEventArgs(QualifiedModuleName qmn)
+        public ComponentEventArgs(QualifiedModuleName qualifiedModuleName)
         {
-            _qmn = qmn;
+            QualifiedModuleName = qualifiedModuleName;
         }
 
-        public string ProjectId => _qmn.ProjectId;
-        public QualifiedModuleName QualifiedModuleName => _qmn;
-
-        public bool TryGetProject(IProjectsRepository repository, out IVBProject project)
-        {
-            try
-            {
-                project = repository.Project(_qmn.ProjectId);
-                return true;
-            }
-            catch
-            {
-                project = null;
-                return false;
-            }
-        }
-
-        public bool TryGetComponent(IProjectsRepository repository, out IVBComponent component)
-        {
-            try
-            {
-                component = repository.Component(_qmn);
-                return true;
-            }
-            catch
-            {
-                component = null;
-                return false;
-            }
-        }
+        public string ProjectId => QualifiedModuleName.ProjectId;
+        public QualifiedModuleName QualifiedModuleName { get; }
     }
 }
