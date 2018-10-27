1c2f6ce2a38a1f9655b1d8070b8d46922ac4599e
bclothier <bgclothier@gmail.com>
2018-10-27 08:08:26 -0500

    Make the generic version of SafeIDispatchWrapper visible only to Rubberduck.VBEditor and Rubberduck.VBEditor.* to ensure raw COM objects will not leak outside those projects. Add comments describing the intended usage of both versions.

--- Rubberduck.VBEEditor/SafeComWrappers/SafeIDispatchWrapper.cs
+++ Rubberduck.VBEEditor/SafeComWrappers/SafeIDispatchWrapper.cs
@@ -4,14 +4,36 @@ using Rubberduck.VBEditor.SafeComWrappers.Abstract;
 
 namespace Rubberduck.VBEditor.SafeComWrappers
 {
-    public class SafeIDispatchWrapper<TDispatch> : SafeIDispatchWrapper
+    /// <summary>
+    /// Creates a new IDispatch-based wrapper for a provided
+    /// COM interface. This is internal and should be used only
+    /// within the Rubberduck.VBEditor.* projects.
+    /// </summary>
+    /// <remarks>
+    /// To avoid exposing additional interop libraries to other projects
+    /// and violating the separation, only the non-generic version of
+    /// <see cref="SafeIDispatchWrapper" /> be provided for consumption outside
+    /// the Rubberduck.VBEditor.* projects.
+    /// Within those projects, the class is useful for wrapping COM interfaces
+    /// that do not have a corresponding  <see cref="ISafeComWrapper{T}" />
+    /// implementations to ensure those are managed and will not leak.
+    /// </remarks>
+    /// <typeparam name="TDispatch">COM interface to wrap</typeparam>
+    /// <inheritdoc />
+    internal class SafeIDispatchWrapper<TDispatch> : SafeIDispatchWrapper
     {
-        public SafeIDispatchWrapper(TDispatch target, bool rewrapping = false) : base(target, rewrapping)
+        internal SafeIDispatchWrapper(TDispatch target, bool rewrapping = false) : base(target, rewrapping)
         { }
 
         public new TDispatch Target => (TDispatch) base.Target;
     }
 
+    /// <summary>
+    /// Provide a IDispatch-based (e.g. late-bound) access to a COM object.
+    /// Use <see cref="Invoke"/> to work with the object. The
+    /// <see cref="SafeIDispatchWrapper.Target"/> must be a raw COM object without
+    /// any wrappers. 
+    /// </summary>
     public class SafeIDispatchWrapper : SafeComWrapper<dynamic>
     {
         public SafeIDispatchWrapper(object target, bool rewrapping = false) : base(target, rewrapping)
@@ -24,8 +46,18 @@ namespace Rubberduck.VBEditor.SafeComWrappers
             IDispatchPointer = GetPointer(target);
         }
 
+        // ReSharper disable once InconsistentNaming
         public IntPtr IDispatchPointer { get; }
 
+        /// <summary>
+        /// Use the method to encapsulate operations against the late-bound COM object.
+        /// It is caller's responsibilty to handle any exceptions that may result as part
+        /// of the operation. 
+        /// </summary>
+        /// <param name="action">
+        /// A method that perform work against the late-bound COM object provided as the
+        /// parameter to the function
+        /// </param>
         public void Invoke(Action<dynamic> action)
         {
             action.Invoke(Target);
