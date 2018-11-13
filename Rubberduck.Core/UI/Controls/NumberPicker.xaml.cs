15671d8d4e906a5e50a66af4a584a491854ff10d
comintern <brent.mckibbin@gmail.com>
2018-11-12 20:19:14 -0600

    Fix bindings.

--- Rubberduck.Core/UI/Controls/NumberPicker.xaml.cs
+++ Rubberduck.Core/UI/Controls/NumberPicker.xaml.cs
@@ -11,6 +11,10 @@ namespace Rubberduck.UI.Controls
     {
         public static readonly DependencyProperty NumValueProperty =
             DependencyProperty.Register("NumValue", typeof(int), typeof(NumberPicker), new UIPropertyMetadata(null));
+        public static readonly DependencyProperty MinNumberProperty =
+            DependencyProperty.Register("MinNumber", typeof(int), typeof(NumberPicker), new UIPropertyMetadata(null));
+        public static readonly DependencyProperty MaxNumberProperty =
+            DependencyProperty.Register("MaxNumber", typeof(int), typeof(NumberPicker), new UIPropertyMetadata(null));
 
         public int NumValue
         {
@@ -22,9 +26,29 @@ namespace Rubberduck.UI.Controls
             }
         }
 
-        public int MinNumber { get; set; } = int.MinValue;
 
-        public int MaxNumber { get; set; } = int.MaxValue;
+        public int MinNumber
+        {
+            get => (int)GetValue(MinNumberProperty);
+            set
+            {
+                SetValue(MinNumberProperty, value);
+                OnPropertyChanged(new DependencyPropertyChangedEventArgs(MinNumberProperty, NumValue - 1, NumValue));
+            }
+        }
+
+        public int MaxNumber
+        {
+            get => (int)GetValue(MaxNumberProperty);
+            set
+            {
+                SetValue(MaxNumberProperty, value);
+                OnPropertyChanged(new DependencyPropertyChangedEventArgs(MaxNumberProperty, NumValue - 1, NumValue));
+            }
+        }
+        //public int MinNumber { get; set; } = int.MinValue;
+
+        //public int MaxNumber { get; set; } = int.MaxValue;
 
         public NumberPicker()
         {
