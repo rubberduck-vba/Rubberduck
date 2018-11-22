f7c152ca0f57dfb39065dae58b903e2a6609d450
bclothier <bgclothier@gmail.com>
2018-11-22 07:53:40 -0600

    Create a path provider to provide an uniform method for getting the root path.

---
+++ Rubberduck.SettingsProvider/IPersistancePathProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Rubberduck.SettingsProvider
+{
+    public interface IPersistancePathProvider
+    {
+        string DataRootPath { get; }
+        string DataFolderPath(string folderName);
+    }
+}
