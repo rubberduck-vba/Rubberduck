acb96b33bf92fa3e71dbb03554cd296eb74f419f
bclothier <bgclothier@gmail.com>
2018-12-31 13:51:51 -0600

    Additional tests

--- RubberduckTests/ComMock/MockProviderTests.cs
+++ RubberduckTests/ComMock/MockProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using Rubberduck.ComClientLibrary.UnitTesting.Mocks;
 
@@ -9,13 +10,38 @@ namespace RubberduckTests.ComMock
     {
         [Test]
         [Category("ComMocks")]
-        public void MockProvider_Returns_Correct_Mock()
+        public void MockProvider_Returns_ComMocked()
         {
             var provider = new MockProvider();
             var mock = provider.Mock("Excel.Application");
             var obj = mock.Object;
 
-            Assert.IsTrue(Marshal.IsComObject(obj));
+            Assert.IsInstanceOf<ComMocked>(obj);
+        }
+
+        [Test]
+        [Category("ComMocks")]
+        public void MockProvider_Create_Correct_Mock()
+        {
+            var pUnk = IntPtr.Zero;
+            var pCom = IntPtr.Zero;
+
+            try
+            {
+                var provider = new MockProvider();
+                var mock = provider.Mock("Scripting.FileSystemObject");
+                var obj = mock.Object;
+
+                pUnk = Marshal.GetIUnknownForObject(obj);
+                var comGuid = new Guid("2A0B9D10-4B87-11D3-A97A-00104B365C9F"); //Scripting.IFileSystem3
+                Marshal.QueryInterface(pUnk, ref comGuid, out pCom);
+                Assert.AreNotEqual(IntPtr.Zero, pCom);
+            }
+            finally
+            {
+                if (pCom != IntPtr.Zero) Marshal.Release(pCom);
+                if (pUnk != IntPtr.Zero) Marshal.Release(pUnk);
+            }
         }
     }
 }
