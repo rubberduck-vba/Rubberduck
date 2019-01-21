a02424e5d4d73522dc73c6d2aa57001857c4d1fa
Max Doerner <maxdoerner@gmx.net>
2019-01-22 00:29:31 +0100

    Introduce the ISelection service
    This service is a provides access to the active selection and selection of individual modules without involving COM wrappers directly; the interface only uses QMNs and selections.

---
+++ Rubberduck.VBEEditor/Utility/ISelectionService.cs
@@ -0,0 +1,12 @@
+﻿namespace Rubberduck.VBEditor.Utility
+{
+    public interface ISelectionService
+    {
+        QualifiedSelection? ActiveSelection();
+        Selection? Selection(QualifiedModuleName module);
+        bool TryActivate(QualifiedModuleName module);
+        bool TrySetActiveSelection(QualifiedSelection selection);
+        bool TrySetSelection(QualifiedModuleName module, Selection selection);
+        bool TrySetSelection(QualifiedSelection selection);
+    }
+}
