443831f6e67ac066d7f0444e4b7ea694745657b0
Max Doerner <maxdoerner@gmx.net>
2019-02-15 21:29:47 +0100

    Introduce abstract base classes for refactorings and refactorings with a presenter

---
+++ Rubberduck.Refactorings/InteractiveRefactoringBase.cs
@@ -0,0 +1,22 @@
+﻿using Rubberduck.Parsing.Rewriter;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.VBEditor;
+using Rubberduck.VBEditor.Utility;
+using System;
+
+namespace Rubberduck.Refactorings
+{
+    public abstract class InteractiveRefactoringBase<TPresenter, TModel> : RefactoringBase where TPresenter : class where TModel : class
+    {
+        protected readonly Func<TModel, IDisposalActionContainer<TPresenter>> PresenterFactory;
+
+        public InteractiveRefactoringBase(IRewritingManager rewritingManager, ISelectionService selectionService, IRefactoringPresenterFactory factory) 
+        :base(rewritingManager, selectionService)
+        {
+            PresenterFactory = ((model) => DisposalActionContainer.Create(factory.Create<TPresenter, TModel>(model), factory.Release));
+        }
+
+        public abstract override void Refactor(QualifiedSelection target);
+        public abstract override void Refactor(Declaration target);
+    }
+}
---
+++ Rubberduck.Refactorings/RefactoringBase.cs
@@ -0,0 +1,33 @@
+﻿using Rubberduck.Parsing.Rewriter;
+using Rubberduck.Parsing.Symbols;
+using Rubberduck.VBEditor;
+using Rubberduck.VBEditor.Utility;
+
+namespace Rubberduck.Refactorings
+{
+    public abstract class RefactoringBase : IRefactoring
+    {
+        protected readonly IRewritingManager RewritingManager;
+        protected readonly ISelectionService SelectionService;
+
+        protected RefactoringBase(IRewritingManager rewritingManager, ISelectionService selectionService)
+        {
+            RewritingManager = rewritingManager;
+            SelectionService = selectionService;
+        }
+
+        public virtual void Refactor()
+        {
+            var activeSelection = SelectionService.ActiveSelection();
+            if (!activeSelection.HasValue)
+            {
+                return;
+            }
+
+            Refactor(activeSelection.Value);
+        }
+
+        public abstract void Refactor(QualifiedSelection target);
+        public abstract void Refactor(Declaration target);
+    }
+}
