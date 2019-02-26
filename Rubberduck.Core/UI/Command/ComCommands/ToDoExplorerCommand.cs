36e8571279ce7c86d99781519981b2b396374f8a
bclothier <bgclothier@gmail.com>
2019-02-26 00:29:15 -0600

    Update ToDoExplorerCommand

--- Rubberduck.Core/UI/Command/ComCommands/ToDoExplorerCommand.cs
+++ Rubberduck.Core/UI/Command/ComCommands/ToDoExplorerCommand.cs
@@ -1,19 +1,20 @@
 ﻿using System.Runtime.InteropServices;
 using NLog;
 using Rubberduck.UI.ToDoItems;
+using Rubberduck.VBEditor.Events;
 
-namespace Rubberduck.UI.Command
+namespace Rubberduck.UI.Command.ComCommands
 {
     /// <summary>
     /// A command that displays the To-Do explorer window.
     /// </summary>
     [ComVisible(false)]
-    public class ToDoExplorerCommand : CommandBase
+    public class ToDoExplorerCommand : ComCommandBase
     {
         private readonly ToDoExplorerDockablePresenter _presenter;
 
-        public ToDoExplorerCommand(ToDoExplorerDockablePresenter presenter)
-            : base(LogManager.GetCurrentClassLogger())
+        public ToDoExplorerCommand(ToDoExplorerDockablePresenter presenter, IVBEEvents vbeEvents)
+            : base(LogManager.GetCurrentClassLogger(), vbeEvents)
         {
             _presenter = presenter;
         }
