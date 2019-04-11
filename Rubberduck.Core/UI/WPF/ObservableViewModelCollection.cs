6dec120d9e9ed3574991c35c4d417982f8c24549
Vogel612 <vogel612@gmx.de>
2019-04-12 01:06:41 +0200

    Update the View when edits disrupt the validity of a HotkeySetting

---
+++ Rubberduck.Core/UI/WPF/ObservableViewModelCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Rubberduck.UI.WPF
+{
+    // Taken and adjusted from https://stackoverflow.com/a/5256827/1803692
+    /// <summary>
+    /// This class extends the capabilities of ObservableCollection.
+    /// It adds an additional event, which is raised when the properties of an item stored in the collection change.
+    /// </summary>
+    public class ObservableViewModelCollection<T> : ObservableCollection<T>
+        where T : INotifyPropertyChanged
+    {
+        public event EventHandler<ElementPropertyChangedEventArgs<T>> ElementPropertyChanged;
+
+        public ObservableViewModelCollection()
+        {
+            CollectionChanged += UpdateContributorEventHandlers;
+        }
+
+        public ObservableViewModelCollection(IEnumerable<T> items) : this()
+        {
+            foreach (var item in items)
+            {
+                this.Add(item);
+            }
+        }
+
+        private void UpdateContributorEventHandlers(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            foreach (INotifyPropertyChanged added in e.NewItems ?? new List<INotifyPropertyChanged>())
+            {
+                added.PropertyChanged += OnItemPropertyChanged;
+            }
+            foreach (INotifyPropertyChanged removed in e.OldItems ?? new List<INotifyPropertyChanged>())
+            {
+                removed.PropertyChanged -= OnItemPropertyChanged;
+            }
+        }
+
+        private void OnItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var specializedArgs = new ElementPropertyChangedEventArgs<T>((T)sender, e.PropertyName);
+            ElementPropertyChanged?.Invoke(this, specializedArgs);
+        }
+    }
+
+    public class ElementPropertyChangedEventArgs<T>
+    {
+        public T Element { get; private set; }
+        public string PropertyName { get; private set; }
+
+        public ElementPropertyChangedEventArgs(T element, string propertyName) 
+        {
+            Element = element;
+            PropertyName = propertyName;
+        }
+    }
+}
