7f42db184328eedc68473ec3aa78b98adbff03a1
jonathan-rizk <jonrizk@protonmail.com>
2019-05-19 14:12:34 -0500

    added ExcelHotKeyAnnotation class. added test case for new attribute.

---
+++ Rubberduck.Parsing/Annotations/ExcelHotKeyAnnotation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rubberduck.Parsing.Grammar;
+using Rubberduck.VBEditor;
+
+namespace Rubberduck.Parsing.Annotations
+{
+    public sealed class ExcelHotKeyAnnotation : FlexibleAttributeValueAnnotationBase
+    {
+        public ExcelHotKeyAnnotation(AnnotationType annotationType, QualifiedSelection qualifiedSelection, VBAParser.AnnotationContext context, IEnumerable<string> attributeValues) :
+            base(annotationType, qualifiedSelection, context, GetHotKeyAttributeValue(attributeValues))
+        { }
+
+        private static IEnumerable<string> GetHotKeyAttributeValue(IEnumerable<string> attributeValues) => 
+            attributeValues.Take(1).Select(StripStringLiteralQuotes).Select(v => v[0] + @"\n14").ToList();
+
+        private static string StripStringLiteralQuotes(string value) =>
+            value.StartsWith("\"") && value.EndsWith("\"") && value.Length > 2
+                ? value.Substring(1, value.Length - 2)
+                : value;
+    }
+}
