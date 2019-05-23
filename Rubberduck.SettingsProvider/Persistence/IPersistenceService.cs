a22d951d9344204a0f3cf189e769d92d3594fc63
Vogel612 <vogel612@gmx.de>
2019-05-23 23:32:01 +0200

    Correct Docstring for IPersistenceService

--- Rubberduck.SettingsProvider/Persistence/IPersistenceService.cs
+++ Rubberduck.SettingsProvider/Persistence/IPersistenceService.cs
@@ -3,12 +3,19 @@
     /// <summary>
     /// An interface exposing persistent storage for a serializable type.
     /// This is intended to be used by Settings only.
-    /// 
+    /// </summary>
+    /// <remarks>
     /// When the optional parameters are given, they override the storage location of the instance state for that one invocation.
     /// Implementations of this interface are expected to <b>not</b> provide any caching.
-    /// 
-    /// Implementations can choose to disregard the optional argument being
-    /// </summary>
+    /// The following property must hold for a given instance of IPersistenceService:
+    /// <code>
+    /// T value;
+    /// string arg; // can be null
+    /// IPersistenceService<T> service;
+    /// service.Save(value, arg);
+    /// service.Load(arg).Equals(value); // must be true
+    /// </code>
+    /// </remarks>
     /// <typeparam name="T">The Type of serializable object to store and retrieve persistently</typeparam>
     public interface IPersistenceService<T> where T : new()
     {
