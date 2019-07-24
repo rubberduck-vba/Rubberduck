8f133435387ef29abbfb8c234da755f609fba31e
IvenBach <ivenbach@gmail.com>
2019-07-24 09:28:10 -0700

    Remove unused using directive

--- Rubberduck.Core/UI/UnitTesting/TestExplorerViewModel.cs
+++ Rubberduck.Core/UI/UnitTesting/TestExplorerViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
