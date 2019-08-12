53b4ae3736f3f273248ae344daf0638d227bee9c
bclothier <bgclothier@gmail.com>
2019-08-11 19:10:27 -0500

    First version of MockExpressionBuilder to help abstract away the building of dynamic expressions for various Moq operations and provide a fluent API to make it easier to use because dynamic expressions are very difficult to work and do not really represent the statically-typed code equivalent, making it hard to build a Moq Mock at runtime.

---
+++ Rubberduck.Main/ComClientLibrary/UnitTesting/Mocks/MockExpressionBuilder.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using Moq;
+
+namespace Rubberduck.ComClientLibrary.UnitTesting.Mocks
+{
+    public interface IRuntimeMock
+    {
+        IRuntimeSetup As(Type targetInterface);
+    }
+
+    public interface IRuntimeSetup : IRuntimeMock, IRuntimeCallback, IRuntimeExecute
+    {
+        IRuntimeCallback Setup(Expression setupExpression);
+        IRuntimeReturns Setup(Expression setupExpression, Type returnType);
+    }
+
+    public interface IRuntimeCallback : IRuntimeExecute
+    {
+        IRuntimeSetup Callback(Action Callback);
+    }
+
+    public interface IRuntimeReturns : IRuntimeExecute
+    {
+        IRuntimeSetup Returns(object value, Type type);
+    }
+
+    public interface IRuntimeExecute
+    {
+        object Execute();
+    }
+
+    // Some interfaces are already implemented by others but we list them all explicitly for clarity
+    [SuppressMessage("ReSharper", "RedundantExtendsListEntry")]
+    public class MockExpressionBuilder :
+        IRuntimeMock,
+        IRuntimeSetup,
+        IRuntimeCallback,
+        IRuntimeReturns,
+        IRuntimeExecute
+    {
+        private readonly Mock _mock;
+        private readonly Type _mockType;
+        private readonly ParameterExpression _mockParameterExpression;
+        private Expression _expression;
+        private Type _currentType;
+        private List<object> _args;
+
+        public static IRuntimeMock Create(Mock runtimeMock)
+        {
+            return new MockExpressionBuilder(runtimeMock);
+        }
+
+        private MockExpressionBuilder(Mock runtimeMock)
+        {
+            _mock = runtimeMock;
+            _mockType = _mock.GetType();
+            _mockParameterExpression = Expression.Parameter(_mockType, "mock");
+            _args = new List<object>();
+        }
+
+        public IRuntimeSetup As(Type targetInterface)
+        {
+            var asMethodInfo = MockMemberInfos.As(targetInterface);
+            _expression = Expression.Call(_mockParameterExpression, asMethodInfo);
+            _currentType = asMethodInfo.ReturnType;
+            return this;
+        }
+
+        public IRuntimeCallback Setup(Expression setupExpression)
+        {
+            var setupMethodInfo = MockMemberInfos.Setup(_currentType, null);
+            _expression = Expression.Call(_expression, setupMethodInfo, setupExpression);
+            _currentType = setupMethodInfo.ReturnType;
+            return this;
+        }
+
+        public IRuntimeReturns Setup(Expression setupExpression, Type returnType)
+        {
+            var setupMethodInfo = MockMemberInfos.Setup(_currentType, returnType);
+            _expression = Expression.Call(_expression, setupMethodInfo, setupExpression);
+            _currentType = setupMethodInfo.ReturnType;
+            return this;
+        }
+
+        public IRuntimeSetup Callback(Action callback)
+        {
+            var callbackMethodInfo = MockMemberInfos.Callback(_currentType);
+            var callbackType = callbackMethodInfo.DeclaringType;
+
+            var valueParameterExpression = Expression.Parameter(callback.GetType(), "value");
+
+            var castCallbackExpression = Expression.Convert(_expression, callbackType);
+            var callCallbackExpression =
+                Expression.Call(castCallbackExpression, callbackMethodInfo, valueParameterExpression);
+            _expression = Expression.Lambda(callCallbackExpression, _mockParameterExpression,
+                valueParameterExpression);
+            _currentType = callbackMethodInfo.ReturnType;
+            _args.Add(callback);
+            return this;
+        }
+
+        public IRuntimeSetup Returns(object value, Type type)
+        {
+            var returnsMethodInfo = MockMemberInfos.Returns(_currentType);
+            var returnsType = returnsMethodInfo.DeclaringType;
+
+            var valueParameterExpression = Expression.Parameter(type, $"value{_args.Count}");
+
+            var castReturnExpression = Expression.Convert(_expression, returnsType);
+            var returnsCallExpression = Expression.Call(castReturnExpression, returnsMethodInfo, valueParameterExpression);
+            _expression = Expression.Lambda(returnsCallExpression, _mockParameterExpression, valueParameterExpression);
+            _currentType = returnsMethodInfo.ReturnType;
+            _args.Add(value);
+            return this;
+        }
+
+        public object Execute()
+        {
+            var args = _args.Count >= 0 ? new List<object> {_mock}.Concat(_args).ToArray() : _args.ToArray();
+
+            return _expression.NodeType == ExpressionType.Lambda 
+                ? ((LambdaExpression)_expression).Compile().DynamicInvoke(args)
+                : Expression.Lambda(_expression, _mockParameterExpression).Compile().DynamicInvoke(args);
+        }
+    }
+}
---
+++ RubberduckTests/ComMock/MockExpressionBuilderTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+using Rubberduck.ComClientLibrary.UnitTesting.Mocks;
+
+namespace RubberduckTests.ComMock
+{
+    [TestFixture]
+    public class MockExpressionBuilderTests
+    {
+        [Test]
+        public void As_Compiles()
+        {
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+
+            builder.As(typeof(ITest1))
+                   .Execute();
+        }
+
+        [Test]
+        public void Setup_Void_Method_Compiles()
+        {
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+            var expression = ArrangeSetupDoExpression();
+
+            builder.As(typeof(ITest1))
+                .Setup(expression)
+                .Execute();
+        }
+
+        [Test]
+        [Ignore("Require additional implementation to wrap a returning method in an Action<>")]
+        public void Setup_Returning_Method_With_Return_Ignored_Compiles()
+        {
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+            var expression = ArrangeSetupDoThisExpression();
+
+            builder.As(typeof(ITest1))
+                .Setup(expression)
+                .Execute();
+        }
+
+        [Test]
+        public void Setup_Returning_Method_Compiles()
+        {
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+            var expression = ArrangeSetupDoThisExpression();
+
+            builder.As(typeof(ITest1))
+                   .Setup(expression, typeof(int))
+                   .Execute();
+        }
+
+        [Test]
+        public void Setup_With_Returns_Compiles()
+        {
+            const int expected = 42;
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+            var expression = ArrangeSetupDoThisExpression();
+
+            builder.As(typeof(ITest1))
+                .Setup(expression, typeof(int))
+                .Returns(expected, typeof(int))
+                .Execute();
+
+            Assert.AreEqual(expected, mock.Object.DoThis());
+        }
+
+        [Test]
+        public void Setup_With_Callback_Compiles()
+        {
+            const int expected = 42;
+            var actual = 0;
+            var action = new Action(() => { actual = expected; });
+            var mock = new Mock<ITest1>();
+            var builder = MockExpressionBuilder.Create(mock);
+            var expression = ArrangeSetupDoExpression();
+
+            builder.As(typeof(ITest1))
+                .Setup(expression)
+                .Callback(action)
+                .Execute();
+
+            mock.Object.Do();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static Expression ArrangeSetupDoExpression()
+        {
+            var typeParameterExpression = Expression.Parameter(typeof(ITest1), "x");
+            var methodInfo = typeof(ITest1).GetMethod(nameof(ITest1.Do));
+            var callExpression = Expression.Call(typeParameterExpression, methodInfo);
+            return Expression.Lambda(callExpression, typeParameterExpression);
+        }
+
+        private static Expression ArrangeSetupDoThisExpression()
+        {
+            var typeParameterExpression = Expression.Parameter(typeof(ITest1), "x");
+            var methodInfo = typeof(ITest1).GetMethod(nameof(ITest1.DoThis));
+            var callExpression = Expression.Call(typeParameterExpression, methodInfo);
+            return Expression.Lambda(callExpression, typeParameterExpression);
+        }
+    }
+}
