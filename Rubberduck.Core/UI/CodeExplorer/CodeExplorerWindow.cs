6888cc7a862e90dae6ae357c0e0b16e84f68439c
IvenBach <ivenbach@gmail.com>
2019-08-21 13:30:46 -0700

    Remove using directive

--- Rubberduck.Core/UI/CodeExplorer/CodeExplorerWindow.cs
+++ Rubberduck.Core/UI/CodeExplorer/CodeExplorerWindow.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
 using Rubberduck.Navigation.CodeExplorer;
 using Rubberduck.Resources.CodeExplorer;
