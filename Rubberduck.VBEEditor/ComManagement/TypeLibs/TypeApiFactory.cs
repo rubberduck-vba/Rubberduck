fbcc077dbcf40d3a64ff44a0cc1d3e2b6fa550dd
bclothier <bgclothier@gmail.com>
2019-10-08 22:27:33 -0500

    Update tracers and factory

--- Rubberduck.VBEEditor/ComManagement/TypeLibs/TypeApiFactory.cs
+++ Rubberduck.VBEEditor/ComManagement/TypeLibs/TypeApiFactory.cs
@@ -1,13 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices.ComTypes;
 using Rubberduck.VBEditor.ComManagement.TypeLibs.Abstract;
+using Rubberduck.VBEditor.ComManagement.TypeLibs.DebugInternal;
+using Rubberduck.VBEditor.ComManagement.TypeLibs.Unmanaged;
 
 // TODO The tracers are broken - using them will cause a NRE inside the 
 // unmanaged boundary. If we need to enable them for diagnostics, this needs
 // to be fixed first. 
-#if DEBUG && TRACE_TYPEAPI
-using Rubberduck.VBEditor.ComManagement.TypeLibs.DebugInternal;
-#endif
 
 namespace Rubberduck.VBEditor.ComManagement.TypeLibs
 {
@@ -21,32 +21,37 @@ namespace Rubberduck.VBEditor.ComManagement.TypeLibs
     {
         internal static ITypeLibWrapper GetTypeLibWrapper(IntPtr rawObjectPtr, bool addRef)
         {
-            var wrapper = new TypeLibWrapper(rawObjectPtr, addRef);
-#if DEBUG && TRACE_TYPEAPI
-            return new TypeLibWrapperTracer(wrapper, wrapper);
-#else
+            ITypeLibWrapper wrapper = new TypeLibWrapper(rawObjectPtr, addRef);
+            TraceWrapper(ref wrapper);
             return wrapper;
-#endif
+        }
+
+        [Conditional("DEBUG")]
+        [Conditional("TRACE_TYPEAPI")]
+        private static void TraceWrapper(ref ITypeLibWrapper wrapper)
+        {
+            wrapper = new TypeLibWrapperTracer(wrapper, (ITypeLibInternal)wrapper);
         }
 
         internal static ITypeInfoWrapper GetTypeInfoWrapper(IntPtr rawObjectPtr, int? parentUserFormUniqueId = null)
         {
-            var wrapper = new TypeInfoWrapper(rawObjectPtr, parentUserFormUniqueId);
-#if DEBUG && TRACE_TYPEAPI
-            return new TypeInfoWrapperTracer(wrapper, wrapper);
-#else
+            ITypeInfoWrapper wrapper = new TypeInfoWrapper(rawObjectPtr, parentUserFormUniqueId);
+            TraceWrapper(ref wrapper);
             return wrapper;
-#endif
         }
 
         internal static ITypeInfoWrapper GetTypeInfoWrapper(ITypeInfo rawTypeInfo)
         {
-            var wrapper = new TypeInfoWrapper(rawTypeInfo);
-#if DEBUG && TRACE_TYPEAPI
-            return new TypeInfoWrapperTracer(wrapper, wrapper);
-#else
+            ITypeInfoWrapper wrapper = new TypeInfoWrapper(rawTypeInfo);
+            TraceWrapper(ref wrapper);
             return wrapper;
-#endif
+        }
+
+        [Conditional("DEBUG")]
+        [Conditional("TRACE_TYPEAPI")]
+        private static void TraceWrapper(ref ITypeInfoWrapper wrapper)
+        {
+            wrapper = new TypeInfoWrapperTracer(wrapper, (ITypeInfoInternal)wrapper);
         }
     }
 }
