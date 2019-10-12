a35ad3983411563d5d0c91c759769f230bf3f99b
Mathieu Guindon <retailcoder@gmail.com>
2019-10-12 01:28:48 -0400

    closes #5145, closes #4964

--- Rubberduck.Core/UI/Command/VersionCheckCommand.cs
+++ Rubberduck.Core/UI/Command/VersionCheckCommand.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using Rubberduck.Interaction;
 using Rubberduck.VersionCheck;
 using Rubberduck.Resources;
+using Rubberduck.SettingsProvider;
+using Rubberduck.Settings;
 
 namespace Rubberduck.UI.Command
 {
@@ -28,31 +30,37 @@ namespace Rubberduck.UI.Command
         private readonly IVersionCheck _versionCheck;
         private readonly IMessageBox _prompt;
         private readonly IExternalProcess _process;
+        IConfigurationService<Configuration> _config;
 
-        public VersionCheckCommand(IVersionCheck versionCheck, IMessageBox prompt, IExternalProcess process)
+        public VersionCheckCommand(IVersionCheck versionCheck, IMessageBox prompt, IExternalProcess process, IConfigurationService<Configuration> config)
         {
             _versionCheck = versionCheck;
             _prompt = prompt;
             _process = process;
+            _config = config;
         }
 
         protected override async void OnExecute(object parameter)
         {
-            Logger.Info("Executing version check.");
+            var settings = _config.Read().UserSettings.GeneralSettings;
+            Logger.Info("Executing version check...");
             await _versionCheck
-                .GetLatestVersionAsync()
+                .GetLatestVersionAsync(settings)
                 .ContinueWith(t =>
                 {
                     if (_versionCheck.CurrentVersion < t.Result)
                     {
-                        PromptAndBrowse(t.Result);
+                        PromptAndBrowse(t.Result, settings.IncludePreRelease);
                     }
                 });
         }
 
-        private void PromptAndBrowse(Version latestVersion)
+        private void PromptAndBrowse(Version latestVersion, bool includePreRelease)
         {
-            var prompt = string.Format(RubberduckUI.VersionCheck_NewVersionAvailable, latestVersion);
+            var buildType = includePreRelease 
+                ? RubberduckUI.VersionCheck_BuildType_PreRelease 
+                : RubberduckUI.VersionCheck_BuildType_Release;
+            var prompt = string.Format(RubberduckUI.VersionCheck_NewVersionAvailable, _versionCheck.CurrentVersion, latestVersion, buildType);
             if (!_prompt.Question(prompt, RubberduckUI.Rubberduck))
             {
                 return;
