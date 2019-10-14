a69300d1dd26637f18ff34de739eaafe246473a6
Brian Zenger <bzngr@outlook.com>
2019-10-14 16:45:08 -0700

    Modified to use InspectionTestsBase

--- RubberduckTests/Inspections/UseMeaningfulNameInspectionTests.cs
+++ RubberduckTests/Inspections/UseMeaningfulNameInspectionTests.cs
@@ -9,12 +9,21 @@ using Rubberduck.SettingsProvider;
 using Rubberduck.VBEditor.SafeComWrappers;
 using RubberduckTests.Mocks;
 using Rubberduck.CodeAnalysis.Settings;
+using Rubberduck.Parsing.VBA;
+using Rubberduck.Parsing.Inspections.Abstract;
 
 namespace RubberduckTests.Inspections
 {
     [TestFixture]
-    public class UseMeaningfulNameInspectionTests
+    public class UseMeaningfulNameInspectionTests : InspectionTestsBase
     {
+        public UseMeaningfulNameInspectionTests()
+        {
+            //The default InspectionTestsBase names are flag as meaningless names (end with a number)
+            TestProjectName = "VBAProject";
+            TestModuleName = "TestModule";
+        }
+
         [Test]
         [Category("Inspections")]
         public void UseMeaningfulName_NoResultForLineNumberLabels()
@@ -24,15 +33,7 @@ Sub DoSomething()
 10 Debug.Print 42
 End Sub
 ";
-            var vbe = MockVbeBuilder.BuildFromSingleStandardModule(inputCode, out _);
-
-            using(var state = MockParser.CreateAndParse(vbe.Object))
-            {
-                var inspection = new UseMeaningfulNameInspection(state, GetInspectionSettings().Object);
-                var inspectionResults = inspection.GetInspectionResults(CancellationToken.None).Where(i => i.Target.DeclarationType == DeclarationType.LineLabel);
-
-                Assert.IsFalse(inspectionResults.Any());
-            }
+            Assert.AreEqual(0, InspectionResultsForStandardModule(inputCode).Count());
         }
 
         [Test]
@@ -55,7 +56,7 @@ Dim a2z as String       'This is the only declaration that should pass
 Dim gGGG as String
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 10);
+            Assert.AreEqual(10, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
 
@@ -66,7 +67,7 @@ End Sub";
             const string inputCode =
 @"Sub Ffffff()
 End Sub";
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 1);
+            Assert.AreEqual(1, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -76,7 +77,7 @@ End Sub";
             const string inputCode =
 @"Sub Oo()
 End Sub";
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 1);
+            Assert.AreEqual(1, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -87,7 +88,7 @@ End Sub";
 @"Sub Foo1()
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 1);
+            Assert.AreEqual(1, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -98,7 +99,7 @@ End Sub";
 @"Sub FooBar()
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 0);
+            Assert.AreEqual(0, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -109,7 +110,7 @@ End Sub";
 @"Sub FOOBAR()
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 0);
+            Assert.AreEqual(0, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -119,7 +120,7 @@ End Sub";
             const string inputCode =
 @"Option Base 1";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 0);
+            Assert.AreEqual(0, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -130,7 +131,7 @@ End Sub";
 @"Sub sss()
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 0);
+            Assert.AreEqual(0, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
@@ -142,34 +143,16 @@ End Sub";
 Sub Ffffff()
 End Sub";
 
-            AssertVbaFragmentYieldsExpectedInspectionResultCount(inputCode, 0);
+            Assert.AreEqual(0, InspectionResultsForModules(("MyClass", inputCode, ComponentType.ClassModule)).Count());
         }
 
         [Test]
         [Category("Inspections")]
         public void InspectionName()
         {
-            const string inspectionName = "UseMeaningfulNameInspection";
             var inspection = new UseMeaningfulNameInspection(null, null);
 
-            Assert.AreEqual(inspectionName, inspection.Name);
-        }
-
-        private void AssertVbaFragmentYieldsExpectedInspectionResultCount(string inputCode, int expectedCount)
-        {
-            var builder = new MockVbeBuilder();
-            var project = builder.ProjectBuilder("VBAProject", ProjectProtection.Unprotected)
-                .AddComponent("MyClass", ComponentType.ClassModule, inputCode)
-                .Build();
-            var vbe = builder.AddProject(project).Build();
-
-            using(var state = MockParser.CreateAndParse(vbe.Object))
-            {
-                var inspection = new UseMeaningfulNameInspection(state, GetInspectionSettings().Object);
-                var inspectionResults = inspection.GetInspectionResults(CancellationToken.None);
-
-                Assert.AreEqual(expectedCount, inspectionResults.Count());
-            }
+            Assert.AreEqual(nameof(UseMeaningfulNameInspection), inspection.Name);
         }
 
         internal static Mock<IConfigurationService<CodeInspectionSettings>> GetInspectionSettings()
@@ -183,6 +166,11 @@ End Sub";
                 }, true));
 
             return settings;
+        }
+
+        protected override IInspection InspectionUnderTest(RubberduckParserState state)
+        {
+            return new UseMeaningfulNameInspection(state, GetInspectionSettings().Object);
         }
     }
 }
