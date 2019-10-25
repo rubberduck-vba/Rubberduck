7b0ca953aebb899e2cdb5325dd4f955723dfa777
IvenBach <ivenbach@gmail.com>
2019-10-25 16:27:16 -0700

    Add GroupingGridLocationConverter

---
+++ Rubberduck.Core/UI/Inspections/GroupingGridLocationConverter.cs
@@ -0,0 +1,25 @@
+﻿using Rubberduck.VBEditor;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Rubberduck.UI.Inspections
+{
+    class GroupingGridLocationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is QualifiedModuleName qualifiedModuleName)
+            {
+                return $"{qualifiedModuleName} - {qualifiedModuleName.ComponentType}";
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
