25cbc557474f745454d1a4e7195ca1845cb37f42
IvenBach <ivenbach@gmail.com>
2019-10-31 11:10:56 -0700

    Revert "Update resource string"
    This reverts commit 6713c2dd80368dab7bbf0cc8123ba6d1b60e87ab.
    Was breaking build process.

--- Rubberduck.Resources/RubberduckUI.Designer.cs
+++ Rubberduck.Resources/RubberduckUI.Designer.cs
@@ -1594,6 +1594,15 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Tell me if a newer pre-release build is available.
+        /// </summary>
+        public static string GeneralSettings_IncludePreRelease {
+            get {
+                return ResourceManager.GetString("GeneralSettings_IncludePreRelease", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Info.
         /// </summary>
         public static string GeneralSettings_InfoLogLevel {
@@ -1711,7 +1720,7 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Filter.
+        ///   Looks up a localized string similar to Issue Description.
         /// </summary>
         public static string GroupingGrid_Filter {
             get {
@@ -4388,7 +4397,30 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Rubberduck version {0} is now available! Would you like to review the release notes now?.
+        ///   Looks up a localized string similar to pre-release.
+        /// </summary>
+        public static string VersionCheck_BuildType_PreRelease {
+            get {
+                return ResourceManager.GetString("VersionCheck_BuildType_PreRelease", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to release.
+        /// </summary>
+        public static string VersionCheck_BuildType_Release {
+            get {
+                return ResourceManager.GetString("VersionCheck_BuildType_Release", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You are currently running Rubberduck version {0}.
+        ///Good news! Version {1} ({2}) is now available.
+        ///
+        ///To review the change log and manually upgrade Rubberduck, click &apos;Yes&apos;.
+        ///
+        ///This dialog is showing because version checks are enabled. You can turn off this feature in Rubberduck &gt; Settings..
         /// </summary>
         public static string VersionCheck_NewVersionAvailable {
             get {
