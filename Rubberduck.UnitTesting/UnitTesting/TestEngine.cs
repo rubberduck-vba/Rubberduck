0f90058175bd0b7068d410b3c634ee5aff008359
Max Doerner <maxdoerner@gmx.net>
2019-11-08 22:50:28 +0100

    Suspend from a background thread in the TestEngine
    Now, we push the suspension to a background thread and push execution back to the UI thread from within to avoid a potential deadlock on acquiring the suspension lock.

--- Rubberduck.UnitTesting/UnitTesting/TestEngine.cs
+++ Rubberduck.UnitTesting/UnitTesting/TestEngine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using NLog;
 using Rubberduck.Parsing.Annotations;
 using Rubberduck.Parsing.Symbols;
@@ -176,7 +177,8 @@ namespace Rubberduck.UnitTesting
             {
                 return;
             }
-            _state.OnSuspendParser(this, AllowedRunStates, () => RunWhileSuspended(tests));
+            //We push the suspension to a background thread to avoid potential deadlocks if a parse is still running.
+            Task.Run(() => _state.OnSuspendParser(this, AllowedRunStates, () => RunWhileSuspended(tests)));
         }
 
         private void EnsureRubberduckIsReferencedForEarlyBoundTests()
@@ -198,6 +200,14 @@ namespace Rubberduck.UnitTesting
         }
 
         private void RunWhileSuspended(IEnumerable<TestMethod> tests)
+        {
+            //Running the tests has to be done on the UI thread, so we push the task to it from within suspension of the parser.
+            //We have to wait for the completion to make sure that the suspension only ends after tests have been completed.
+            var testTask = _uiDispatcher.StartTask(() => RunWhileSuspendedOnUIThread(tests));
+            testTask.Wait();
+        }
+
+        private void RunWhileSuspendedOnUIThread(IEnumerable<TestMethod> tests)
         {
             var testMethods = tests as IList<TestMethod> ?? tests.ToList();
             if (!testMethods.Any())
