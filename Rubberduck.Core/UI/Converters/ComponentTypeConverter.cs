69e5d1e28593a083a4bc58efdd17ddfeada379c0
IvenBach <ivenbach@gmail.com>
2019-11-10 11:59:52 -0800

    Update resource strings
    Removed where keys might be used.

--- Rubberduck.Core/UI/Converters/ComponentTypeConverter.cs
+++ Rubberduck.Core/UI/Converters/ComponentTypeConverter.cs
@@ -14,33 +14,33 @@ namespace Rubberduck.UI.Converters
                 switch (componentType)
                 {
                     case ComponentType.ComComponent:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_ComComponent;
+                        return Resources.RubberduckUI.ComponentType_ComComponent;
                     case ComponentType.Undefined:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_Undefined;
+                        return Resources.RubberduckUI.ComponentType_Undefined;
                     case ComponentType.StandardModule:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_StandardModule;
+                        return Resources.RubberduckUI.ComponentType_StandardModule;
                     case ComponentType.ClassModule:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_ClassModule;
+                        return Resources.RubberduckUI.ComponentType_ClassModule;
                     case ComponentType.UserForm:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_UserForm;
+                        return Resources.RubberduckUI.ComponentType_UserForm;
                     case ComponentType.ResFile:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_ResFile;
+                        return Resources.RubberduckUI.ComponentType_ResFile;
                     case ComponentType.VBForm:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_VBForm;
+                        return Resources.RubberduckUI.ComponentType_VBForm;
                     case ComponentType.MDIForm:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_MDIForm;
+                        return Resources.RubberduckUI.ComponentType_MDIForm;
                     case ComponentType.PropPage:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_PropPage;
+                        return Resources.RubberduckUI.ComponentType_PropPage;
                     case ComponentType.UserControl:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_UserControl;
+                        return Resources.RubberduckUI.ComponentType_UserControl;
                     case ComponentType.DocObject:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_DocObject;
+                        return Resources.RubberduckUI.ComponentType_DocObject;
                     case ComponentType.RelatedDocument:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_RelatedDocument;
+                        return Resources.RubberduckUI.ComponentType_RelatedDocument;
                     case ComponentType.ActiveXDesigner:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_ActiveXDesigner;
+                        return Resources.RubberduckUI.ComponentType_ActiveXDesigner;
                     case ComponentType.Document:
-                        return Resources.RubberduckUI.CodeInspections_ComponentType_Document;
+                        return Resources.RubberduckUI.ComponentType_Document;
                     default:
                         return Binding.DoNothing;
                 }
--- Rubberduck.Resources/RubberduckUI.Designer.cs
+++ Rubberduck.Resources/RubberduckUI.Designer.cs
@@ -370,132 +370,6 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ActiveX Designer.
-        /// </summary>
-        public static string CodeInspections_ComponentType_ActiveXDesigner {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_ActiveXDesigner", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Class Module.
-        /// </summary>
-        public static string CodeInspections_ComponentType_ClassModule {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_ClassModule", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to COM component.
-        /// </summary>
-        public static string CodeInspections_ComponentType_ComComponent {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_ComComponent", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Document Object.
-        /// </summary>
-        public static string CodeInspections_ComponentType_DocObject {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_DocObject", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Document Module.
-        /// </summary>
-        public static string CodeInspections_ComponentType_Document {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_Document", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to MDI Form.
-        /// </summary>
-        public static string CodeInspections_ComponentType_MDIForm {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_MDIForm", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Property Page.
-        /// </summary>
-        public static string CodeInspections_ComponentType_PropPage {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_PropPage", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Related Document.
-        /// </summary>
-        public static string CodeInspections_ComponentType_RelatedDocument {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_RelatedDocument", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Resource File.
-        /// </summary>
-        public static string CodeInspections_ComponentType_ResFile {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_ResFile", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Standard Module.
-        /// </summary>
-        public static string CodeInspections_ComponentType_StandardModule {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_StandardModule", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Undefined.
-        /// </summary>
-        public static string CodeInspections_ComponentType_Undefined {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_Undefined", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to User Control.
-        /// </summary>
-        public static string CodeInspections_ComponentType_UserControl {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_UserControl", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to UserForm.
-        /// </summary>
-        public static string CodeInspections_ComponentType_UserForm {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_UserForm", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to VB Form.
-        /// </summary>
-        public static string CodeInspections_ComponentType_VBForm {
-            get {
-                return ResourceManager.GetString("CodeInspections_ComponentType_VBForm", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Copy inspection results to clipboard.
         /// </summary>
         public static string CodeInspections_CopyToolTip {
@@ -890,6 +764,132 @@ namespace Rubberduck.Resources {
         public static string Component {
             get {
                 return ResourceManager.GetString("Component", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ActiveX Designer.
+        /// </summary>
+        public static string ComponentType_ActiveXDesigner {
+            get {
+                return ResourceManager.GetString("ComponentType_ActiveXDesigner", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Class Module.
+        /// </summary>
+        public static string ComponentType_ClassModule {
+            get {
+                return ResourceManager.GetString("ComponentType_ClassModule", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to COM component.
+        /// </summary>
+        public static string ComponentType_ComComponent {
+            get {
+                return ResourceManager.GetString("ComponentType_ComComponent", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Document Object.
+        /// </summary>
+        public static string ComponentType_DocObject {
+            get {
+                return ResourceManager.GetString("ComponentType_DocObject", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Document Module.
+        /// </summary>
+        public static string ComponentType_Document {
+            get {
+                return ResourceManager.GetString("ComponentType_Document", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to MDI Form.
+        /// </summary>
+        public static string ComponentType_MDIForm {
+            get {
+                return ResourceManager.GetString("ComponentType_MDIForm", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Property Page.
+        /// </summary>
+        public static string ComponentType_PropPage {
+            get {
+                return ResourceManager.GetString("ComponentType_PropPage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Related Document.
+        /// </summary>
+        public static string ComponentType_RelatedDocument {
+            get {
+                return ResourceManager.GetString("ComponentType_RelatedDocument", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Resource File.
+        /// </summary>
+        public static string ComponentType_ResFile {
+            get {
+                return ResourceManager.GetString("ComponentType_ResFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Standard Module.
+        /// </summary>
+        public static string ComponentType_StandardModule {
+            get {
+                return ResourceManager.GetString("ComponentType_StandardModule", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Undefined.
+        /// </summary>
+        public static string ComponentType_Undefined {
+            get {
+                return ResourceManager.GetString("ComponentType_Undefined", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to User Control.
+        /// </summary>
+        public static string ComponentType_UserControl {
+            get {
+                return ResourceManager.GetString("ComponentType_UserControl", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UserForm.
+        /// </summary>
+        public static string ComponentType_UserForm {
+            get {
+                return ResourceManager.GetString("ComponentType_UserForm", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to VB Form.
+        /// </summary>
+        public static string ComponentType_VBForm {
+            get {
+                return ResourceManager.GetString("ComponentType_VBForm", resourceCulture);
             }
         }
         
