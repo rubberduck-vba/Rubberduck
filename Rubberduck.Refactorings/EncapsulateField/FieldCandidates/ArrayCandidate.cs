c75ada4c44020729fdcab9116b275ad17cdab8a4
Brian Zenger <bzngr@outlook.com>
2019-12-20 16:38:35 -0800

    In source module, use module variable internally

--- Rubberduck.Refactorings/EncapsulateField/FieldCandidates/ArrayCandidate.cs
+++ Rubberduck.Refactorings/EncapsulateField/FieldCandidates/ArrayCandidate.cs
@@ -1,11 +1,8 @@
-﻿using Rubberduck.Parsing;
+﻿using Antlr4.Runtime;
+using Rubberduck.Parsing;
 using Rubberduck.Parsing.Grammar;
 using Rubberduck.Parsing.Symbols;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rubberduck.Refactorings.EncapsulateField
 {
@@ -31,5 +28,41 @@ namespace Rubberduck.Refactorings.EncapsulateField
 
         public override string AsUDTMemberDeclaration
             => $"{PropertyName}({_subscripts}) {Tokens.As} {AsTypeName_Field}";
+
+        public override void StageFieldReferenceReplacements(IStateUDT stateUDT = null)
+        {
+            PropertyAccessor = stateUDT is null ? AccessorTokens.Field : AccessorTokens.Property;
+            ReferenceAccessor = AccessorTokens.Property;
+            ReferenceQualifier = stateUDT?.FieldIdentifier ?? null;
+            LoadFieldReferenceContextReplacements();
+        }
+
+        protected override void LoadFieldReferenceContextReplacements()
+        {
+            var field = this;
+            foreach (var idRef in field.Declaration.References)
+            {
+                var replacementText = RequiresAccessQualification(idRef)
+                    ? $"{field.QualifiedModuleName.ComponentName}.{field.ReferenceForPreExistingReferences}"
+                    : field.FieldIdentifier;
+
+                field.SetReferenceRewriteContent(idRef, replacementText);
+            }
+        }
+
+        public override void SetReferenceRewriteContent(IdentifierReference idRef, string replacementText)
+        {
+            var context = idRef.Context;
+            if (idRef.Context is VBAParser.IndexExprContext idxExpression)
+            {
+                context = idxExpression.children.ElementAt(0) as ParserRuleContext;
+            }
+            if (IdentifierReplacements.ContainsKey(idRef))
+            {
+                IdentifierReplacements[idRef] = (context, replacementText);
+                return;
+            }
+            IdentifierReplacements.Add(idRef, (context, replacementText));
+        }
     }
 }
