8bdf90f3b310e4fda17c8f55fdd18ab86ac5bebf
Brian Zenger <bzngr@outlook.com>
2019-12-22 19:49:34 -0800

    sizing the Preview textBox

--- Rubberduck.Core/UI/Refactorings/EncapsulateField/EncapsulateFieldPresenter.cs
+++ Rubberduck.Core/UI/Refactorings/EncapsulateField/EncapsulateFieldPresenter.cs
@@ -6,7 +6,7 @@ namespace Rubberduck.UI.Refactorings.EncapsulateField
     internal class EncapsulateFieldPresenter : RefactoringPresenterBase<EncapsulateFieldModel>, IEncapsulateFieldPresenter
     {
         private static readonly DialogData DialogData =
-            DialogData.Create(RubberduckUI.EncapsulateField_Caption, 800, 1000);
+            DialogData.Create(RubberduckUI.EncapsulateField_Caption, 800, 900);
 
         public EncapsulateFieldPresenter(EncapsulateFieldModel model,
             IRefactoringDialogFactory dialogFactory) : base(DialogData, model, dialogFactory) { }
