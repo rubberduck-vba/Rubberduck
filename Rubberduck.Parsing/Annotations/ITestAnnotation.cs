57521e509755eb627e5a4f6a7feef2d11737ce16
jonathan-rizk <jonrizk@protonmail.com>
2020-01-28 16:57:57 -0600

    removed unnecessary namespaces

--- Rubberduck.Parsing/Annotations/ITestAnnotation.cs
+++ Rubberduck.Parsing/Annotations/ITestAnnotation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rubberduck.Parsing.Annotations
+﻿namespace Rubberduck.Parsing.Annotations
 {
     public interface ITestAnnotation
     {
