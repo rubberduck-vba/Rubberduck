8781f560a7375583839c4480f155208b0d99c8f1
jonathan-rizk <jonrizk@protonmail.com>
2020-01-28 11:16:06 -0600

    ProcedureNotUsedInspection ignores declarations that have ITestAnnotation annotations.

--- Rubberduck.Parsing/Annotations/Concrete/ModuleCleanupAnnotation.cs
+++ Rubberduck.Parsing/Annotations/Concrete/ModuleCleanupAnnotation.cs
@@ -7,7 +7,7 @@ namespace Rubberduck.Parsing.Annotations
     /// <summary>
     /// Marks a method that the test engine will execute after all unit tests in a test module have executed.
     /// </summary>
-    public sealed class ModuleCleanupAnnotation : AnnotationBase
+    public sealed class ModuleCleanupAnnotation : AnnotationBase, ITestAnnotation
     {
         public ModuleCleanupAnnotation()
             : base("ModuleCleanup", AnnotationTarget.Member)
---
+++ Rubberduck.Parsing/Annotations/ITestAnnotation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubberduck.Parsing.Annotations
+{
+    public interface ITestAnnotation
+    {
+    }
+}
