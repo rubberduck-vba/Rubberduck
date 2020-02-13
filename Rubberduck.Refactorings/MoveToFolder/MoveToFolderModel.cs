eb8dd7b3e8452127ae91c2b8c3abfc629db197af
Max Doerner <maxdoerner@gmx.net>
2020-02-14 00:42:02 +0100

    Add MoveToFolderBaseRefactoring
    Also adds BaseRefactoringTestBase to support testing the base refactorings in separation from the full implementations of IRefactoring.

---
+++ Rubberduck.Refactorings/MoveToFolder/MoveToFolderModel.cs
@@ -0,0 +1,16 @@
+﻿using Rubberduck.Parsing.Symbols;
+
+namespace Rubberduck.Refactorings.MoveToFolder
+{
+    public class MoveToFolderModel : IRefactoringModel
+    {
+        public ModuleDeclaration Target { get; }
+        public string TargetFolder { get; }
+
+        public MoveToFolderModel(ModuleDeclaration target, string targetFolder)
+        {
+            Target = target;
+            TargetFolder = targetFolder;
+        }
+    }
+}
