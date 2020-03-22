9b2fc3be1129cd5fde87649e41e8cdbe32d98c2f
Max Doerner <maxdoerner@gmx.net>
2020-03-22 22:55:39 +0100

    Stop moving components into folders they are already in
    The exception is moving into the project folder. This simply adds the explicit folder annotation.

--- RubberduckTests/Refactoring/MoveToFolder/MoveToFolderRefactoringActionTests.cs
+++ RubberduckTests/Refactoring/MoveToFolder/MoveToFolderRefactoringActionTests.cs
@@ -6,6 +6,8 @@ using Rubberduck.Parsing.Symbols;
 using Rubberduck.Parsing.VBA;
 using Rubberduck.Refactorings;
 using Rubberduck.Refactorings.MoveToFolder;
+using Rubberduck.VBEditor.SafeComWrappers;
+using RubberduckTests.Mocks;
 
 namespace RubberduckTests.Refactoring.MoveToFolder
 {
@@ -89,6 +91,62 @@ End Sub
             var refactoredCode = RefactoredCode(code, modelBuilder);
 
             Assert.AreEqual(expectedCode, refactoredCode);
+        }
+
+        [Test]
+        [Category("Refactorings")]
+        public void MoveToFolderBaseRefactoring_HasAnnotation_SameFolder_DoesNotDoAnything()
+        {
+            const string code = @"
+'@Folder(""MyOldFolder.MyOldSubfolder.SubSub"")
+Public Sub Foo()
+End Sub
+";
+            const string expectedCode = code;
+            Func<RubberduckParserState, MoveToFolderModel> modelBuilder = (state) =>
+            {
+                var module = state.DeclarationFinder
+                    .UserDeclarations(DeclarationType.ProceduralModule)
+                    .Single() as ModuleDeclaration;
+                return new MoveToFolderModel(module, "MyOldFolder.MyOldSubfolder.SubSub");
+            };
+
+            var refactoredCode = RefactoredCode(code, modelBuilder);
+
+            Assert.AreEqual(expectedCode, refactoredCode);
+        }
+
+        [Test]
+        [Category("Refactorings")]
+        public void MoveToFolderBaseRefactoring_NoAnnotation_SameFolder_AddsAnnotation()
+        {
+            const string code = @"
+Public Sub Foo()
+End Sub
+";
+            const string expectedCode = @"'@Folder ""TestProject""
+
+Public Sub Foo()
+End Sub
+";
+            Func<RubberduckParserState, MoveToFolderModel> modelBuilder = (state) =>
+            {
+                var module = state.DeclarationFinder
+                    .UserDeclarations(DeclarationType.ProceduralModule)
+                    .Single() as ModuleDeclaration;
+                return new MoveToFolderModel(module, "TestProject");
+            };
+
+            var vbe = new MockVbeBuilder()
+                .ProjectBuilder("TestProject", ProjectProtection.Unprotected)
+                .AddComponent("TestModule", ComponentType.StandardModule, code)
+                .AddProjectToVbeBuilder()
+                .Build()
+                .Object;
+
+            var refactoredCode = RefactoredCode(vbe, modelBuilder);
+
+            Assert.AreEqual(expectedCode, refactoredCode["TestModule"]);
         }
 
         protected override IRefactoringAction<MoveToFolderModel> TestBaseRefactoring(RubberduckParserState state, IRewritingManager rewritingManager)
