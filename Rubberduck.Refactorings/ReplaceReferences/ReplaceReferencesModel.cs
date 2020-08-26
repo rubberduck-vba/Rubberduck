7b55d9be6cee3aa430a07a6f12dfbcb185c06026
Brian Zenger <bzngr@outlook.com>
2020-08-26 14:02:55 -0700

    Add ReplaceReferencesRefactoringAction
    Supports renaming an IdentifierReference independent of its Declaration.
    
    Renamed ReplaceFieldReferencesRefactoringActionTests to
    ReplaceReferencesRefactoringActionTests to match the refactoring action
    class name.  Modified namespace accordingly.

---
+++ Rubberduck.Refactorings/ReplaceReferences/ReplaceReferencesModel.cs
@@ -0,0 +1,25 @@
+﻿using Rubberduck.Parsing.Symbols;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rubberduck.Refactorings.ReplaceReferences
+{
+    public class ReplaceReferencesModel :IRefactoringModel
+    {
+        private Dictionary<IdentifierReference, string> _fieldTargets = new Dictionary<IdentifierReference, string>();
+
+        public bool ModuleQualifyExternalReferences { set; get; } = false;
+
+        public void AssignFieldReferenceReplacementExpression(IdentifierReference fieldReference, string replacementIdentifier)
+        {
+            if (_fieldTargets.ContainsKey(fieldReference))
+            {
+                _fieldTargets[fieldReference] = replacementIdentifier;
+                return;
+            }
+            _fieldTargets.Add(fieldReference, replacementIdentifier);
+        }
+        public IReadOnlyList<(IdentifierReference IdentifierReference, string NewName)> FieldReferenceReplacementPairs
+            => _fieldTargets.Select(t => (t.Key, t.Value)).ToList();
+    }
+}
