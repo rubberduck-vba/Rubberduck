6c9b9db91bac41608e20316261c8538488a37fe9
bclothier <bgclothier@gmail.com>
2020-10-04 09:43:39 -0500

    Introduce FileSystemProvider to support the classes that cannot be DI'd.

---
+++ Rubberduck.InternalApi/Common/FileSystemProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Abstractions;
+
+namespace Rubberduck.InternalApi.Common
+{
+    public static class FileSystemProvider
+    {
+        static FileSystemProvider()
+        {
+            FileSystem = new FileSystem();
+        }
+
+        public static IFileSystem FileSystem { get; internal set; }
+    }
+}
