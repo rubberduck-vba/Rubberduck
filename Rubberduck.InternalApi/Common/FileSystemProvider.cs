2fb1eaaf37902074f43b7f78e198ab7fdc2cdcad
bclothier <bgclothier@gmail.com>
2020-10-17 11:31:14 -0500

    Add a comment about the injectable property.

--- Rubberduck.InternalApi/Common/FileSystemProvider.cs
+++ Rubberduck.InternalApi/Common/FileSystemProvider.cs
@@ -9,6 +9,7 @@ namespace Rubberduck.InternalApi.Common
             FileSystem = new FileSystem();
         }
 
+        /// <remarks>The property is injectable for use in unit testing. Requires use of <see cref="System.Runtime.CompilerServices.InternalsVisibleToAttribute"/>.</remarks>
         public static IFileSystem FileSystem { get; internal set; }
     }
 }
