3a03d528d5a4faa99b2699238f7548076dd8ad9f
IvenBach <ivenbach@gmail.com>
2020-10-21 13:29:34 -0700

    Add toggle to hide ignored tests

--- Rubberduck.Resources/RubberduckUI.Designer.cs
+++ Rubberduck.Resources/RubberduckUI.Designer.cs
@@ -4922,6 +4922,15 @@ namespace Rubberduck.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Ignored.
+        /// </summary>
+        public static string TestOutcome_Ignored {
+            get {
+                return ResourceManager.GetString("TestOutcome_Ignored", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Inconclusive.
         /// </summary>
         public static string TestOutcome_Inconclusive {
