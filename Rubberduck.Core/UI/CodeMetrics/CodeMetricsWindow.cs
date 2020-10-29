06dbac08f54885712397adf470e033b0219d3d93
IvenBach <ivenbach@gmail.com>
2020-10-29 15:59:16 -0700

    Migrate CodeMetrics resources

--- Rubberduck.Core/UI/CodeMetrics/CodeMetricsWindow.cs
+++ Rubberduck.Core/UI/CodeMetrics/CodeMetricsWindow.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
-using Rubberduck.Resources;
+using Rubberduck.CodeAnalysis;
 using Rubberduck.CodeAnalysis.CodeMetrics;
 
 namespace Rubberduck.UI.CodeMetrics
@@ -10,7 +10,7 @@ namespace Rubberduck.UI.CodeMetrics
     {
         private const string ClassId = "C5318B5A-172F-417C-88E3-B377CDA2D809";
         string IDockableUserControl.ClassId => ClassId;
-        string IDockableUserControl.Caption => RubberduckUI.CodeMetricsDockablePresenter_Caption;
+        string IDockableUserControl.Caption => CodeAnalysisUI.CodeMetricsDockablePresenter_Caption;
 
         private CodeMetricsWindow()
         {
