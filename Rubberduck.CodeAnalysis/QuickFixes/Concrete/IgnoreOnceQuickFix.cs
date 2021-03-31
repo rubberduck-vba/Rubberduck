6cab9e2d35ba83af5ae2316862b08608efbbcbb8
Mathieu Guindon <retailcoder@gmail.com>
2021-03-31 18:03:35 -0400

    closes #5444

--- Rubberduck.CodeAnalysis/QuickFixes/Concrete/IgnoreOnceQuickFix.cs
+++ Rubberduck.CodeAnalysis/QuickFixes/Concrete/IgnoreOnceQuickFix.cs
@@ -12,6 +12,62 @@ using Rubberduck.Parsing.VBA;
 namespace Rubberduck.CodeAnalysis.QuickFixes.Concrete
 {
     /// <summary>
+    /// Adds an '@EntryPoint annotation to mark as an entry point a procedure that isn't intended to be invoked by any code.
+    /// </summary>
+    /// <canfix multiple="true" procedure="true" module="true" project="true" all="true" />
+    /// <example>
+    /// <before>
+    /// <![CDATA[
+    /// Public Sub DoSomething()
+    ///     Debug.Print 42
+    /// End Sub
+    /// ]]>
+    /// </before>
+    /// <after>
+    /// <![CDATA[
+    /// '@EntryPoint
+    /// Public Sub DoSomething()
+    ///     Debug.Print 42
+    /// End Sub
+    /// ]]>
+    /// </after>
+    /// </example>
+    internal sealed class AnnotateEntryPointQuickFix : QuickFixBase
+    {
+        private readonly RubberduckParserState _state;
+        private readonly IAnnotationUpdater _annotationUpdater;
+
+        public AnnotateEntryPointQuickFix(IAnnotationUpdater annotationUpdater, RubberduckParserState state, IEnumerable<IInspection> inspections)
+            : base(inspections.Select(s => s.GetType()).Where(i => i.CustomAttributes.All(a => a.AttributeType != typeof(CannotAnnotateAttribute))).ToArray())
+        {
+            _state = state;
+            _annotationUpdater = annotationUpdater;
+        }
+
+        public override bool CanFixMultiple => true;
+        public override bool CanFixInProcedure => true;
+        public override bool CanFixInModule => true;
+        public override bool CanFixInProject => true;
+        public override bool CanFixAll => true;
+
+        public override void Fix(IInspectionResult result, IRewriteSession rewriteSession)
+        {
+            var module = result.QualifiedSelection.QualifiedName;
+            var lineToAnnotate = result.QualifiedSelection.Selection.StartLine;
+            var existingEntryPointAnnotation = _state.DeclarationFinder
+                .FindAnnotations(module, lineToAnnotate)
+                .FirstOrDefault(pta => pta.Annotation is EntryPointAnnotation);
+
+            if (existingEntryPointAnnotation == null)
+            {
+                _annotationUpdater.AddAnnotation(rewriteSession, new QualifiedContext(module, result.Context), new EntryPointAnnotation());
+            }
+        }
+
+        public override string Description(IInspectionResult result) => Resources.Inspections.QuickFixes.AnnotateEntryPointQuickFix;
+    }
+
+    /// <summary>
     /// Adds an '@Ignore annotation to ignore a specific inspection result. Applicable to all inspections whose results can be annotated in a module.
     /// </summary>
     /// <canfix multiple="true" procedure="true" module="true" project="true" all="true" />
---
+++ Rubberduck.Parsing/Annotations/Concrete/EntryPointAnnotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rubberduck.Parsing.Annotations.Concrete
+{
+    /// <summary>
+    /// @EntryPoint annotation, marks a procedure as an entry point that is not intended to be referenced directly from the code.
+    /// </summary>
+    /// <parameter name="CallerName" type="Text" required="False">
+    /// If provided, the first argument is interpreted as referring to an external caller, for example the name of a Shape in the host document.
+    /// </parameter>
+    /// <remarks>
+    /// Members with this annotation are ignored by the ProcedureNotUsed inspection. The CallerName argument is currently not being validated, but may be in the future.
+    /// </remarks>
+    /// <example>
+    /// <module name="Module1" type="Standard Module">
+    /// </module>
+    /// <![CDATA[
+    /// Option Explicit
+    ///
+    /// '@EntryPoint "Rounded Rectangle 1"
+    /// Public Sub DoSomething()
+    /// End Sub
+    /// 
+    /// '@EntryPoint "Rounded Rectangle 2"
+    /// Public Sub DoSomethingElse()
+    /// End Sub
+    /// ]]>
+    /// </example>
+    public sealed class EntryPointAnnotation : AnnotationBase
+    {
+        public string CallerName { get; private set; }
+
+        public EntryPointAnnotation()
+            : base("EntryPoint", AnnotationTarget.Member, allowedArguments: 1, allowedArgumentTypes: new[] { AnnotationArgumentType.Text })
+        { }
+
+        public override IReadOnlyList<string> ProcessAnnotationArguments(IEnumerable<string> arguments)
+        {
+            var args = arguments.ToList();
+
+            CallerName = args.Any()
+                ? args[0]
+                : string.Empty;
+
+            return base.ProcessAnnotationArguments(args);
+        }
+    }
+}
