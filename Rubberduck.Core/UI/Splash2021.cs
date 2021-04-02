0c84c2d0d20d8879a08b3ae8175169422b20719d
Mathieu Guindon <retailcoder@gmail.com>
2021-04-02 02:14:06 -0400

    updated splash, deleted corrupt designer

---
+++ Rubberduck.Core/UI/Splash2021.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rubberduck.UI
+{
+    public partial class Splash2021 : Form
+    {
+        public Splash2021()
+        {
+            InitializeComponent();
+#if DEBUG
+            VersionLabel.Text = $"Version {GetType().Assembly.GetName().Version.ToString()} (debug)";
+#else
+            VersionLabel.Text = $"Version {GetType().Assembly.GetName().Version.ToString()}";
+#endif
+        }
+    }
+}
