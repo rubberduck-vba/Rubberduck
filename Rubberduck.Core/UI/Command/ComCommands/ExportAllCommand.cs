f5c849705df0e491edb145ef2394f530333429c6
Brian Zenger <bzngr@outlook.com>
2022-06-17 12:25:58 -0700

    Implement Project Export Folder caching
    Introduce static Project-to-ExportPath Dictionary
    Extracted/Refactored content to protected scope methods for testing.
    Change IFileSystemBrowserFactory field to protected scope for testing.

--- Rubberduck.Core/UI/Command/ComCommands/ExportAllCommand.cs
+++ Rubberduck.Core/UI/Command/ComCommands/ExportAllCommand.cs
@@ -1,4 +1,5 @@
 using Path = System.IO.Path;
+using Directory = System.IO.Directory;
 using System.Windows.Forms;
 using Rubberduck.Navigation.CodeExplorer;
 using Rubberduck.Resources;
@@ -6,14 +7,19 @@ using Rubberduck.VBEditor.ComManagement;
 using Rubberduck.VBEditor.Events;
 using Rubberduck.VBEditor.SafeComWrappers;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
+using System.Collections.Generic;
 
 namespace Rubberduck.UI.Command.ComCommands
 {
     public class ExportAllCommand : ComCommandBase 
     {
         private readonly IVBE _vbe;
-        private readonly IFileSystemBrowserFactory _factory;
         private readonly IProjectsProvider _projectsProvider;
+
+        //protected scope to support testing
+        protected IFileSystemBrowserFactory _factory;
+
+        private static Dictionary<string,string> _projectExportFolderpaths;
 
         public ExportAllCommand(
             IVBE vbe, 
@@ -25,6 +31,10 @@ namespace Rubberduck.UI.Command.ComCommands
             _vbe = vbe;
             _factory = folderBrowserFactory;
             _projectsProvider = projectsProvider;
+            if (_projectExportFolderpaths is null)
+            {
+                _projectExportFolderpaths = new Dictionary<string, string>();
+            }
 
             AddToCanExecuteEvaluation(SpecialEvaluateCanExecute);
         }
@@ -103,23 +113,63 @@ namespace Rubberduck.UI.Command.ComCommands
 
         private void Export(IVBProject project)
         {
+            var initialFolderBrowserPath = GetInitialFolderBrowserPath(project);
+
             var desc = string.Format(RubberduckUI.ExportAllCommand_SaveAsDialog_Title, project.Name);
 
-            // If .GetDirectoryName is passed an empty string for a RootFolder, 
-            // it defaults to the Documents library (Win 7+) or equivalent.
-            var path = string.IsNullOrWhiteSpace(project.FileName)
-                ? string.Empty
-                : Path.GetDirectoryName(project.FileName);
-
-            using (var _folderBrowser = _factory.CreateFolderBrowser(desc, true, path))
+            using (var _folderBrowser = _factory.CreateFolderBrowser(desc, true, initialFolderBrowserPath))
             {
                 var result = _folderBrowser.ShowDialog();
 
                 if (result == DialogResult.OK)
                 {
+                    _projectExportFolderpaths[project.FileName] = _folderBrowser.SelectedPath;
                     project.ExportSourceFiles(_folderBrowser.SelectedPath);
                 }
             }
+        }
+
+        //protected scope to support testing
+        protected string GetInitialFolderBrowserPath(IVBProject project)
+        {
+            if (!(string.IsNullOrWhiteSpace(project.FileName))
+                && _projectExportFolderpaths.TryGetValue(project.FileName, out string initialFolderBrowserPath))
+            {
+                if (FolderExists(initialFolderBrowserPath))
+                {
+                    //Return the cached folderpath of the previous ExportAllCommand process
+                    return _projectExportFolderpaths[project.FileName];
+                }
+
+                //The folder used in the previous ExportAllComand process no longer exists, remove the cached folderpath
+                _projectExportFolderpaths.Remove(project.FileName);
+            }
+
+            //The folder of the workbook, or an empty string
+            initialFolderBrowserPath = GetDefaultExportFolder(project.FileName);
+
+            if (!string.IsNullOrEmpty(initialFolderBrowserPath))
+            {
+                _projectExportFolderpaths.Add(project.FileName, initialFolderBrowserPath);
+            }
+
+            return initialFolderBrowserPath;
+        }
+
+        //protected scope to support testing
+        protected string GetDefaultExportFolder(string projectFileName)
+        {
+            // If .GetDirectoryName is passed an empty string for a RootFolder, 
+            // it defaults to the Documents library (Win 7+) or equivalent.
+            return string.IsNullOrWhiteSpace(projectFileName)
+                ? string.Empty
+                : Path.GetDirectoryName(projectFileName);
+        }
+
+        //protected virtual to support testing
+        protected virtual bool FolderExists(string path)
+        {
+            return Directory.Exists(path);
         }
     }
 }
